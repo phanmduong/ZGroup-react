bcad62e2d39bb526a5d0586ae8200f6f14e40f1d
phanmduong <phanminhduong@hotmail.com>
2017-08-04 01:08:55 +0700

    post change role staff

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -19,6 +19,6 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
         integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
         crossorigin="anonymous"></script>
-<script type="text/javascript" src="colorme-manage/main.cb1b26b604efb74bacae.js"></script>
+<script type="text/javascript" src="colorme-manage/main.8ff3e90cd77260145153.js"></script>
 </body>
 </html>
