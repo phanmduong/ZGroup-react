a8472402ecdeaa02352aa58af9469bbb3cc7d519
phanmduong <phanminhduong@hotmail.com>
2017-08-06 01:40:40 +0700

    update code

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -19,6 +19,6 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
         integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
         crossorigin="anonymous"></script>
-<script type="text/javascript" src="colorme-manage/main.d5f3e6fbed4c15993bd5.js"></script>
+<script type="text/javascript" src="colorme-manage/main.62ec39808e3468852049.js"></script>
 </body>
 </html>
