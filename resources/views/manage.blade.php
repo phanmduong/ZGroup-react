554888a2e3f27e9b857719899bf88c36ce5f6f7e
phanmduong <phanminhduong@hotmail.com>
2017-08-06 22:36:26 +0700

    add role and delete role

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -19,6 +19,6 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
         integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
         crossorigin="anonymous"></script>
-<script type="text/javascript" src="colorme-manage/main.4e1a9312d06db55e3f2d.js"></script>
+<script type="text/javascript" src="colorme-manage/main.297782952ca13fa182d9.js"></script>
 </body>
 </html>
