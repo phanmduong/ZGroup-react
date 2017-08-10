24ab1212dd7720b5bad49f1c6b68997664404a3a
Cao Anh Quan <aquancva@gmail.com>
2017-08-10 12:38:37 +0700

    doi mau nut confirm khi delete

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -14,6 +14,7 @@ export function confirm(type, title, html, success, cancel) {
         html,
         showCloseButton: true,
         showCancelButton: true,
+        confirmButtonColor: "#c50000",
         confirmButtonText:
             'Xác nhận',
         cancelButtonText:
