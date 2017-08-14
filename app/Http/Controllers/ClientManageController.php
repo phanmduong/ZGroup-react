dff38915c5f77e46cffa18ff14e3cdf025ff5f20
Cao Anh Quan <aquancva@gmail.com>
2017-08-14 17:07:15 +0700

    up

--- app/Http/Controllers/ClientManageController.php
+++ app/Http/Controllers/ClientManageController.php
@@ -47,8 +47,7 @@ class ClientManageController extends Controller
 
     public function update()
     {
-        exec("/home/ec2-user/sync.sh 2>&1", $outputAndErrors, $return_value);
-        exec("/home/ec2-user/sync.sh", $outputAndErrors1, $return_value1);
+        exec("/home/sync.sh 2>&1", $outputAndErrors, $return_value);
         return $this->respond([
             'updated' => "Thay đổi tính năng thành công",
             'output' => $outputAndErrors,
