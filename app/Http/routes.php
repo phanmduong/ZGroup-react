17377bcc074910e4019255906705c7dde77a9236
Cao Anh Quan <aquancva@gmail.com>
2017-08-18 20:31:23 +0700

    up

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -36,6 +36,7 @@ Route::get('manage/email/open', 'PublicController@open_email');
 Route::post('manage/receive_notifications', 'PublicController@receive_notifications');
 Route::post('manage/receive_video_convert_notifications', 'PublicController@receive_video_convert_notifications');
 Route::get('access_forbidden', 'PublicController@access_forbidden');
+Route::get('test', 'PublicController@test');
 
 //Route::post('/api/topic/{topicId}/images','PublicController@store_images');
 //Route::group(['domain' => 'manage.zgroup.{ga}'], function () {
