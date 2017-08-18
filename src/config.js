be0c2fdbb0f693745473c50454bdb45a9b720e97
phanmduong <phanminhduong@hotmail.com>
2017-08-18 17:32:22 +0700

    update env

--- src/config.js
+++
@@ -1,8 +0,0 @@
-/*eslint-disable */
-var env = {
-    "DOMAIN": "zgroup.dev",
-    "NAME_COMPANY": "zGroup",
-    "LOGO_LOGIN": "http://d2xbg5ewmrmfml.cloudfront.net/web/logo2.png",
-    "LOGO_SIDEBAR": "http://d2xbg5ewmrmfml.cloudfront.net/web/logo2.png",
-    "LOGO_MAIN": "http://d2xbg5ewmrmfml.cloudfront.net/web/logo2.png"
-}
