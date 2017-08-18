d25c5a4ed634e0b5d6d428b202a6426f07b74847
Cao Anh Quan <aquancva@gmail.com>
2017-08-18 20:23:00 +0700

    up

--- app/Console/Commands/SendSurvey.php
+++ app/Console/Commands/SendSurvey.php
@@ -60,25 +60,28 @@ class SendSurvey extends Command
             $surveys = $lesson->surveys;
             if ($session) {
                 foreach ($surveys as $survey) {
-                    $lessonSurvey = LessonSurvey::where('lesson_id', $lesson->id)->where('survey_id', $survey->id)->first();
-                    if ($lessonSurvey) {
-                        $start_time_display = $lessonSurvey->start_time_display;
-                        $time_display = $lessonSurvey->time_display;
-                        $start_time = date("H:i", strtotime($session->start_time) + ($start_time_display * 60));
+                    if ($survey->active) {
+                        $lessonSurvey = LessonSurvey::where('lesson_id', $lesson->id)->where('survey_id', $survey->id)->first();
+                        if ($lessonSurvey) {
+                            $start_time_display = $lessonSurvey->start_time_display;
+                            $time_display = $lessonSurvey->time_display;
+                            $start_time = date("H:i", strtotime($session->start_time) + ($start_time_display * 60));
 
-                        $start_time_delay = strtotime($start_time) - strtotime('00:05');
+                            $start_time_delay = strtotime($start_time) - strtotime('01:00');
 
-                        $create_survey_job = (new CreateSurvey($class, $survey))->delay($start_time_delay);
-                        $this->dispatch($create_survey_job);
+                            $create_survey_job = (new CreateSurvey($class, $survey))->delay($start_time_delay);
+                            $this->dispatch($create_survey_job);
 
-                        $end_time_delay = $start_time_delay + $time_display * 60;
-                        $close_survey_job = (new CloseSurvey($class, $survey))->delay($end_time_delay);
-                        $this->dispatch($close_survey_job);
+                            $end_time_delay = $start_time_delay + $time_display * 60;
+                            $close_survey_job = (new CloseSurvey($class, $survey))->delay($end_time_delay);
+                            $this->dispatch($close_survey_job);
+                        }
                     }
                 }
             }
         }
         $this->info('The surveys were sent successfully!');
+
 
     }
 }
