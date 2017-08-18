d12a3ed8120a8b60f3e17b57dfa61d5ba498a953
Cao Anh Quan <aquancva@gmail.com>
2017-08-18 19:04:37 +0700

    up

--- app/Colorme/Transformers/CommentTransformer.php
+++ app/Colorme/Transformers/CommentTransformer.php
@@ -16,6 +16,7 @@ class CommentTransformer extends Transformer
     {
         return [
             'id' => $comment->id,
+            'image_url' => $comment->image_url,
             "commenter" => [
                 'id' => $comment->commenter->id,
                 'avatar_url' => $comment->commenter->avatar_url ? $comment->commenter->avatar_url : url('img/user.png'),
@@ -23,7 +24,7 @@ class CommentTransformer extends Transformer
                 'url' => url('profile/' . $comment->commenter->username)
             ],
             'likes' => $comment->likes,
-            'likers' => $comment->comment_likes->map(function($c) {
+            'likers' => $comment->comment_likes->map(function ($c) {
                 return [
                     'name' => $c->liker->name
                 ];
@@ -33,6 +34,7 @@ class CommentTransformer extends Transformer
                     'id' => $comment->product->author->id
                 ]
             ],
+            'parent_id' => $comment->parent_id,
             'content' => $comment->content,
             'created_at' => time_elapsed_string(strtotime($comment->created_at))
         ];
