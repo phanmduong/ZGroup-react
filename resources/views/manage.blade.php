57aed955329e5e8c4760d05b0447b119aff319d2
Cao Anh Quan <aquancva@gmail.com>
2017-08-22 10:16:50 +0700

    fix bug with boards

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -64,6 +64,6 @@
 <!-- Material Dashboard DEMO methods, don't include it in your project! -->
 <script src="http://d1j8r0kxyu9tj8.cloudfront.net/libs/material/assets/js/demo.js"></script>
 
-<script type="text/javascript" src="manage/main.134a94dca61e5569e1f8.js"></script>
+<script type="text/javascript" src="manage/main.2fe54cb3eaaa20fe232d.js"></script>
 </body>
 </html>
