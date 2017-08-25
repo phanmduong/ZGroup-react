ab62f32d8433e81afb0610fcd261b874d50ebdfa
Cao Anh Quan <aquancva@gmail.com>
2017-08-25 11:39:54 +0700

    update gitignore

--- src/config.js
+++
@@ -1,9 +0,0 @@
-/*eslint-disable */
-var env = {
-    "DOMAIN": "keetoolclient.dev",
-    "NAME_COMPANY": "KEETOOL test",
-    "LOGO_LOGIN": "https://d255zuevr6tr8p.cloudfront.net/logo/Artboard+5+copy.png",
-    "LOGO_SIDEBAR": "https://d255zuevr6tr8p.cloudfront.net/logo/Artboard+5+copy.png",
-    "LOGO_MAIN": "https://d255zuevr6tr8p.cloudfront.net/logo/Artboard+5+copy.png",
-    "PROTOCOL": "http://"
-};
