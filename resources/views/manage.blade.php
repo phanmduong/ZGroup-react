61a2dc0de8e8a141d8b949642d93fc2487ff4335
caoquan95 <aquancva@gmail.com>
2017-08-27 23:05:27 +0700

    up

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -64,6 +64,6 @@
 <!-- Material Dashboard DEMO methods, don't include it in your project! -->
 <script src="http://d1j8r0kxyu9tj8.cloudfront.net/libs/material/assets/js/demo.js"></script>
 
-<script type="text/javascript" src="manage/main.b7cdc82e984b6b92ccc3.js"></script>
+<script type="text/javascript" src="manage/main.ab94247010b6d4864046.js"></script>
 </body>
 </html>
