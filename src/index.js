595fe9b768d731f8feffc56a606f68bb27de0da7
phanmduong <phanminhduong@hotmail.com>
2017-08-29 12:02:36 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import {Router, browserHistory} from 'react-router';
 import routes from './routes';
 import configureStore from './store/configureStore';
 
-import './styles/styles.scss';
 import '../node_modules/toastr/build/toastr.min.css';
 import './styles/react-bootstrap-switch.min.css';
 import './styles/dragula.css';
 import './modules/tasks/task.css';
 import './styles/react-select.css';
 import "./styles/react-draft-wysiwyg.css";
+import './styles/styles.scss';
 // import { syncHistoryWithStore } from 'react-router-redux';
 
 const store = configureStore();
