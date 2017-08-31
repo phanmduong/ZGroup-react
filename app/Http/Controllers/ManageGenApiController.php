0ac9ac6b13eccf551e0a7fab4164e2fa2e639e49
phanmduong <phanminhduong@hotmail.com>
2017-08-31 22:57:53 +0700

    update code gens

---
+++ app/Http/Controllers/ManageGenApiController.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: phanmduong
+ * Date: 8/31/17
+ * Time: 15:29
+ */
+
+namespace App\Http\Controllers;
+
+
+use App\Gen;
+use Illuminate\Http\Request;
+
+class ManageGenApiController extends ManageApiController
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function get_gens()
+    {
+        $limit = 7;
+        $gens = Gen::orderBy('id', 'decs')->paginate($limit);
+        $data = [
+            "gens" => $gens->map(function ($gen) {
+                return [
+                    'id' => $gen->id,
+                    'name' => $gen->name,
+                    'description' => $gen->description,
+                    'start_time' => format_date_to_mysql($gen->start_time),
+                    'end_time' => format_date_to_mysql($gen->end_time),
+                    'status' => $gen->status,
+                    'teach_status' => $gen->teach_status,
+                ];
+            })
+        ];
+        return $this->respondWithPagination($gens, $data);
+    }
+
+    public function add_gen(Request $request)
+    {
+        $gen = new Gen();
+        $gen->name = $request->name;
+        $gen->description = $request->description;
+        $gen->start_time = date('Y-m-d', strtotime($request->start_time));
+        $gen->end_time = date('Y-m-d', strtotime($request->end_time));
+        $gen->save();
+        return $this->respondSuccessWithStatus([
+            'gen' => $gen
+        ]);
+    }
+
+    public function edit_gen($genId, Request $request)
+    {
+        $gen = Gen::find($genId);
+        $gen->name = $request->name;
+        $gen->description = $request->description;
+        $gen->start_time = date('Y-m-d', strtotime($request->start_time));
+        $gen->end_time = date('Y-m-d', strtotime($request->end_time));
+        $gen->save();
+        return $this->respondSuccessWithStatus([
+            'message' => 'Thành công'
+        ]);
+    }
+
+    public function delete_gen(Request $request)
+    {
+        $gen = Gen::where('id', $request->id)->first();
+        $gen->delete();
+        return $this->respondSuccessWithStatus([
+            'message' => 'Xóa thành công'
+        ]);
+    }
+
+    public function change_status(Request $request)
+    {
+        $gens = Gen::all();
+        $id = $request->id;
+        foreach ($gens as $gen) {
+            if ($id == $gen->id) {
+                $gen->status = 1;
+                $gen->save();
+            } else {
+                $gen->status = 0;
+                $gen->save();
+            }
+        }
+        return $this->respondSuccessWithStatus([
+            'message' => 'Thay đổi thành công'
+        ]);
+    }
+
+    public function change_teach_status(Request $request)
+    {
+        $gens = Gen::all();
+        $id = $request->id;
+        foreach ($gens as $gen) {
+            if ($id == $gen->id) {
+                $gen->teach_status = 1;
+                $gen->save();
+            } else {
+                $gen->teach_status = 0;
+                $gen->save();
+            }
+        }
+        return $this->respondSuccessWithStatus([
+            'message' => 'Thay đổi thành công'
+        ]);
+    }
+}
