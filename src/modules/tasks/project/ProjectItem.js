b643034b44c1b703b3c096ee163b6df7afda0316
caoquan95 <aquancva@gmail.com>
2017-09-05 20:17:35 +0700

    save

--- src/modules/tasks/project/ProjectItem.js
+++ src/modules/tasks/project/ProjectItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from "react-router";
 
 class ProjectItem extends React.Component {
     constructor(props, context) {
@@ -7,10 +8,12 @@ class ProjectItem extends React.Component {
     }
 
     render() {
+        const {project} = this.props;
         return (
             <div className="col-md-4 col-sm-6 col-lg-3">
-                <button style={{width: "100%", background: "white", color: "#455a64", textAlign: "left"}}
-                        className="btn btn-default btn-lg">
+                <Link to={"project/" + project.id + "/boards"}
+                      style={{width: "100%", background: "white", color: "#455a64", textAlign: "left"}}
+                      className="btn btn-default btn-lg">
                     <div style={{position: "absolute", top: "20px", right: "15px"}}>
                         <div className="board-action">
                             <a style={{color: "#455a64"}}>
@@ -22,30 +25,35 @@ class ProjectItem extends React.Component {
                         <i className="material-icons">account_balance_wallet</i> {project.title}
                     </div>
                     <div className="row"
-                         style={{height: "5px", marginTop: "10px", marginBottom: "10px", background: "#42a5f5"}}>
-
+                         style={{height: "5px", marginTop: "10px", marginBottom: "10px", background: project.color}}>
                     </div>
                     <div className="row" style={{textTransform: "none", marginBottom: "10px"}}>
                         Mô tả dự án<br/>
-                        3 bảng | 62 thẻ | 4 thành viên<br/>
+                        {project.board_count} bảng | {project.card_count} thẻ | {project.members.length} thành viên<br/>
                     </div>
                     <div className="row" style={{display: "flex", flexFlow: "row-reverse wrap"}}>
-                        <div style={{padding: "2px 0px"}}>
-                            <div style={{
-                                width: "25px",
-                                marginRight: "5px",
-                                height: "25px",
-                                backgroundPosition: "center center",
-                                backgroundSize: "cover",
-                                borderRadius: "4px",
-                                backgroundImage: "url('http://d1j8r0kxyu9tj8.cloudfront.net/images/1503369355g3nTaVigDKKyjUQ.jpg')"
-                            }}>
-                            </div>
-                        </div>
+                        {
+                            project.members.map((member) => {
+                                return (
+                                    <div style={{padding: "2px 0px"}}>
+                                        <div style={{
+                                            width: "25px",
+                                            marginRight: "5px",
+                                            height: "25px",
+                                            backgroundPosition: "center center",
+                                            backgroundSize: "cover",
+                                            borderRadius: "4px",
+                                            backgroundImage: `url('${member.avatar_url}')`
+                                        }}>
+                                        </div>
+                                    </div>
+                                );
+                            })
+                        }
                     </div>
                     <div className="ripple-container">
                     </div>
-                </button>
+                </Link>
             </div>
         );
     }
