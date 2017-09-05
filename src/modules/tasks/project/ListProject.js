8ebf05d699451b8158c5683e444b949528e24a8f
Cao Anh Quan <aquancva@gmail.com>
2017-09-05 18:11:23 +0700

    up

--- src/modules/tasks/project/ListProject.js
+++ src/modules/tasks/project/ListProject.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Link} from "react-router";
-import FormInputSelect from "../../../components/common/FormInputSelect";
-import ButtonGroupAction from "../../../components/common/ButtonGroupAction";
+// import {Link} from "react-router";
+// import FormInputSelect from "../../../components/common/FormInputSelect";
+// import ButtonGroupAction from "../../../components/common/ButtonGroupAction";
+import ProjectItem from "./ProjectItem";
 
 class ListProject extends React.Component {
     constructor(props, context) {
@@ -12,64 +13,69 @@ class ListProject extends React.Component {
 
     render() {
         return (
-            <div className="table-responsive">
-                <table className="table">
-                    <thead>
-                    <tr className="text-rose">
-                        <th>Tên dự án</th>
-                        <th>Mô tả</th>
-                        <th>Người thêm</th>
-                        <th>Thêm vào lúc</th>
-                        <th>Người sửa gần nhất</th>
-                        <th>Sửa gần nhất</th>
-                        <th>Trạng thái</th>
-                        <th/>
-                    </tr>
-                    </thead>
-
-                    <tbody>
-                    {this.props.projects.map(project => {
-                        return (
-                            <tr key={project.id}>
-                                <td>
-                                    <Link className="btn btn-rose btn-sm btn-main"
-                                          to={"project/" + project.id + "/boards"}>
-                                        {project.title}
-                                    </Link>
-                                </td>
-                                <td>{project.description}</td>
-                                <td>{project.creator.name}</td>
-                                <td>{project.created_at}</td>
-                                <td>{project.editor.name}</td>
-                                <td>{project.updated_at}</td>
-                                <td>
-                                    <FormInputSelect
-                                        isNotForm={true}
-                                        placeholder="Vui lòng chọn 1 trạng thái"
-                                        name="status"
-                                        data={[
-                                            {id: 'open', name: 'mở'},
-                                            {id: 'close', name: 'đóng'}
-                                        ]}
-                                        value={project.status}
-                                        updateFormData={(event) => this.props.changeProjectStatus(project, event.target.value)}
-                                    />
-                                </td>
-
-                                <td>
-                                    <ButtonGroupAction
-                                        delete={this.props.deleteProject}
-                                        editUrl={'/project/' + project.id + "/edit"}
-                                        object={project}
-                                    />
-                                </td>
-                            </tr>
-                        );
-                    })}
-                    </tbody>
-                </table>
+            <div className="row">
+                {this.props.projects.map((project) => <ProjectItem key={project.id} project={project}/>)}
             </div>
         );
+        // return (
+        //     <div className="table-responsive">
+        //         <table className="table">
+        //             <thead>
+        //             <tr className="text-rose">
+        //                 <th>Tên dự án</th>
+        //                 <th>Mô tả</th>
+        //                 <th>Người thêm</th>
+        //                 <th>Thêm vào lúc</th>
+        //                 <th>Người sửa gần nhất</th>
+        //                 <th>Sửa gần nhất</th>
+        //                 <th>Trạng thái</th>
+        //                 <th/>
+        //             </tr>
+        //             </thead>
+        //
+        //             <tbody>
+        //             {this.props.projects.map(project => {
+        //                 return (
+        //                     <tr key={project.id}>
+        //                         <td>
+        //                             <Link className="btn btn-rose btn-sm btn-main"
+        //                                   to={"project/" + project.id + "/boards"}>
+        //                                 {project.title}
+        //                             </Link>
+        //                         </td>
+        //                         <td>{project.description}</td>
+        //                         <td>{project.creator.name}</td>
+        //                         <td>{project.created_at}</td>
+        //                         <td>{project.editor.name}</td>
+        //                         <td>{project.updated_at}</td>
+        //                         <td>
+        //                             <FormInputSelect
+        //                                 isNotForm={true}
+        //                                 placeholder="Vui lòng chọn 1 trạng thái"
+        //                                 name="status"
+        //                                 data={[
+        //                                     {id: 'open', name: 'mở'},
+        //                                     {id: 'close', name: 'đóng'}
+        //                                 ]}
+        //                                 value={project.status}
+        //                                 updateFormData={(event) => this.props.changeProjectStatus(project, event.target.value)}
+        //                             />
+        //                         </td>
+        //
+        //                         <td>
+        //                             <ButtonGroupAction
+        //                                 delete={this.props.deleteProject}
+        //                                 editUrl={'/project/' + project.id + "/edit"}
+        //                                 object={project}
+        //                             />
+        //                         </td>
+        //                     </tr>
+        //                 );
+        //             })}
+        //             </tbody>
+        //         </table>
+        //     </div>
+        // );
     }
 }
 
---
+++ src/modules/tasks/project/ProjectItem.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ProjectItem extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        return (
+            <div className="col-md-4 col-sm-6 col-lg-3">
+                <button style={{width: "100%", background: "white", color: "#455a64", textAlign: "left"}}
+                        className="btn btn-default btn-lg">
+                    <div style={{position: "absolute", top: "20px", right: "15px"}}>
+                        <div className="board-action">
+                            <a style={{color: "#455a64"}}>
+                                <i className="material-icons" style={{fontSize: "18px"}}>edit</i>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="row" style={{fontSize: "16px", fontWeight: 600}}>
+                        <i className="material-icons">account_balance_wallet</i> {project.title}
+                    </div>
+                    <div className="row"
+                         style={{height: "5px", marginTop: "10px", marginBottom: "10px", background: "#42a5f5"}}>
+
+                    </div>
+                    <div className="row" style={{textTransform: "none", marginBottom: "10px"}}>
+                        Mô tả dự án<br/>
+                        3 bảng | 62 thẻ | 4 thành viên<br/>
+                    </div>
+                    <div className="row" style={{display: "flex", flexFlow: "row-reverse wrap"}}>
+                        <div style={{padding: "2px 0px"}}>
+                            <div style={{
+                                width: "25px",
+                                marginRight: "5px",
+                                height: "25px",
+                                backgroundPosition: "center center",
+                                backgroundSize: "cover",
+                                borderRadius: "4px",
+                                backgroundImage: "url('http://d1j8r0kxyu9tj8.cloudfront.net/images/1503369355g3nTaVigDKKyjUQ.jpg')"
+                            }}>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="ripple-container">
+                    </div>
+                </button>
+            </div>
+        );
+    }
+}
+
+ProjectItem.propTypes = {
+    project: PropTypes.object.isRequired
+};
+
+export default ProjectItem;
