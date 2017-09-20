cba0b265e86cbdb225dabd9429828ee0facce176
phanmduong <phanminhduong@hotmail.com>
2017-09-20 21:34:58 +0700

    update code

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -23,7 +23,7 @@
     <link rel="stylesheet" type="text/css"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"/>
 
-    <link href="manage/main.213f95bfbc6da99571c3a2d14801e2e9.css" rel="stylesheet">
+    <link href="manage/main.332a88f741592898f215e9f354afbda2.css" rel="stylesheet">
 
     <link href="{{url('config.css')}}" rel="stylesheet">
 </head>
@@ -74,7 +74,7 @@
 
 
 <script src="{{url('config.js')}}"></script>
-<script type="text/javascript" src="manage/main.c5ed7e2abd38e8a3fdc8.js"></script>
+<script type="text/javascript" src="manage/main.883346343962c1d1e699.js"></script>
 
 
 <!-- Material Dashboard javascript methods -->
