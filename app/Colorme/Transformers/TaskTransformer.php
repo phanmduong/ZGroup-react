847a8967dfe6d74764b04fe9b32275a4babde8b0
Cao Anh Quan <aquancva@gmail.com>
2017-09-22 10:55:01 +0700

    add member to task

--- app/Colorme/Transformers/TaskTransformer.php
+++ app/Colorme/Transformers/TaskTransformer.php
@@ -14,11 +14,19 @@ class TaskTransformer extends Transformer
 
     public function transform($task)
     {
-        return [
+        $data = [
             "title" => $task->title,
             "status" => $task->status,
             "id" => $task->id,
             "task_list_id" => $task->task_list_id
         ];
+        if ($task->member) {
+            $data["member"] = [
+                "id" => $task->member->id,
+                "name" => $task->member->name,
+                "avatar_url" => generate_protocol_url($task->member->avatar_url),
+            ];
+        }
+        return $data;
     }
 }
