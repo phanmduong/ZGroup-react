b66caf1200e8aec200cedf0a1487f796e1099f17
Cao Anh Quan <aquancva@gmail.com>
2017-09-23 23:20:34 +0700

    up

--- app/Console/Commands/CreateShifts.php
+++ app/Console/Commands/CreateShifts.php
@@ -44,9 +44,9 @@ class CreateShifts extends Command
     public function handle()
     {
         $date = new \DateTime();
-        $date->modify('+8 days');
+        $date->modify('+3 days');
         $formatted_date_from = $date->format('Y-m-d');
-        $date->modify('+1 days');
+        $date->modify('+6 days');
         $formatted_date_to = $date->format('Y-m-d');
         $dates = createDateRangeArray(strtotime($formatted_date_from), strtotime($formatted_date_to));
         $bases = Base::where('center', 1)->get();
