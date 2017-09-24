5f1cafefbcfcc2852c85198eb1a5005993913d6e
Cao Anh Quan <aquancva@gmail.com>
2017-09-24 16:46:51 +0700

    save

--- Modules/Task/Entities/TaskList.php
+++ Modules/Task/Entities/TaskList.php
@@ -19,4 +19,15 @@ class TaskList extends Model
     {
         return $this->belongsTo(Card::class, 'card_id');
     }
+
+    public function transform()
+    {
+        return [
+            "id" => $this->id,
+            "title" => $this->title,
+            "tasks" => $this->tasks->map(function ($task) {
+                return $task->transform();
+            })
+        ];
+    }
 }
--- app/Task.php
+++ app/Task.php
@@ -26,4 +26,26 @@ class Task extends Model
     {
         return $this->belongsTo(User::class, 'editor_id');
     }
+
+    public function transform()
+    {
+        $data = [
+            "title" => $this->title,
+            "status" => $this->status,
+            "id" => $this->id,
+            "task_list_id" => $this->task_list_id
+        ];
+        if ($this->deadline && $this->deadline != "0000-00-00 00:00:00") {
+            $data["deadline_str"] = time_remain_string(strtotime($this->deadline));
+            $data["deadline"] = date("H:i d-m-Y", strtotime($this->deadline));
+        }
+        if ($this->member) {
+            $data["member"] = [
+                "id" => $this->member->id,
+                "name" => $this->member->name,
+                "avatar_url" => generate_protocol_url($this->member->avatar_url),
+            ];
+        }
+        return $data;
+    }
 }
