e88915f3ec70aa82572763867c431c7e0231465a
thanhphamtt <thanhpham1479@gmail.com>
2017-10-01 14:21:15 +0700

    1234

--- Modules/SocialApi/Http/routes.php
+++ Modules/SocialApi/Http/routes.php
@@ -4,4 +4,10 @@ Route::group(['domain' => 'api.' . config('app.domain'), 'prefix' => 'apiv2', 'n
 {
     Route::post('/register', 'RegisterController@register');
     Route::post('/login', 'LoginController@login');
+    Route::get('/product','ProductApiController@products');
 });
+
+//Route::group(['domain'=>'api.' . config('api.domain'), 'prefix'=>'apiv2', 'middleware'=>['jwt.auth'], 'namespace'=>'Modules\SocialApi\Http\Controllers'], function()
+//{
+//    Route::get('/product','ProductApiController@products');
+//});
