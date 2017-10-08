d7cb459c8c3d7e19a41f786dce09b2e4656f0311
datvithanh98 <datvithanh98@gmail.com>
2017-10-08 11:55:53 +0700

    initial course routes

--- Modules/Course/Http/routes.php
+++ Modules/Course/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'course', 'namespace' => 'Modules\Course\Http\Controllers'], function()
-{
-    Route::get('/', 'CourseController@index');
+Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => 'course', 'namespace' => 'Modules\Course\Http\Controllers'], function () {
+
 });
+
