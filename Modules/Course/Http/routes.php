73c5022db575051dd76afce822f9e93429efa0f9
thanhphamtt <thanhpham1479@gmail.com>
2017-10-08 11:56:52 +0700

    upp

--- Modules/Course/Http/routes.php
+++ Modules/Course/Http/routes.php
@@ -2,5 +2,7 @@
 
 Route::group(['middleware' => 'web', 'prefix' => 'course', 'namespace' => 'Modules\Course\Http\Controllers'], function()
 {
-    Route::get('/', 'CourseController@index');
+
+
+
 });
