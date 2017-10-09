27ac47df4f7cccb302c089c1ef73671f430c8b64
phanmduong <phanminhduong@hotmail.com>
2017-10-10 03:01:50 +0700

    update code

--- socket-http.js
+++ socket-http.js
@@ -25,7 +25,7 @@ redis.subscribe(env.CHANNEL, function (err, count) {
 
 redis.on('message', function (channel, message) {
     // console.log('Message Recieved: ' + message);
-    message = JSON.parse(messagdsae);
+    message = JSON.parse(message);
 
     if (message.event === 'add_token_browser') {
         addTokenBrowser(message.data.user_id, message.data.token);
