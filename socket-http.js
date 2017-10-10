de3a95e07fbb93b1419be3d860269a8c9da291f1
phanmduong <phanminhduong@hotmail.com>
2017-10-11 01:53:51 +0700

    update code notification

--- socket-http.js
+++ socket-http.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var Redis = require('ioredis');
+var htmlToText = require('html-to-text');
 var env = require('./socketEnv');
 
 var redis = new Redis(env.REDIS_PORT, env.REDIS_HOST);
