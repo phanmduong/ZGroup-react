c495a043a457e7528fd15bb6a18930f885208dfc
phanmduong <phanminhduong@hotmail.com>
2017-10-12 02:02:11 +0700

    update code

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -72,9 +72,8 @@
 <!--  Full Calendar Plugin    -->
 <script src="http://d1j8r0kxyu9tj8.cloudfront.net/libs/material/assets/js/fullcalendar.min.js"></script>
 
-
 <script src="{{url('config.js')}}"></script>
-<script type="text/javascript" src="manage/main.3e2b4d48a0dc72f1b414.js"></script>
+<script type="text/javascript" src="manage/main.8b10c4e1ec1223f09344.js"></script>
 
 
 <!-- Material Dashboard javascript methods -->
