61c4e7c5f9137c7b63e1dbc4de33646b44a235ab
Cao Anh Quan <aquancva@gmail.com>
2017-10-13 21:32:41 +0700

    up

--- database/migrations/2017_10_13_213216_add_start_board_to_boards.php
+++ database/migrations/2017_10_13_213216_add_start_board_to_boards.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddStartBoardToBoard extends Migration
+class AddStartBoardToBoards extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,9 @@ class AddStartBoardToBoard extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('boards', function (Blueprint $table) {
+            $table->boolean('is_start');
+        });
     }
 
     /**
@@ -22,8 +24,6 @@ class AddStartBoardToBoard extends Migration
      */
     public function down()
     {
-        Schema::table('boards', function (Blueprint $table) {
-            $table->boolean('is_start');
-        });
+        //
     }
 }
