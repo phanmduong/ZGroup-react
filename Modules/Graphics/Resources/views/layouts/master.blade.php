a49d3a2654e77b2d05dc2067f9885a0e27681b2e
datvithanh98 <datvithanh98@gmail.com>
2017-10-14 17:07:37 +0700

    helppaaa

--- Modules/Graphics/Resources/views/layouts/master.blade.php
+++ Modules/Graphics/Resources/views/layouts/master.blade.php
@@ -3,23 +3,22 @@
 
     <head>
         <meta charset="utf-8" />
-        <link rel="icon" type="image/png" href="assets/img/favicon.ico">
-        <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+        <link rel="icon" type="image/png" href="/assets/img/favicon.ico">
+        <link rel="apple-touch-icon" sizes="76x76" href="/assets/img/apple-icon.png">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
         <title>Tạp chí Graphics</title>
 
         <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
         <meta name="viewport" content="width=device-width" />
 
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-        <link href="assets/css/paper-kit.css" rel="stylesheet"/>
-        <link href="assets/css/demo.css" rel="stylesheet" />
+        <link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="/assets/css/paper-kit.css" rel="stylesheet"/>
+        <link href="/assets/css/demo.css" rel="stylesheet" />
 
         <!--     Fonts and icons     -->
         <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
         <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-        <link href="assets/css/nucleo-icons.css" rel="stylesheet">
+        <link href="/assets/css/nucleo-icons.css" rel="stylesheet">
 
     </head>
     <body class="profile" style="background:#fafafa">
@@ -63,7 +62,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-2 col-sm-3 col-xs-12">
-                    <img src="assets/img/darklogo.png" width="150px"/>
+                    <img src="/assets/img/darklogo.png" width="150px"/>
                     <div class="social-area">
                         <a class="btn btn-just-icon btn-round btn-facebook">
                             <i class="fa fa-facebook" aria-hidden="true"></i>
@@ -182,9 +181,9 @@
     </body>
 
 <!-- Core JS Files -->
-<script src="assets/js/jquery-3.2.1.min.js" type="text/javascript"></script>
-<script src="assets/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
-<script src="assets/js/tether.min.js" type="text/javascript"></script>
-<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="assets/js/paper-kit.js?v=2.0.0"></script>
+<script src="/assets/js/jquery-3.2.1.min.js" type="text/javascript"></script>
+<script src="/assets/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
+<script src="/assets/js/tether.min.js" type="text/javascript"></script>
+<script src="/assets/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="/assets/js/paper-kit.js?v=2.0.0"></script>
 </html>
