dfb5461ebd1d046ee16d28215acc2e767e9f8312
datvithanh98 <datvithanh98@gmail.com>
2017-10-14 16:39:24 +0700

    helppaaa

--- Modules/Graphics/Http/Controllers/GraphicsController.php
+++ Modules/Graphics/Http/Controllers/GraphicsController.php
@@ -18,17 +18,17 @@ class GraphicsController extends Controller
     public function product($link, $goodId)
     {
         $good = Good::find($goodId);
-        $properties = GoodProperty::where('good_id',$goodId)->get();
+        $properties = GoodProperty::where('good_id', $goodId)->get();
 
-        $data =[];
-//            "cover" => $good->cover_url,
-//            "avatar" => $good->avatar_url,
-//            "type" => $good->type,
-//        ];
+        $data = [];
+            "cover" => $good->cover_url,
+            "avatar" => $good->avatar_url,
+            "type" => $good->type,
+        ];
         foreach ($properties as $property) {
             $data[$property->name] = $property->value;
         }
-        dd($properties);
+        dd($data);
         return view('graphics::index', [
             'properties' => $data,
         ]);
