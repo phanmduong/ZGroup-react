30d73018c850aebc3fca11c926709a7353e24001
thanhphamtt <thanhpham1479@gmail.com>
2017-10-15 09:56:11 +0700

    about-us

--- Modules/Graphics/Http/routes.php
+++ Modules/Graphics/Http/routes.php
@@ -3,5 +3,8 @@
 Route::group(['middleware' => 'web', 'prefix' => 'graphics', 'namespace' => 'Modules\Graphics\Http\Controllers'], function()
 {
     Route::get('/', 'GraphicsController@aaa');
+    Route::get('/about-us', function () {
+        return view('graphics::about_us');
+    });
     Route::get('/product/{good_id}', 'GraphicsController@product');
 });
