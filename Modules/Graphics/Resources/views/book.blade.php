9ea7bdb37bc8164167f6a7246f1cadff90b0e8d0
phanmduong <phanminhduong@hotmail.com>
2017-10-16 02:04:30 +0700

    update code

--- Modules/Graphics/Resources/views/book.blade.php
+++ Modules/Graphics/Resources/views/book.blade.php
@@ -7,7 +7,7 @@
             <div class="filter"></div>
             <div class="content-center">
                 <div class="container">
-                    <h1 style="font-weight:600; text-transform: uppercase">{{$properties['product-type']}}
+                    <h1 style="font-weight:600; text-transform: uppercase">{{$properties['product_type']}}
                         <br>
                         {{$properties['name']}}
                         </br>
