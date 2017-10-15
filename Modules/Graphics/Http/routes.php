721786615eb824cd203cc4c91612b09ca7f567e2
Cao Anh Quan <aquancva@gmail.com>
2017-10-15 23:45:28 +0700

    up

--- Modules/Graphics/Http/routes.php
+++ Modules/Graphics/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => 'web', 'domain' => "graphics.dev", 'namespace' => 'Modules\Graphics\Http\Controllers'], function () {
+Route::group(['middleware' => 'web', 'domain' => config("app.domain_commerce"), 'namespace' => 'Modules\Graphics\Http\Controllers'], function () {
     Route::get('/', 'GraphicsController@index');
     Route::get('/contact-us', 'GraphicsController@contact_us');
     Route::get('/about-us', 'GraphicsController@aboutUs');
@@ -10,10 +10,10 @@ Route::group(['middleware' => 'web', 'domain' => "graphics.dev", 'namespace' => 
     Route::get('/remove-book/{goodId}', 'GraphicsController@removeBookFromCart');
     Route::get('/load-books-from-session', 'GraphicsController@getGoodsFromSession');
     Route::get('/about-us', 'GraphicsController@about_us');
-    Route::post('/contact_information','GraphicsController@contact_info');
+    Route::post('/contact_information', 'GraphicsController@contact_info');
     Route::get('/book/{good_id}', 'GraphicsController@book');
-    Route::get('/blog','GraphicsController@blog');
-    Route::get('/blog/post/{post_id}','GraphicsController@post');
+    Route::get('/blog', 'GraphicsController@blog');
+    Route::get('/blog/post/{post_id}', 'GraphicsController@post');
 });
 
 
