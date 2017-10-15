33a6b7dc4bbd84b95deaa6d761b1104986fa3adf
thanhphamtt <thanhpham1479@gmail.com>
2017-10-15 15:27:42 +0700

    blogs

--- Modules/Graphics/Http/routes.php
+++ Modules/Graphics/Http/routes.php
@@ -3,10 +3,11 @@
 Route::group(['middleware' => 'web','domain' => config('app.domain_commerce'), 'namespace' => 'Modules\Graphics\Http\Controllers'], function () {
     Route::get('/', 'GraphicsController@index');
     Route::get('/contact-us', 'GraphicsController@contact_us');
-    Route::get('/about-us', 'GraphicsController@aboutUs');
+    Route::get('/about-us', 'GraphicsController@about_us');
     Route::post('/contact_information','GraphicsController@contact_info');
     Route::get('/book/{good_id}', 'GraphicsController@book');
-
+    Route::get('/blog','GraphicsController@blog');
+    Route::get('/post/{blog_id}','GraphicsController@post');
 });
 
 
