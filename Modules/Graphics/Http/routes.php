03c01ece5d0b6ecdaa3316f2f6e5bded2dc5e14f
datvithanh98 <datvithanh98@gmail.com>
2017-10-15 10:26:02 +0700

    fix route 2

--- Modules/Graphics/Http/routes.php
+++ Modules/Graphics/Http/routes.php
@@ -1,9 +1,12 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'graphics', 'namespace' => 'Modules\Graphics\Http\Controllers'], function()
+Route::group(['middleware' => 'web', 'namespace' => 'Modules\Graphics\Http\Controllers'], function()
 {
-    Route::get('/contact-us','GraphicsController@contact_us');
-    Route::get('/index', 'GraphicsController@index');
-    Route::get('/about-us', 'GraphicsController@aboutUs');
-    //Route::get('/product/{good_id}', 'GraphicsController@product');
+    Route::group(['domain' => 'graphics.vn'], function () {
+        Route::get('/contact-us','GraphicsController@contact_us');
+        Route::get('/index', 'GraphicsController@index');
+        Route::get('/about-us', 'GraphicsController@aboutUs');
+    });
 });
+
+
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -735,4 +735,8 @@ Route::group(['middleware' => 'web'], function () {
     Route::post('manage/store-marketing-campaign', 'ManageMarketingController@store_marketing_campaign');
     Route::get('manage/delete-marketing-campaign/{campaign_id}', 'ManageMarketingController@delete_marketing_campaign');
     Route::get('/manage/staff/add-staff', 'RoleController@add_staff');
+
+
+    //Route graphics
 });
+
