4fc99a5b8db20d15352cb89b53c6d44255ee845e
phanmduong <phanminhduong@hotmail.com>
2017-10-16 13:47:09 +0700

    update code

--- Modules/Graphics/Resources/views/index.blade.php
+++ Modules/Graphics/Resources/views/index.blade.php
@@ -36,7 +36,7 @@
                 <div>
                     <div class="description">
                         <h1 class="medium-title">
-                            Sản phẩm nổi bật<br>
+                            Sản phẩm mới nhất<br>
                         </h1>
                         <br><a href="#pablo" class="btn btn-link btn-success"
                                style="padding:0!important; margin:0!important">Xem tất cả <i
@@ -52,34 +52,37 @@
         <div class="row">
             @foreach($books as $book)
                 <div class="col-md-6 book-item">
-                    <div class="card card-profile" style="border-radius:0">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <div class="card card-profile card-plain">
-                                        <img class="card-book-image" src="{{$book['avatar']}}">
+                    <div class="card card-profile" style="border-radius:0; height: 90%">
+                        <div class="flex flex-col flex-justify-content-space-between" style="height: 100%">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="card card-profile card-plain">
+                                            <img class="card-book-image" src="{{$book['avatar']}}">
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-md-8 text-left">
-                                    <br>
-                                    <h5 style="font-weight:600">{{$book['name']}}</h5>
-                                    <h6>{{$book['short_description']}}</h6><br>
-                                    <p>{{$book['description']}}</p>
-                                    <h5>{{currency_vnd_format($book['price'])}}</h5><br>
-                                </div>
+                                    <div class="col-md-8 text-left">
+                                        <br>
+                                        <h5 style="font-weight:600">{{$book['name']}}</h5>
+                                        <h6>{{$book['short_description']}}</h6><br>
+                                        <p>{{$book['description']}}</p>
+                                        <h5>{{currency_vnd_format($book['price'])}}</h5><br>
+                                    </div>
 
+                                </div>
                             </div>
-                        </div>
-                        <div class="card-footer" style="border-top: 1px solid #dcdbdb!important;">
-                            <div style="display:flex;flex-direction:row;justify-content:space-between;">
-                                <div>
-                                    <a href="http://graphics.vn/book/{{$book['id']}}" class="btn btn-link btn-success">
-                                        Xem thêm
-                                    </a>
-                                    <button onclick="openModalBuy({{$book["id"]}},{{$book["price"]}})"
-                                            class="btn btn-sm btn-success">
-                                        Đặt mua ngay <i class="fa fa-angle-right"></i>
-                                    </button>
+                            <div class="card-footer" style="border-top: 1px solid #dcdbdb!important;">
+                                <div style="display:flex;flex-direction:row;justify-content:space-between;">
+                                    <div>
+                                        <a href="http://graphics.vn/book/{{$book['id']}}"
+                                           class="btn btn-link btn-success">
+                                            Xem thêm
+                                        </a>
+                                        <button onclick="openModalBuy({{$book["id"]}},{{$book["price"]}})"
+                                                class="btn btn-sm btn-success">
+                                            Đặt mua ngay <i class="fa fa-angle-right"></i>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -110,15 +113,20 @@
                                 </h1>
                                 <br><h5>CHO NHỮNG NGƯỜI YÊU THÍCH THIẾT KẾ</h5><br>
 
-                                <p>Đã rất lâu trước đó, chúng tôi từng hi vọng sẽ có một ai đó ở Việt Nam viết ra những cuốn sách,
-                                những cuốn tạp chí liên quan đến thiết kế đồ họa. Nó phải thật đẹp, có nhiều thông tin hữu ích, để
+                                <p>Đã rất lâu trước đó, chúng tôi từng hi vọng sẽ có một ai đó ở Việt Nam viết ra những
+                                    cuốn sách,
+                                    những cuốn tạp chí liên quan đến thiết kế đồ họa. Nó phải thật đẹp, có nhiều thông
+                                    tin hữu ích, để
                                     chúng tôi có thêm cảm hứng, và có thứ để cầm, nắm.</p>
                                 <br>
-                                <p>Nhưng nhiều năm sau, chúng tôi vẫn không thấy họ xuất hiện. Vì vậy chúng tôi quyết định sẽ không
-                                chờ đợi nữa, mà sẽ bắt tay vào tự thực hiện chúng, thứ chúng tôi từng ao ước.</p>
+                                <p>Nhưng nhiều năm sau, chúng tôi vẫn không thấy họ xuất hiện. Vì vậy chúng tôi quyết
+                                    định sẽ không
+                                    chờ đợi nữa, mà sẽ bắt tay vào tự thực hiện chúng, thứ chúng tôi từng ao ước.</p>
                                 <br>
-                                <p>Hi vọng lớn nhất, từ chúng tôi, là bạn có được thật nhiều điều thú vị khi đọc cuốn tạp chí này.
-                                    Mọi sự giúp đỡ, ủng hộ từ bạn, dù là nhỏ nhất, đều là động lực để chúng tôi cố gắng làm tốt hơn
+                                <p>Hi vọng lớn nhất, từ chúng tôi, là bạn có được thật nhiều điều thú vị khi đọc cuốn
+                                    tạp chí này.
+                                    Mọi sự giúp đỡ, ủng hộ từ bạn, dù là nhỏ nhất, đều là động lực để chúng tôi cố gắng
+                                    làm tốt hơn
                                     trong mỗi số của tạp chí.
                                 </p>
                                 <br>
@@ -159,33 +167,36 @@
         <div class="row">
             @foreach($books as $book)
                 <div class="col-md-6 book-item">
-                    <div class="card card-profile" style="border-radius:0">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <div class="card card-profile card-plain">
-                                        <img class="card-book-image" src="{{$book['avatar']}}">
+                    <div class="card card-profile" style="border-radius:0; height: 90%">
+                        <div class="flex flex-col flex-justify-content-space-between" style="height: 100%">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="card card-profile card-plain">
+                                            <img class="card-book-image" src="{{$book['avatar']}}">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8 text-left">
+                                        <br>
+                                        <h5 style="font-weight:600">{{$book['name']}}</h5>
+                                        <h6>{{$book['short_description']}}</h6><br>
+                                        <p>{{$book['description']}}</p>
+                                        <h5>{{currency_vnd_format($book['price'])}}</h5><br>
                                     </div>
                                 </div>
-                                <div class="col-md-8 text-left">
-                                    <br>
-                                    <h5 style="font-weight:600">{{$book['name']}}</h5>
-                                    <h6>{{$book['short_description']}}</h6><br>
-                                    <p>{{$book['description']}}</p>
-                                    <h5>{{currency_vnd_format($book['price'])}}</h5><br>
-                                </div>
                             </div>
-                        </div>
-                        <div class="card-footer" style="border-top: 1px solid #dcdbdb!important;">
-                            <div style="display:flex;flex-direction:row;justify-content:space-between;">
-                                <div>
-                                    <a href="http://graphics.vn/book/{{$book['id']}}" class="btn btn-link btn-success">Xem
-                                        thêm</a>
-                                    {{--<button data-toggle="modal" data-target="#modalBuy" class="btn btn-sm btn-success">Đặt mua ngay <i class="fa fa-angle-right"></i></button>--}}
-                                    <button onclick="openModalBuy({{$book["id"]}},{{$book['price']}})"
-                                            class="btn btn-sm btn-success">Đặt
-                                        mua ngay <i
-                                                class="fa fa-angle-right"></i></button>
+                            <div class="card-footer" style="border-top: 1px solid #dcdbdb!important;">
+                                <div style="display:flex;flex-direction:row;justify-content:space-between;">
+                                    <div>
+                                        <a href="http://graphics.vn/book/{{$book['id']}}"
+                                           class="btn btn-link btn-success">Xem
+                                            thêm</a>
+                                        {{--<button data-toggle="modal" data-target="#modalBuy" class="btn btn-sm btn-success">Đặt mua ngay <i class="fa fa-angle-right"></i></button>--}}
+                                        <button onclick="openModalBuy({{$book["id"]}},{{$book['price']}})"
+                                                class="btn btn-sm btn-success">Đặt
+                                            mua ngay <i
+                                                    class="fa fa-angle-right"></i></button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -216,10 +227,10 @@
                                 </a>
                             </div>
                             <p class="card-description text-center">
-                                <h4><b>Huyền Thanh</b></h4><br>
-                                A group becomes a team when each member is sure enough of himself and his contribution
-                                to praise the skill of the others. No one can whistle a symphony. It takes an orchestra
-                                to play it.
+                            <h4><b>Huyền Thanh</b></h4><br>
+                            A group becomes a team when each member is sure enough of himself and his contribution
+                            to praise the skill of the others. No one can whistle a symphony. It takes an orchestra
+                            to play it.
                             </p>
                         </div>
                         <div class="card-footer text-center">
@@ -241,10 +252,10 @@
                                 </a>
                             </div>
                             <p class="card-description text-center">
-                                <h4><b>Hoàng Hiệp</b></h4><br>
-                                The strength of the team is each individual member. The strength of each member is the
-                                team. If you can laugh together, you can work together, silence isn’t golden, it’s
-                                deadly.
+                            <h4><b>Hoàng Hiệp</b></h4><br>
+                            The strength of the team is each individual member. The strength of each member is the
+                            team. If you can laugh together, you can work together, silence isn’t golden, it’s
+                            deadly.
                             </p>
                         </div>
                         <div class="card-footer text-center">
@@ -268,8 +279,8 @@
                             <p class="card-description text-center">
                             <h4><b>Hương Phan</b></h4><br>
                             Great teams do not hold back with one another. They are unafraid to air their dirty
-                                laundry. They admit their mistakes, their weaknesses and their concerns without fear of
-                                reprisal.
+                            laundry. They admit their mistakes, their weaknesses and their concerns without fear of
+                            reprisal.
                             </p>
                         </div>
                         <div class="card-footer text-center">
