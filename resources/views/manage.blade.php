8a1a1e856e8b9adcbd0e2cff47fbb1f5ca7b41ef
Cao Anh Quan <aquancva@gmail.com>
2017-10-23 23:15:54 +0700

    up

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -23,7 +23,7 @@
     <link rel="stylesheet" type="text/css"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"/>
 
-    <link href="manage/main.15e88f30bd12bc3b067baaee1634845b.css" rel="stylesheet">
+    <link href="manage/main.61c8f7da08a5336ec6601ba1ed21a6df.css" rel="stylesheet">
 
     <link href="{{url('config.css')}}" rel="stylesheet">
 </head>
@@ -74,7 +74,7 @@
 
 <script src="{{url('config.js')}}"></script>
 
-<script type="text/javascript" src="manage/main.213a3c0d284868e62e85.js"></script>
+<script type="text/javascript" src="manage/main.6efe0fb637641e08afb1.js"></script>
 
 <!-- Material Dashboard javascript methods -->
 <script src="http://d1j8r0kxyu9tj8.cloudfront.net/libs/material/assets/js/material-dashboard.js"></script>
