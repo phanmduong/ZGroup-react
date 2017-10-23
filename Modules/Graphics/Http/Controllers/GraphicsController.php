141e4c8e81b2741939de7cd17b5792656cbf4a6f
Cao Anh Quan <aquancva@gmail.com>
2017-10-23 15:43:53 +0700

    up

--- Modules/Graphics/Http/Controllers/GraphicsController.php
+++ Modules/Graphics/Http/Controllers/GraphicsController.php
@@ -147,7 +147,7 @@ class GraphicsController extends Controller
         $totalPrice = 0;
 
         foreach ($goods as $good) {
-            $totalPrice += $good->price * $good["coupon_value"] * $good->number;
+            $totalPrice += $good->price * (1 - $good["coupon_value"]) * $good->number;
         }
         $data = [
             "books" => $goods,
