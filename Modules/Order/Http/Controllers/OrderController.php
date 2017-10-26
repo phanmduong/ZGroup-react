b26dc5a9ab6da3d347f8cbea69f40e2bf8a49a06
datvithanh98 <datvithanh98@gmail.com>
2017-10-26 23:34:05 +0700

    up

--- Modules/Order/Http/Controllers/OrderController.php
+++ Modules/Order/Http/Controllers/OrderController.php
@@ -169,7 +169,7 @@ class OrderController extends ManageApiController
                 'id' => $importOrder->id,
                 'code' => $importOrder->code,
                 'status' => $importOrder->status,
-                'created_at' => $importOrder->format_vn_short_datetime(created_at),
+                'created_at' => format_vn_short_datetime($importOrder->created_at),
                 'import_price' => $importOrder->import_price,
                 'warehouse_id' => $importOrder->warehouse_id,
                 'total_money' => $total_money,
