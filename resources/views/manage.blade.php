4f504d751b4df33e00f79c0692bb49a7072be7ab
Cao Anh Quan <aquancva@gmail.com>
2017-10-31 12:16:27 +0700

    up

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -74,7 +74,7 @@
 
 <script src="{{url('config.js')}}"></script>
 
-<script type="text/javascript" src="manage/main.7bd9b8ae3a266985dd3f.js"></script>
+<script type="text/javascript" src="manage/main.a6c384495d12341c7162.js"></script>
 
 <!-- Material Dashboard javascript methods -->
 <script src="http://d1j8r0kxyu9tj8.cloudfront.net/libs/material/assets/js/material-dashboard.js"></script>
