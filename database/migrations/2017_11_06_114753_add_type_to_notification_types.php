0e67ecab1c87ba015e6f2d43e9736a37487c2719
Cao Anh Quan <aquancva@gmail.com>
2017-11-06 12:02:13 +0700

    fix db

--- database/migrations/2017_11_06_114753_add_type_to_notification_types.php
+++ database/migrations/2017_11_06_114753_add_type_to_notification_types.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangeGoodsTable1 extends Migration
+class AddTypeToNotificationTypes extends Migration
 {
     /**
      * Run the migrations.
@@ -12,9 +12,8 @@ class ChangeGoodsTable1 extends Migration
      */
     public function up()
     {
-        //
-        Schema::table('goods', function (Blueprint $table){
-            $table->string('status')->nullable()->change();
+        Schema::table('notification_types', function (Blueprint $table) {
+            $table->string('type')->nullable();
         });
     }
 
