7a53e5618ec7802a0f9916e37f248fad8cbe5236
Batman Bruce Wayne <taideptrai02@gmail.com>
2017-11-12 17:21:32 +0700

    them man hinh moi

---
+++ src/modules/inventoryGood/InventoryGoodContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class InventoryGoodContainer extends React.Component{
+    constructor(props,context){
+        super(props,context);
+    }
+}
+export default InventoryGoodContainer;
