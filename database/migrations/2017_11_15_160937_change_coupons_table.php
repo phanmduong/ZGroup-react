7fcfc7d9dc808b97a102783bf865223f3c79303c
thanhphamtt <thanhpham1479@gmail.com>
2017-11-15 18:54:48 +0700

    create coupon table

--- database/migrations/2017_11_15_160937_change_coupons_table.php
+++ database/migrations/2017_11_15_160937_change_coupons_table.php
@@ -13,6 +13,19 @@ class ChangeCouponsTable extends Migration
     public function up()
     {
         //
+        Schema::table('coupons', function (Blueprint $table) {
+            $table->string('coupon');
+            $table->string('discount_type');
+            $table->integer('discount_value');
+            $table->string('used_for');
+            $table->integer('order_value');
+            $table->integer('category_id')->unsigned()->index();
+            $table->integer('good_id')->unsigned()->index();
+            $table->integer('customer_id')->unsigned()->index();
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->softDeletes();
+        });
     }
 
     /**
