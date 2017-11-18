eba75a3a4ad7d783ae0a7c4226798dffc9d3c0b5
datvithanh98 <datvithanh98@gmail.com>
2017-11-18 09:59:16 +0700

    up

--- Modules/Order/Http/Controllers/ImportApiController.php
+++ Modules/Order/Http/Controllers/ImportApiController.php
@@ -26,12 +26,11 @@ class ImportApiController extends ManageApiController
 
         $importOrders = Order::where('type', 'import');
         if($keyword) {
-            dd($keyword);
             $userIds = User::where(function ($query) use ($keyword){
                 $query->where('name', 'like', "%$keyword%")->orWhere('phone', 'like', "%$keyword%")->orWhere('email', 'like', "%$keyword%");
             })->select('id')->get();
             $importOrders = $importOrders->whereIn('user_id', $userIds);
-            $importOrders = $importOrders->where("code", "like", "%$keyword%");
+            $importOrders = $importOrders->where('code', 'like', "%$keyword%");
         }
 
         if($staff_id)
