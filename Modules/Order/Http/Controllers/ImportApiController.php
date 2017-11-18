1c4434073c001f40b6757bba155b22047321c3b4
datvithanh98 <datvithanh98@gmail.com>
2017-11-18 09:47:53 +0700

    up

---
+++ Modules/Order/Http/Controllers/ImportApiController.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Modules\Order\Http\Controllers;
+
+use App\Good;
+use App\Http\Controllers\ManageApiController;
+use App\ImportedGoods;
+use App\User;
+use Illuminate\Http\Request;
+use App\Order;
+
+class ImportApiController extends ManageApiController
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function allImportOrders(Request $request)
+    {
+        $startTime = $request->start_time;
+        $endTime = $request->end_time;
+        $status = $request->status;
+        $importOrders = Order::where('type', 'import');
+        $keyword = $request->search;
+        $staff_id = $request->staff_id;
+
+        if($keyword) {
+            $userIds = User::where(function ($query) use ($keyword){
+                $query->where('name', 'like', "%$keyword%")->orWhere('phone', 'like', "%$keyword%")->orWhere('email', 'like', "%$keyword%");
+            })->select('id')->get();
+            $importOrders = $importOrders->whereIn('user_id', $userIds);
+            $importOrders = $importOrders->where('code', 'like', "%$keyword%");
+        }
+
+        if($staff_id)
+            $importOrders = $importOrders->where('staff_id', $staff_id);
+        if ($startTime)
+            $importOrders = $importOrders->whereBetween('created_at', array($startTime, $endTime));
+        if ($status)
+            $importOrders = $importOrders->where('status', $status);
+        $importOrders = $importOrders->orderBy('created_at', 'desc');
+
+        $data = $importOrders->map(function ($importOrder) {
+            $total_money = $importOrder->importedGoods->reduce(function ($total, $importedGood) {
+                return $total + $importedGood->quantity * $importedGood->import_price;
+            }, 0);
+            $total_quantity = $importOrder->importedGoods->reduce(function ($total, $importedGood) {
+                return $total + $importedGood->quantity;
+            }, 0);
+            $debt = $total_money - $importOrder->orderPaidMoneys->reduce(function ($total, $orderPaidMoney) {
+                    return $total + $orderPaidMoney->money;
+                }, 0);
+            $importOrderData = [
+                'id' => $importOrder->id,
+                'code' => $importOrder->code,
+                'status' => $importOrder->status,
+                'created_at' => format_vn_short_datetime(strtotime($importOrder->created_at)),
+                'import_price' => $importOrder->import_price,
+                'warehouse_id' => $importOrder->warehouse_id,
+                'total_money' => $total_money,
+                'total_quantity' => $total_quantity,
+                'debt' => $debt,
+            ];
+            if (isset($importOrder->staff)) {
+                $staff = [
+                    'id' => $importOrder->staff->id,
+                    'name' => $importOrder->staff->name,
+                ];
+                $importOrderData['staff'] = $staff;
+            }
+            if (isset($importOrder->user)) {
+                $user = [
+                    'id' => $importOrder->user->id,
+                    'name' => $importOrder->user->name,
+                ];
+                $importOrderData['user'] = $user;
+            }
+            return $importOrderData;
+        });
+
+        return $this->respondSuccessWithStatus([
+            'import_orders' => $data
+        ]);
+    }
+
+    public function detailedImportOrder($importOrderId)
+    {
+        $importOrder = Order::find($importOrderId);
+        $total_money = $importOrder->importedGoods->reduce(function ($total, $importedGood) {
+            return $total + $importedGood->quantity * $importedGood->import_price;
+        }, 0);
+        $total_quantity = $importOrder->importedGoods->reduce(function ($total, $importedGood) {
+            return $total + $importedGood->quantity;
+        }, 0);
+        $debt = $total_money - $importOrder->orderPaidMoneys->reduce(function ($total, $orderPaidMoney) {
+                return $total + $orderPaidMoney->money;
+            }, 0);
+        $data = [
+            'id' => $importOrder->id,
+            'name' => $importOrder->name,
+            'code' => $importOrder->code,
+            'created_at' => format_vn_short_datetime(strtotime($importOrder->created_at)),
+            'note' => $importOrder->note,
+            'total_money' => $total_money,
+            'total_quantity' => $total_quantity,
+            'debt' => $debt,
+        ];
+        $data['imported_goods'] = $importOrder->importedGoods->map(function ($importedGood) {
+            return [
+                'name' => $importedGood->good->name,
+                'code' => $importedGood->good->code,
+                'quantity' => $importedGood->quantity,
+                'import_price' => $importedGood->import_price
+            ];
+        });
+        $data['order_paid_money'] = $importOrder->orderPaidMoneys->map(function ($orderPaidMoney) {
+            return [
+                'id' => $orderPaidMoney->id,
+                'money' => $orderPaidMoney->money,
+                'note' => $orderPaidMoney->note,
+            ];
+        });
+        if (isset($importOrder->user)) {
+            $user = [
+                'id' => $importOrder->user->id,
+                'name' => $importOrder->user->name,
+            ];
+            $data['user'] = $user;
+        }
+        return $this->respondSuccessWithStatus([
+            'import_order' => $data,
+        ]);
+    }
+
+    public function addImportedGood(Request $request)
+    {
+        $importedGood = new ImportedGoods;
+        if ($request->order_import_id == null || Order::find($request->order_import_id) == null)
+            return $this->respondErrorWithStatus([
+                'message' => 'Không tồn tại đơn nhập hàng'
+            ]);
+        if ($request->good_id == null || Good::find($request->good_id) == null)
+            return $this->respondErrorWithStatus([
+                'message' => 'Không tồn tại sản phẩm'
+            ]);
+        if ($request->quantity == null)
+            return $this->respondErrorWithStatus([
+                'message' => 'Thiếu số lượng hàng'
+            ]);
+        if ($request->import_price == null)
+            return $this->respondErrorWithStatus([
+                'message' => 'Thiếu giá nhập hàng'
+            ]);
+        $importedGood->order_import_id = $request->order_import_id;
+        $importedGood->good_id = $request->good_id;
+        $importedGood->quantity = $request->quantity;
+        $importedGood->import_price = $request->import_price;
+        $importedGood->staff_id = Order::find($request->order_import_id)->staff->id;
+        $importedGood->warehouse_id = Order::find($request->order_import_id)->warehouse->id;
+        $importedGood->save();
+        return $this->respondSuccessWithStatus([
+            'message' => 'SUCCESS'
+        ]);
+    }
+}
--- Modules/Order/Http/routes.php
+++ Modules/Order/Http/routes.php
@@ -4,31 +4,33 @@ Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => 'orde
     Route::get('/all-orders', 'OrderController@allOrders');
     Route::put('/{order_id}/edit', 'OrderController@editOrder');
     Route::get('/{order_id}/info', 'OrderController@detailedOrder');
-    Route::get('/category/all', 'OrderController@allCategory');
-    Route::post('/category/add', 'OrderController@addCategory');
-    Route::put('/category/edit-category', 'OrderController@editCategory');
-    Route::delete('category/{category_id}/delete', 'OrderController@deleteCategory');
-    Route::get('/import-orders', 'OrderController@allImportOrders');
-    Route::get('/detailed-import-order/{importOrderId}', 'OrderController@detailedImportOrder');
-    Route::post('/add-import-order', 'OrderController@addImportOrder');
-    Route::post('/add-imported-good', 'OrderController@addImportOrder');
-    Route::post('/add-import-order-goods', 'OrderController@addImportOrderGoods');
     Route::post('/pay-order/{orderId}', 'OrderController@payOrder');
-    Route::post('/add-supplier', 'OrderController@addSupplier');
-    Route::get('/all-suppliers', 'OrderController@allSuppliers');
-    Route::get('/all-warehouses', 'OrderController@getWarehouses');
-    Route::get('/warehouses/all', 'OrderController@allWarehouses');
-    Route::post('/warehouse/add', 'OrderController@addWarehouse');
-    Route::put('/warehouse/{warehouseId}/edit', 'OrderController@editWarehouse');
-    Route::delete('/warehouse/{warehouseId}/delete', 'OrderController@deleteWarehouse');
-    Route::get('/bases/all', 'OrderController@allBases');
     Route::get('/all-order-paid-money', 'OrderController@getOrderPaidMoney');
-    Route::get('/warehouse/goods/{warehouseId}', 'OrderController@warehouseGoods');
     Route::post('/check-goods', 'OrderController@checkGoods');
+
+    Route::get('/category/all', 'CategoryApiController@allCategory');
+    Route::post('/category/add', 'CategoryApiController@addCategory');
+    Route::put('/category/edit-category', 'CategoryApiController@editCategory');
+    Route::delete('category/{category_id}/delete', 'CategoryApiController@deleteCategory');
+
+    Route::get('/import-orders', 'ImportApiController@allImportOrders');
+    Route::get('/detailed-import-order/{importOrderId}', 'ImportApiController@detailedImportOrder');
+    Route::post('/add-import-order-goods', 'ImportApiController@addImportOrderGoods');
+
+    Route::post('/add-supplier', 'WarehouseApiController@addSupplier');
+    Route::get('/all-suppliers', 'WarehouseApiController@allSuppliers');
+    Route::get('/all-warehouses', 'WarehouseApiController@getWarehouses');
+    Route::get('/warehouses/all', 'WarehouseApiController@allWarehouses');
+    Route::post('/warehouse/add', 'WarehouseApiController@addWarehouse');
+    Route::put('/warehouse/{warehouseId}/edit', 'WarehouseApiController@editWarehouse');
+    Route::delete('/warehouse/{warehouseId}/delete', 'WarehouseApiController@deleteWarehouse');
+    Route::get('/bases/all', 'WarehouseApiController@allBases');
+    Route::get('/warehouse/goods/{warehouseId}', 'WarehouseApiController@warehouseGoods');
+
     Route::get('/all-customers', 'CustomerController@allCustomers');
     Route::get('total-and-debt-money', 'CustomerController@countMoney');
     Route::post('add-customer', 'CustomerController@addCustomer');
-    //Route::delete('delete-customer','CustomerController@deleteCustomer');
+
     Route::get('/staffs', 'StaffController@getStaffs');
 });
 
