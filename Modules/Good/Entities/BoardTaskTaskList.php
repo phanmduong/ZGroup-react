a43a41a13a8cda25289fcf58b690a8a0e6eb20ba
Cao Anh Quan <aquancva@gmail.com>
2017-11-20 21:36:04 +0700

    up

--- Modules/Good/Entities/BoardTaskTaskList.php
+++ Modules/Good/Entities/BoardTaskTaskList.php
@@ -2,9 +2,21 @@
 
 namespace Modules\Good\Entities;
 
+use App\Board;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Task\Entities\TaskList;
 
 class BoardTaskTaskList extends Model
 {
     protected $table = "board_tasks";
+
+    public function board()
+    {
+        return $this->belongsToMany(Board::class);
+    }
+
+    public function taskList()
+    {
+        return $this->belongsToMany(TaskList::class);
+    }
 }
