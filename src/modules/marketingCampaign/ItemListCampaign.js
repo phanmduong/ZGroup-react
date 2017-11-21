6816adbc4dac354a62b2b7fc3d19912c7fbbd2ac
phanmduong <phanminhduong@hotmail.com>
2017-11-21 10:24:19 +0700

    update code

--- src/modules/marketingCampaign/ItemListCampaign.js
+++ src/modules/marketingCampaign/ItemListCampaign.js
@@ -3,7 +3,6 @@ import ButtonGroupAction from "../../components/common/ButtonGroupAction";
 import ReactSelect from "react-select";
 import {BASE_URL} from "../../constants/env";
 import * as helper from "../../helpers/helper";
-import {browserHistory} from "react-router";
 
 class ItemListCampaign extends React.Component {
     constructor(props, context) {
@@ -26,10 +25,6 @@ class ItemListCampaign extends React.Component {
         if (helper.isEmptyInput(this.state.selectedCourse)) {
             helper.showTypeNotification("Vui lòng chọn khóa học", "info");
         }
-        //
-        //
-        // console.log(url);
-        // browserHistory.push(url);
     }
 
     render() {
@@ -60,7 +55,11 @@ class ItemListCampaign extends React.Component {
                             </a>
                     }
                 </td>
-                <td><ButtonGroupAction/></td>
+                <td><ButtonGroupAction
+                    object={this.props.campaign}
+                    edit={this.props.openModalStoreCampaign}
+                    disabledDelete
+                /></td>
             </tr>
         );
     }
