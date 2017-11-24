97e41a1995d2adf2135fd2cfe792be19fffa846f
datvithanh98 <datvithanh98@gmail.com>
2017-11-24 11:40:52 +0700

    up

--- Modules/Coupon/Http/Controllers/CouponController.php
+++ Modules/Coupon/Http/Controllers/CouponController.php
@@ -2,71 +2,13 @@
 
 namespace Modules\Coupon\Http\Controllers;
 
+use App\Http\Controllers\ManageApiController;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Routing\Controller;
 
-class CouponController extends Controller
+class CouponController extends ManageApiController
 {
-    /**
-     * Display a listing of the resource.
-     * @return Response
-     */
-    public function index()
+    public function createCoupon(Request $request)
     {
-        return view('coupon::index');
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Response
-     */
-    public function create()
-    {
-        return view('coupon::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     * @param  Request $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-    }
-
-    /**
-     * Show the specified resource.
-     * @return Response
-     */
-    public function show()
-    {
-        return view('coupon::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @return Response
-     */
-    public function edit()
-    {
-        return view('coupon::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param  Request $request
-     * @return Response
-     */
-    public function update(Request $request)
-    {
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @return Response
-     */
-    public function destroy()
-    {
     }
 }
--- Modules/Coupon/Http/routes.php
+++ Modules/Coupon/Http/routes.php
@@ -2,5 +2,5 @@
 
 Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => 'coupon', 'namespace' => 'Modules\Coupon\Http\Controllers'], function()
 {
-    Route::get('/', 'CouponController@index');
+    Route::post('/create', 'CouponController@createCoupon');
 });
