ee1ab6b7b425fa66888e8301f0b9036240346456
Cao Anh Quan <aquancva@gmail.com>
2017-11-24 13:51:07 +0700

    rework addPropertyItems

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -74,7 +74,7 @@
 
 <script src="{{url('config.js')}}"></script>
 
-<script type="text/javascript" src="manage/main.d23f4a86bdae983eea53.js"></script>
+<script type="text/javascript" src="manage/main.02e6202da4898ab1d8da.js"></script>
 
 <!-- Material Dashboard javascript methods -->
 <script src="http://d1j8r0kxyu9tj8.cloudfront.net/libs/material/assets/js/material-dashboard.js"></script>
