c4f85d108a18cad6452e735ac718a2ed63bb5438
datvithanh98 <datvithanh98@gmail.com>
2017-11-25 11:07:44 +0700

    up

--- Modules/Course/Http/Controllers/CourseController.php
+++ Modules/Course/Http/Controllers/CourseController.php
@@ -154,9 +154,7 @@ class CourseController extends ManageApiController
             $link->link_icon = $link_icon;
             $link->link_icon_url = $this->s3_url . $link_icon;
         } else {
-            if ($link->link_icon_url == null) {
-                $link->link_icon_url = 'https://placehold.it/800x600';
-            }
+            $link->link_icon_url = trim($request->link_icon_url) ? trim($request->link_icon_url) : 'https://placehold.it/800x600';
         }
         $link->save();
         return $this->respondSuccessWithStatus([
