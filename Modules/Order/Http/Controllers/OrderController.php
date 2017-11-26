2f36839ae56c32b19a88d63435ee875bf8f506be
datvithanh98 <datvithanh98@gmail.com>
2017-11-26 17:23:57 +0700

    up

--- Modules/Order/Http/Controllers/OrderController.php
+++ Modules/Order/Http/Controllers/OrderController.php
@@ -79,7 +79,6 @@ class OrderController extends ManageApiController
     public function editOrder($order_id, Request $request)
     {
         $order = Order::find($order_id);
-        dd($request->code);
         if ($request->code == null && trim($request->code) == '')
             return $this->respondErrorWithStatus([
                 'message' => 'Thiếu code'
