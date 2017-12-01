0c24cf3f094b3100971f6f7a221b2f0f5d580a3d
phanmduong <phanminhduong@hotmail.com>
2017-12-01 14:35:56 +0700

    update code

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -74,7 +74,7 @@
 
 <script src="{{url('config.js')}}"></script>
 
-<script type="text/javascript" src="manage/main.f4ffcfc20d0a4a06a553.js"></script>
+<script type="text/javascript" src="manage/main.34acb49808862a534933.js"></script>
 
 <!-- Material Dashboard javascript methods -->
 <script src="http://d1j8r0kxyu9tj8.cloudfront.net/libs/material/assets/js/material-dashboard.js"></script>
