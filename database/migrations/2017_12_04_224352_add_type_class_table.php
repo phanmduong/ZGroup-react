553d74d01ce4718a2a523f7bf927103e848aae34
phanmduong <phanminhduong@hotmail.com>
2017-12-04 23:20:08 +0700

    update code

---
+++ database/migrations/2017_12_04_224352_add_type_class_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTypeClassTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('classes', function (Blueprint $table) {
+            $table->string('type')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('classes', function (Blueprint $table) {
+            //
+        });
+    }
+}
