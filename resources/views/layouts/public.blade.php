ce4cfbf673b9de100a421f16ffa89d0fcd2d5776
Cao Anh Quan <aquancva@gmail.com>
2017-12-07 13:35:16 +0700

    up

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -66,15 +66,6 @@
         gtag('js', new Date());
 
         gtag('config', 'AW-923433004');
-    </script>
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-110883203-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
         gtag('config', 'UA-110883203-1');
     </script>
 
