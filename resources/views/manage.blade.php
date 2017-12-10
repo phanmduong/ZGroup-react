21e8c339702540406b3cf2695c474be2d8d9fa3e
Cao Anh Quan <aquancva@gmail.com>
2017-12-10 14:28:25 +0700

    up

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -75,7 +75,7 @@
 <script src="{{url('config.js')}}"></script>
 
 
-<script type="text/javascript" src="manage/main.0a6702a25db6215f0f9a.js"></script>
+<script type="text/javascript" src="manage/main.536e4dd2a3f6a689906e.js"></script>
 
 
 <!-- Material Dashboard javascript methods -->
