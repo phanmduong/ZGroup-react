2a93685ba987aa293c1b3521aae0b007e1580c2e
phanmduong <phanminhduong@hotmail.com>
2017-12-11 15:44:17 +0700

    update code

--- app/Repositories/RoomRepository.php
+++ app/Repositories/RoomRepository.php
@@ -26,9 +26,9 @@ class RoomRepository
             return [
                 'id' => $room->id,
                 'name' => $room->name,
-                'base' => $room->base ? $room->base->name : "",
-                'address' => $room->base ? $room->base->address : "",
-                'base_id' => $room->base ? $room->base->id : ""
+                'base' => $room->base()->withTrashed()->get()->name,
+                'address' => $room->base()->withTrashed()->get()->address,
+                'base_id' => $room->base()->withTrashed()->get()->id
             ];
     }
 }
