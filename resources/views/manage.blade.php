e85c3c6701ab9e9694d74bc21f89860c5c99c6c1
phanmduong <phanminhduong@hotmail.com>
2017-12-12 18:23:03 +0700

    update codE

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -23,7 +23,7 @@
     <link rel="stylesheet" type="text/css"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"/>
 
-    <link href="manage/main.cb91a1a7932ba0c4e4a3508519fca995.css" rel="stylesheet">
+    <link href="manage/main.165dbfa35077cde2d51d090c2245ac3b.css" rel="stylesheet">
 
     <link href="{{url('config.css')}}" rel="stylesheet">
 </head>
@@ -74,7 +74,7 @@
 
 <script src="{{url('config.js')}}"></script>
 
-<script type="text/javascript" src="manage/main.01595c12cd4414094265.js"></script>
+<script type="text/javascript" src="manage/main.4bb49aad3c699076781a.js"></script>
 
 <!-- Material Dashboard javascript methods -->
 <script src="http://d1j8r0kxyu9tj8.cloudfront.net/libs/material/assets/js/material-dashboard.js"></script>
