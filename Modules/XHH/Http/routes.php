de5c638f5ec884af891f423ccc550630136dc1bb
datvithanh98 <datvithanh98@gmail.com>
2017-12-14 16:12:59 +0700

    up

--- Modules/XHH/Http/routes.php
+++ Modules/XHH/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => 'web', 'domain' => "alibaba.{subfix}", 'namespace' => 'Modules\XHH\Http\Controllers'], function () {
+Route::group(['middleware' => 'web', 'domain' => "54.169.191.45", 'namespace' => 'Modules\XHH\Http\Controllers'], function () {
     Route::get('/', 'XHHController@index');
     Route::get('/blog', 'XHHController@blog');
     Route::get('/blog/post/{post_id}', 'XHHController@post');
