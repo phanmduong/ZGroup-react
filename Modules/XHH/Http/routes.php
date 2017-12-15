d8da9027d5e04d22d7468498db21ac37f0e44960
datvithanh98 <datvithanh98@gmail.com>
2017-12-15 11:38:35 +0700

    up

--- Modules/XHH/Http/routes.php
+++ Modules/XHH/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => 'web', 'domain' => "54.169.191.45", 'namespace' => 'Modules\XHH\Http\Controllers'], function () {
+Route::group(['middleware' => 'web', 'domain' => "quanca.net", 'namespace' => 'Modules\XHH\Http\Controllers'], function () {
     Route::get('/', 'XHHController@index');
     Route::get('/blog', 'XHHController@blog');
     Route::get('/blog/post/{post_id}', 'XHHController@post');
