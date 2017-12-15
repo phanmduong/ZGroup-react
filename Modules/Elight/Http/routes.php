193147b90ed8c6cb60b111e864e3f771cf83a5af
datvithanh98 <datvithanh98@gmail.com>
2017-12-15 16:59:21 +0700

    up

--- Modules/Elight/Http/routes.php
+++ Modules/Elight/Http/routes.php
@@ -1,11 +1,11 @@
 <?php
 
-Route::group(['middleware' => 'web', 'domain' => "keetool3.{subfix}", 'namespace' => 'Modules\Elight\Http\Controllers'], function () {
+Route::group(['middleware' => 'web', 'domain' => "alibaba.{subfix}", 'namespace' => 'Modules\Elight\Http\Controllers'], function () {
     Route::get('/', 'ElightController@index');
     Route::get('/blog', 'ElightController@blog');
     Route::get('/about-us', 'ElightController@aboutUs');
     Route::get('/contact-us', 'ElightController@contactUs');
     Route::get('/all-books', 'ElightController@allBooks');
     Route::get('/blog/post/{post_id}', 'ElightController@post');
-    Route::get('/book/{book_id}', 'ElightController@book');
+    Route::get('/sach/{book_id}', 'ElightController@book');
 });
