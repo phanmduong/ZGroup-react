7e4b1e08727b5a60c56439726762416c99400c4c
Cao Anh Quan <aquancva@gmail.com>
2017-12-18 16:37:01 +0700

    up

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class ClientController extends Controller
+{
+
+    public function __construct()
+    {
+    }
+
+    public function email($path)
+    {
+        return view("client.email");
+    }
+
+}
