aa78aa6679e156ef7a472cd70f2735d7ebcb63c1
datvithanh98 <datvithanh98@gmail.com>
2017-12-18 15:32:30 +0700

    up

--- public/js/nhatquangshop.js
+++ public/js/nhatquangshop.js
@@ -73,6 +73,9 @@ var modalBuy = new Vue({
             $('#modalBuy').modal('hide');
             $('#modalPurchase').modal("show");
             $("body").css("overflow", "hidden");
+            modalPurchase.loadingProvince = true;
+            modalPurchase.showProvince = false;
+            modalPurchase.openModal();
         },
     }
 });
@@ -117,27 +120,51 @@ var modalPurchase = new Vue({
         email: '',
         address: '',
         payment: '',
+        provinceid: '',
+        districtid: '',
+        wardid: '',
+        loadingProvince: false,
+        showProvince: false,
+        loadingDistrict: false,
+        showDistrict: false,
         provinces: [],
         districts: [],
-        wards: [],
     },
     methods: {
-        getDistricts: function () {
-            axios.get(window.url + '/load-books-from-session/v2')
+        getProvinces: function () {
+            axios.get(window.url + '/province')
                 .then(function (response) {
-                    this.goods = response.data.goods;
-                    this.total_price = response.data.total_price;
-                    this.isLoading = false;
+                    this.provinces = response.data.provinces;
+                    this.loadingProvince = false;
+                    this.showProvince = true;
                 }.bind(this))
                 .catch(function (error) {
 
                 });
         },
+        getDistricts: function () {
+            axios.get(window.url + '/district/' + this.provinceid)
+                .then(function (response) {
+                    this.districts = response.data.districts;
+                    this.loadingDistrict = false;
+                    this.showDistrict = true;
+                }.bind(this))
+                .catch(function (error) {
+
+                });
+        },
+        openModal: function () {
+            this.getProvinces();
+        },
+        changeProvince: function () {
+            this.loadingDistrict = true;
+            this.getDistricts();
+            console.log(this.provinceid);
+        },
         submitOrder: function () {
             $("#purchase-error").css("display", "none");
             $("#btn-purchase-group").css("display", "none");
             $("#purchase-loading-text").css("display", "block");
-
             if (!this.name || !this.phone || !this.email || !this.address || !this.payment) {
                 alert("Bạn vui lòng nhập đủ thông tin và kiểm tra lại email");
                 $("#purchase-error").css("display", "block");
@@ -150,9 +177,11 @@ var modalPurchase = new Vue({
                 name: this.name,
                 phone: this.phone,
                 email: this.email,
+                provinceid: this.provinceid ? this.provinceid : '01',
+                districtid: this.districtid ? this.districtid : '001',
                 address: this.address,
                 payment: this.payment,
-                _token: window.token
+                _token: window.token,
             })
                 .then(function (response) {
                     $("#purchase-loading-text").css("display", "none");
