57b6271b7b2c3e85cc6c81f3461a4b414cd6acdc
phanmduong <phanminhduong@hotmail.com>
2017-12-19 19:28:21 +0700

    update code

---
+++ resources/views/client/teaching.blade.php
@@ -0,0 +1,9 @@
+@extends("client.layout.master")
+
+@section("css")
+    <link href="manage/teaching/main.8beb53522833d876fdbefef05a414858.css" rel="stylesheet">
+@endsection
+
+@section("js")
+    <script type="text/javascript" src="manage/teaching/main.73717da66eabcd6cf9ac.js"></script>
+@endsection
