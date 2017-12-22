9229b085b348b172b0a3f147920456a2f4394897
Cao Anh Quan <aquancva@gmail.com>
2017-12-22 13:42:22 +0700

    fix start script

--- src/routes/renderer/RouteRenderer.js
+++ src/routes/renderer/RouteRenderer.js
@@ -5,20 +5,23 @@ import {IndexRoute, Route} from "react-router";
 import LoginContainer from "../../modules/login/LoginContainer";
 
 export default (InputRoutes) => (
-    <Route path="/" component={AppContainer}>
-        {
-            InputRoutes && InputRoutes.map((route) => <Route {...route}>
-                {
-                    route.children && route.children.map((routeChild) => {
-                        if (routeChild.path === "/")
-                            return <IndexRoute component={routeChild.component}/>;
-                        return (
-                            <Route {...routeChild}/>
-                        );
-                    })
-                }
-            </Route>)
-        }
+    <Route>
+        <Route path="/" component={AppContainer}>
+            {
+                InputRoutes && InputRoutes.map((route) => <Route {...route}>
+                    {
+                        route.children && route.children.map((routeChild) => {
+                            if (routeChild.path === "/")
+                                return <IndexRoute component={routeChild.component}/>;
+                            return (
+                                <Route {...routeChild}/>
+                            );
+                        })
+                    }
+                </Route>)
+            }
+
+        </Route>
         <Route path="/login" component={LoginContainer}/>
         <Route path="*" component={NotFoundPage}/>
     </Route>
