b62232cba71d7d3de5cc66196aafaf34a62705fe
Cao Anh Quan <aquancva@gmail.com>
2017-12-24 16:43:24 +0700

    up

---
+++ Modules/Staff/Http/Controllers/StaffChartApiController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Modules\Staff\Http\Controllers;
+
+use App\Http\Controllers\ManageApiController;
+use App\User;
+use Faker\Provider\DateTime;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class StaffChartApiController extends ManageApiController
+{
+    /**
+     * GET /staff
+     * @return Response
+     */
+    public function countStaffCards(Response $request)
+    {
+        $from = $request->from;
+        $to = $request->to;
+        if ($request->to === null) {
+            $request->to = date();
+        }
+//        if ($request->from === null){
+//            $to->modify('-7 days');
+//            $request->from =  ;
+//        }
+        return $this->respondSuccessWithStatus([
+//            "user" => $user
+        ]);
+    }
+
+}
--- Modules/Staff/Http/routes.php
+++ Modules/Staff/Http/routes.php
@@ -1,6 +1,8 @@
 <?php
 
-Route::group([ 'prefix' => 'staff', 'namespace' => 'Modules\Staff\Http\Controllers'], function()
-{
+Route::group(['prefix' => 'staff', 'namespace' => 'Modules\Staff\Http\Controllers'], function () {
     Route::post('/', 'StaffApiController@createStaff');
+
+
+    Route::get("/{staffId}/chart/card","@countStaffCards");
 });
