a1a6dbe69411ebe9d45a95fe6d1f074afc599e60
phanmduong <phanminhduong@hotmail.com>
2017-12-25 23:25:04 +0700

    update code

--- Modules/XHH/Resources/views/layouts/master.blade.php
+++ Modules/XHH/Resources/views/layouts/master.blade.php
@@ -1,19 +1,19 @@
 <!doctype html>
 <html lang="en" class="no-js">
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
     <link rel="shortcut icon" type="image/png" href="" cph-ssorder="0">
     <link rel="icon" type="image/png" href="">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 
     <title>SOCIOLOGY HUE</title>
 
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+    <meta name="viewport" content="width=device-width"/>
 
-    <link href="https://d255zuevr6tr8p.cloudfront.net/landingpage/assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://d255zuevr6tr8p.cloudfront.net/landingpage/assets/css/bootstrap.min.css" rel="stylesheet"/>
     <link href="https://d255zuevr6tr8p.cloudfront.net/landingpage/assets/css/paper-kit.css" rel="stylesheet"/>
-    <link href="https://d255zuevr6tr8p.cloudfront.net/landingpage/assets/css/demo.css" rel="stylesheet" />
+    <link href="https://d255zuevr6tr8p.cloudfront.net/landingpage/assets/css/demo.css" rel="stylesheet"/>
 
     <!--     Fonts and icons     -->
     <link href='https://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
@@ -22,21 +22,11 @@
 
 </head>
 <body class="profile" style="background: #f2f2f2;">
-<nav class="navbar navbar-toggleable-md fixed-top bg-dark" style="height:35px; background:#272727!important">
-    <div class="container">
-        <div style="text-align:right; width:100%">
-            <button class="btn btn-google" style="padding:0px 5px;margin:3px;font-size:10px;">
-                <i class="fa fa-google"></i> Google Login
-            </button>
-            <button class="btn btn-facebook" style="padding:0px 5px;margin:3px;font-size:10px;">
-                <i class="fa fa-facebook"></i> Facebook Login
-            </button>
-        </div>
-    </div>
-</nav>
 <nav class="navbar navbar-toggleable-md fixed-top" style="margin-top:35px">
     <div class="container">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false"
+                aria-label="Toggle navigation">
             <span class="navbar-toggler-bar"></span>
             <span class="navbar-toggler-bar"></span>
             <span class="navbar-toggler-bar"></span>
@@ -59,6 +49,15 @@
         </div>
     </div>
 </nav>
+<div id="fb-root"></div>
+<script>(function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.11';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
 
 @yield('content')
 
@@ -141,7 +140,8 @@
                 <div class="copyright">
                     <div class="pull-left">
                         ©
-                        <script>document.write(new Date().getFullYear())</script>2017
+                        <script>document.write(new Date().getFullYear())</script>
+                        2017
                         KEE Agency
                     </div>
                     <div class="links pull-right">
@@ -182,13 +182,65 @@
 <script src="http://d1j8r0kxyu9tj8.cloudfront.net/libs/vue.min.js"></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <script src="/js/nhatquangshop.js?6868"></script>
-<div id="modalPurchase" class="modal fade" style="overflow-y: scroll;"><div class="modal-dialog modal-large"><div class="modal-content"><div class="modal-header"><button type="button" data-dismiss="modal" class="close">×</button> <h2 class="medium-title">Thanh toán</h2></div> <div class="modal-body"><form class="register-form "><h6>Họ và tên</h6> <input type="text" placeholder="Họ và tên" class="form-control"><br> <h6>Số điện thoại</h6> <input type="text" placeholder="Số điện thoại" class="form-control"><br> <h6>Email</h6> <input type="text" placeholder="Số điện thoại" class="form-control"><br> <h6>Địa chỉ nhận sách</h6> <input type="text" placeholder="Địa chỉ nhận sách" class="form-control"><br> <h6>Phương thức thanh toán</h6> <select id="sel1" class="form-control"><option value="Chuyển khoản">Chuyển khoản</option> <option value="Thanh toán trực tiếp khi nhận hàng(COD)">
+<div id="modalPurchase" class="modal fade" style="overflow-y: scroll;">
+    <div class="modal-dialog modal-large">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" data-dismiss="modal" class="close">×</button>
+                <h2 class="medium-title">Thanh toán</h2></div>
+            <div class="modal-body">
+                <form class="register-form "><h6>Họ và tên</h6> <input type="text" placeholder="Họ và tên"
+                                                                       class="form-control"><br> <h6>Số điện thoại</h6>
+                    <input type="text" placeholder="Số điện thoại" class="form-control"><br> <h6>Email</h6> <input
+                            type="text" placeholder="Số điện thoại" class="form-control"><br> <h6>Địa chỉ nhận sách</h6>
+                    <input type="text" placeholder="Địa chỉ nhận sách" class="form-control"><br> <h6>Phương thức thanh
+                        toán</h6> <select id="sel1" class="form-control">
+                        <option value="Chuyển khoản">Chuyển khoản</option>
+                        <option value="Thanh toán trực tiếp khi nhận hàng(COD)">
                             Thanh toán trực tiếp khi nhận hàng(COD)
-                        </option></select></form> <div id="purchase-error" style="display: none; color: red; padding: 10px; text-align: center;">
+                        </option>
+                    </select></form>
+                <div id="purchase-error" style="display: none; color: red; padding: 10px; text-align: center;">
                     Bạn vui lòng nhập đầy đủ thông tin
-                </div></div> <div class="modal-footer" style="display: block;"><div id="purchase-loading-text" style="display: none; text-align: center; width: 100%; padding: 15px;"><i class="fa fa-spin fa-spinner"></i>Đang tải...
-                </div> <div id="btn-purchase-group" style="text-align: right;"><button data-dismiss="modal" class="btn btn-link btn-google" style="width: auto !important;">Tiếp
-                        tục mua <i class="fa fa-angle-right"></i></button> <button class="btn btn-sm btn-google" style="margin: 10px 10px 10px 0px !important;">Thanh toán <i class="fa fa-angle-right"></i></button></div></div></div></div></div><div id="modalBuy" class="modal fade"><div class="modal-dialog modal-large"><div class="modal-content"><div class="modal-header"><button type="button" data-dismiss="modal" class="close">×</button> <h2 class="medium-title">Giỏ hàng</h2></div> <div id="modal-buy-body" class="modal-body"><br> <!---->  <hr> <div class="row"><div class="col-md-4"><h4 class="text-left"><b>Tổng</b></h4></div> <div class="col-md-8"><h4 class="text-right"><b>0</b></h4></div></div></div> <div class="modal-footer"><button data-toggle="modal" data-target="#modalBuy" class="btn btn-link btn-google" style="width: auto !important;">Tiếp tục mua <i class="fa fa-angle-right"></i></button> <button id="btn-purchase" class="btn btn-sm btn-google" style="margin: 10px 10px 10px 0px !important;">Thanh toán <i class="fa fa-angle-right"></i></button></div></div></div></div><div id="modalgoogle" class="modal fade">
+                </div>
+            </div>
+            <div class="modal-footer" style="display: block;">
+                <div id="purchase-loading-text" style="display: none; text-align: center; width: 100%; padding: 15px;">
+                    <i class="fa fa-spin fa-spinner"></i>Đang tải...
+                </div>
+                <div id="btn-purchase-group" style="text-align: right;">
+                    <button data-dismiss="modal" class="btn btn-link btn-google" style="width: auto !important;">Tiếp
+                        tục mua <i class="fa fa-angle-right"></i></button>
+                    <button class="btn btn-sm btn-google" style="margin: 10px 10px 10px 0px !important;">Thanh toán <i
+                                class="fa fa-angle-right"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="modalBuy" class="modal fade">
+    <div class="modal-dialog modal-large">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" data-dismiss="modal" class="close">×</button>
+                <h2 class="medium-title">Giỏ hàng</h2></div>
+            <div id="modal-buy-body" class="modal-body"><br> <!---->
+                <hr>
+                <div class="row">
+                    <div class="col-md-4"><h4 class="text-left"><b>Tổng</b></h4></div>
+                    <div class="col-md-8"><h4 class="text-right"><b>0</b></h4></div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button data-toggle="modal" data-target="#modalBuy" class="btn btn-link btn-google"
+                        style="width: auto !important;">Tiếp tục mua <i class="fa fa-angle-right"></i></button>
+                <button id="btn-purchase" class="btn btn-sm btn-google" style="margin: 10px 10px 10px 0px !important;">
+                    Thanh toán <i class="fa fa-angle-right"></i></button>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="modalgoogle" class="modal fade">
     <div class="modal-dialog modal-large">
 
         <!-- Modal content-->
@@ -206,14 +258,18 @@
         </div>
 
     </div>
-</div></body>
+</div>
+</body>
 
 <!--  Plugins -->
 <!-- Core JS Files -->
-<script src="https://d255zuevr6tr8p.cloudfront.net/landingpage/assets/js/jquery-3.2.1.min.js" type="text/javascript"></script>
-<script src="https://d255zuevr6tr8p.cloudfront.net/landingpage/assets/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
+<script src="https://d255zuevr6tr8p.cloudfront.net/landingpage/assets/js/jquery-3.2.1.min.js"
+        type="text/javascript"></script>
+<script src="https://d255zuevr6tr8p.cloudfront.net/landingpage/assets/js/jquery-ui-1.12.1.custom.min.js"
+        type="text/javascript"></script>
 <script src="https://d255zuevr6tr8p.cloudfront.net/landingpage/assets/js/tether.min.js" type="text/javascript"></script>
-<script src="https://d255zuevr6tr8p.cloudfront.net/landingpage/assets/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="https://d255zuevr6tr8p.cloudfront.net/landingpage/assets/js/bootstrap.min.js"
+        type="text/javascript"></script>
 <script src="https://d255zuevr6tr8p.cloudfront.net/landingpage/assets/js/paper-kit.js?v=2.0.0"></script>
 <script src="https://d255zuevr6tr8p.cloudfront.net/landingpage/assets/js/demo.js"></script>
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
@@ -225,26 +281,30 @@
 <script async defer src="https://buttons.github.io/buttons.js"></script>
 
 <script type="text/javascript">
-    (function() {
+    (function () {
         function getRandomInt(min, max) {
             return Math.floor(Math.random() * (max - min + 1)) + min;
         }
 
         new IsoGrid(document.querySelector('.isolayer--deco1'), {
-            transform : 'translateX(33vw) translateY(-340px) rotateX(45deg) rotateZ(45deg)',
-            stackItemsAnimation : {
-                properties : function(pos) {
+            transform: 'translateX(33vw) translateY(-340px) rotateX(45deg) rotateZ(45deg)',
+            stackItemsAnimation: {
+                properties: function (pos) {
                     return {
-                        translateZ: (pos+1) * 30,
+                        translateZ: (pos + 1) * 30,
                         rotateZ: getRandomInt(-4, 4)
                     };
                 },
-                options : function(pos, itemstotal) {
+                options: function (pos, itemstotal) {
                     return {
                         type: dynamics.bezier,
                         duration: 500,
-                        points: [{"x":0,"y":0,"cp":[{"x":0.2,"y":1}]},{"x":1,"y":1,"cp":[{"x":0.3,"y":1}]}],
-                        delay: (itemstotal-pos-1)*40
+                        points: [{"x": 0, "y": 0, "cp": [{"x": 0.2, "y": 1}]}, {
+                            "x": 1,
+                            "y": 1,
+                            "cp": [{"x": 0.3, "y": 1}]
+                        }],
+                        delay: (itemstotal - pos - 1) * 40
                     };
                 }
             }
