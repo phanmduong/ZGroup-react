a77ab5e980be88032ae7b3025bfe2f068cdd11d7
Cao Anh Quan <aquancva@gmail.com>
2017-12-25 12:08:33 +0700

    select card by date

--- Modules/Staff/Http/Controllers/StaffChartApiController.php
+++ Modules/Staff/Http/Controllers/StaffChartApiController.php
@@ -2,11 +2,11 @@
 
 namespace Modules\Staff\Http\Controllers;
 
+use App\Card;
 use App\Http\Controllers\ManageApiController;
-use App\User;
-use Faker\Provider\DateTime;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class StaffChartApiController extends ManageApiController
 {
@@ -14,20 +14,31 @@ class StaffChartApiController extends ManageApiController
      * GET /staff
      * @return Response
      */
-    public function countStaffCards(Response $request)
+    public function countStaffCards(Request $request)
     {
-        $from = $request->from;
-        $to = $request->to;
-        dd(from);
-        if ($request->to === null) {
-            $request->to = date();
+        if ($request->to === null || $request->from === null) {
+            return $this->respondErrorWithStatus("Bạn cần truyền lên người bắt đầu và ngày kết thúc ");
         }
-        if ($request->from === null){
-            $to->modify('-7 days');
-            $request->from =  $to;
+
+        $to = date_create_from_format("d/m/Y H:i:s", $request->to . " 00:00:00");
+        $from = date_create_from_format("d/m/Y H:i:s", $request->from . " 00:00:00");
+
+        if ($to < $from) {
+            return $this->respondErrorWithStatus("Thời gian bắt đầu không được lớn hơn thời gian kết thúc");
         }
+
+        $cards = Card::whereBetween("updated_at", [$from, $to])->groupBy(DB::raw("date(updated_at)"))
+            ->select(DB::raw('count(1) as num_cards, date(updated_at) as day'))
+            ->orderBy("day")->get();
+
+
         return $this->respondSuccessWithStatus([
-//            "user" => $user
+            "days" => $cards->map(function ($card) {
+                return $card->day;
+            }),
+            "num_cards" => $cards->map(function ($card) {
+                return $card->num_cards;
+            })
         ]);
     }
 
