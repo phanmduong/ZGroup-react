8c443c56658a917d7a3151d08f353970151bd794
datvithanh98 <datvithanh98@gmail.com>
2017-12-25 11:29:54 +0700

    up

--- Modules/Alibaba/Http/Controllers/AlibabaManageApiController.php
+++ Modules/Alibaba/Http/Controllers/AlibabaManageApiController.php
@@ -88,6 +88,7 @@ class AlibabaManageApiController extends ManageApiController
             return $this->respondErrorWithStatus([
                 'message' => 'Trung code'
             ]);
+        dd($register->code);
         $register = Register::find($register_id);
         if($request->money === null || $register->code === null)
             return $this->respondErrorWithStatus([
