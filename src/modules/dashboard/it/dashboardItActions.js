c4659ba7b59415e62968a7f219ae862adf8d61f5
Cao Anh Quan <aquancva@gmail.com>
2017-12-26 15:35:09 +0700

    chart cards

--- src/modules/dashboard/it/dashboardItActions.js
+++ src/modules/dashboard/it/dashboardItActions.js
@@ -1,16 +1,15 @@
 import * as types from '../../../constants/actionTypes';
 import * as dashboardItApi from './dashboardItApi';
 
-
 /*eslint no-console: 0 */
 
-export function loadCountCardsByStaffDuration(staffId, from, to) {
+export function loadCountCardsByStaffDuration(from, to, projectId = null, staffId = null) {
     return function (dispatch) {
         dispatch({
             type: types.BEGIN_LOAD_CARDS_STAFF_DURATION,
 
         });
-        dashboardItApi.loadCards(staffId,from, to)
+        dashboardItApi.loadCards(from, to, projectId, staffId)
             .then((res) => {
                 dispatch({
                     type: types.LOAD_CARDS_STAFF_DURATION_SUCCESS,
--- src/modules/dashboard/it/dashboardItApi.js
+++ src/modules/dashboard/it/dashboardItApi.js
@@ -1,15 +1,31 @@
 import axios from 'axios';
 import * as env from '../../../constants/env';
 
-export function loadCards(staffId, from, to) {
-    let url = env.MANAGE_API_URL + `/staff/${staffId}/chart/card?from=${from}&to=${to}`;
+export function loadCards(from, to, projectId = null, staffId = null) {
+    let url = env.MANAGE_API_URL + `/card/chart?from=${from}&to=${to}&project_id=${projectId}&staff_id=${staffId}`;
     let token = localStorage.getItem('token');
     if (token) {
         url += "&token=" + token;
     }
-
     return axios.get(url);
 }
 
+export const loadProjects = () => {
+    let url = env.MANAGE_API_URL + `/projects?limit=-1`;
+    let token = localStorage.getItem('token');
+    if (token) {
+        url += "&token=" + token;
+    }
+    return axios.get(url);
+};
 
+
+export const loadStaffs = () => {
+    let url = env.MANAGE_API_URL + `/staff?limit=-1`;
+    let token = localStorage.getItem('token');
+    if (token) {
+        url += "&token=" + token;
+    }
+    return axios.get(url);
+};
 
