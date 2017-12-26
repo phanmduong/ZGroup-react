51ef5f5bbd8c6e59aa60b57cc73c0f914d0bf5c5
Cao Anh Quan <aquancva@gmail.com>
2017-12-26 11:23:46 +0700

    add quan ly sach

---
+++ src/routes/BookRoute.js
@@ -0,0 +1,12 @@
+/**
+ * Tab Nhân sự
+ */
+import BookListContainer from "../modules/book/graphics/BookListContainer";
+
+export default [
+    {
+        path: "/book/all",
+        component: BookListContainer,
+    }
+
+];
