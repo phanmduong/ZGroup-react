b06a58ed2f0886f159c3f6808e4e26536bd4f86b
thanhphamtt <thanhpham1479@gmail.com>
2017-12-26 21:16:13 +0700

    up

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -35,9 +35,8 @@ class AuthenticateController extends Controller
         }
 
         if ($user->phone != null && $user->phone != "" && $user->homeland != null && $user->homeland != "" &&
-            $user->age != null && $user->age != "" &&
-            $user->address != null && $user->address != "" && $user->university != null && $user->university != "" &&
-            $user->start_company != null && $user->start_company != "") $user->first_login = 1;
+            $user->age != null && $user->age != "" && $user->name != null && $user->name != "" &&
+            $user->address != null && $user->address != "" && $user->color != null && $user->color != "" ) $user->first_login = 1;
         // all good so return the token
 
         return response()->json([
