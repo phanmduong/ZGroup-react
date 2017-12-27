35fbf45590cc50a9af272b29b03001f6d5893481
thanhphamtt <thanhpham1479@gmail.com>
2017-12-27 13:46:38 +0700

    up

--- Modules/Work/Http/Controllers/WorkApiController.php
+++ Modules/Work/Http/Controllers/WorkApiController.php
@@ -33,4 +33,97 @@ class WorkApiController extends ManageApiController
 
 
     }
+
+    public function deleteWork($workId, Request $request)
+    {
+        $work = Work::find($workId);
+        if (!$work) return $this->respondErrorWithStatus("Không tồn tại công việc");
+        $work->delete();
+        return $this->respondSuccessWithStatus([
+            "message" => "Xóa thành công"
+        ]);
+    }
+
+    public function getDetailWork($workId, Request $request)
+    {
+        $work = Work::find($workId);
+        if (!$work) return $this->respondErrorWithStatus("Không tồn tại công việc");
+        $staffs = $work->staffs;
+        return $this->respondSuccessWithStatus([
+            "work" => [
+                "id" => $work->id,
+                "name" => $work->name,
+                "type" =>$work->type,
+                "cost" =>$work->cost,
+                "deadline" =>$work->deadline,
+                "bonus_value" =>$work->bonus_value,
+                "bonus_type" => $work->bonus_type
+            ],
+            "staffs" => $staffs->map(function ($staff) {
+                return [
+                    "id" => $staff->id,
+                    "name" => $staff->name,
+                    "avatar_url" => $staff->avatar_url ? $staff->avatar_url : "http://api.colorme.vn/img/user.png",
+                ];
+            })
+        ]);
+    }
+
+    public function editWork($workId, Request $request)
+    {
+        $work = Work::find($workId);
+        if (!$work) return $this->respondErrorWithStatus("Không tồn tại công việc");
+        if (!$request->name) return $this->respondErrorWithStatus("Thiếu tên");
+        $work->name = $request->name;
+        $work->type = $request->type;
+        $work->cost = $request->cost ? $request->cost : 0;
+        $work->deadline = $request->deadline;
+        $work->bonus_value = $request->bonus_value ? $request->bonus_value : 0;
+        $work->bonus_type = $request->bounus_type;
+        $staffs = json_decode($request->staffs);
+        $work->save();
+        if (count($staffs) > 0) {
+            $work->staffs()->detach();
+            foreach ($staffs as $staff) {
+                $work->staffs()->attach($staff->id);
+            }
+        }
+        return $this->respondSuccessWithStatus([
+            "message" => "Sửa thành công"
+        ]);
+    }
+
+    public function getAll(Request $request)
+    {
+        $limit = $request->limit ? $request->limit : 20;
+        $keyword = $request->search;
+        $works = Work::where(function ($query) use ($keyword) {
+            $query->where('name', 'like', "%$keyword%");
+        })->orderBy("created_at", "desc")->paginate($limit);
+
+        return $this->respondWithPagination($works, [
+            "works" => $works->map(function ($work) {
+                $staffs = $work->staffs;
+                return [
+                    "work" => [
+                        "id" => $work->id,
+                        "name" => $work->name,
+                        "type" =>$work->type,
+                        "cost" =>$work->cost,
+                        "deadline" =>$work->deadline,
+                        "bonus_value" =>$work->bonus_value,
+                        "bonus_type" => $work->bonus_type
+                    ],
+                    "staffs" => $staffs->map(function ($staff) {
+                        return [
+                            "id" => $staff->id,
+                            "name" => $staff->name,
+                            "avatar_url" => $staff->avatar_url ? $staff->avatar_url : "http://api.colorme.vn/img/user.png",
+                        ];
+                    })
+                ];
+            })
+        ]);
+
+    }
 }
--- Modules/Work/Http/routes.php
+++ Modules/Work/Http/routes.php
@@ -3,4 +3,8 @@
 Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => 'work', 'namespace' => 'Modules\Work\Http\Controllers'], function()
 {
     Route::post('/', 'WorkApiController@createWork');
+    Route::get('/{workId}','WorkApiController@getDetailWork');
+    Route::get('/','WorkApiController@getAll');
+    Route::put('/{workId}','WorkApiController@editWork');
+    Route::delete('/{workId}','WorkApiController@deleteWork');
 });
