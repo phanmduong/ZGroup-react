dd56c9c4ee963d5d4f4f4e14b9e351ac4cf508ae
phanmduong <phanminhduong@hotmail.com>
2017-12-29 21:02:31 +0700

    update code

--- resources/views/client/blog.blade.php
+++ resources/views/client/blog.blade.php
@@ -5,5 +5,5 @@
 @endsection
 
 @section("js")
-    <script type="text/javascript" src="manage/blog/main.1fa017a8d8bc462025b9.js"></script>
+    <script type="text/javascript" src="manage/blog/main.3c79e446975c57f89627.js"></script>
 @endsection
