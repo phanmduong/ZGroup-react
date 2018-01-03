b07de041c7f1a6ada4e70809848ad790c403735f
quan <aquancva@gmail.com>
2018-01-02 23:19:28 -0500

    up

--- Modules/Order/Http/routes.php
+++ Modules/Order/Http/routes.php
@@ -2,12 +2,10 @@
 
 Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => 'order', 'namespace' => 'Modules\Order\Http\Controllers'], function () {
     Route::get('/all-orders', 'OrderController@allOrders');
-
     Route::get('/statistic','OrderController@statisticalOrder');
-
     Route::put('/{order_id}', 'OrderController@editOrder');
     Route::get('/{order_id}/info', 'OrderController@detailedOrder');
-    Route::put('/change-status-order', 'OrderController@changeStatus');
+    Route::put('/{orderId}/status', 'OrderController@changeStatus');
     Route::post('/pay-order/{orderId}', 'OrderController@payOrder');
     Route::get('/all-order-paid-money', 'OrderController@getOrderPaidMoney');
     Route::post('/check-goods', 'OrderController@checkGoods');
