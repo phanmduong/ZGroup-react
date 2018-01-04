9123674718660d729ea3de680fb9db433f6bcb2c
phanmduong <phanminhduong@hotmail.com>
2018-01-04 14:51:21 +0700

    update code

--- Modules/Course/Http/routes.php
+++ Modules/Course/Http/routes.php
@@ -19,7 +19,7 @@ Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => '/v2/
     Route::put('/lesson/edit/{lessonId}', 'CourseController@editLesson');
     Route::get('/get-attendance-lesson/{classId}/{lessonId}','CourseController@getAttendance');
     Route::post('/change-attendances','CourseController@changeAttendance');
-
+    Route::put('/{course_id}/change-status','CourseController@changeStatusCourse');
 });
 
 Route::group(['domain' => 'api.' . config('app.domain'), 'prefix' => 'apiv2', 'namespace' => 'Modules\Course\Http\Controllers'], function () {
