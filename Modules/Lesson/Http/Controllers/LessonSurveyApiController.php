ee276f5ce200436adcb68dd6fa21b263c4888ab7
datvithanh98 <datvithanh98@gmail.com>
2018-01-05 23:42:57 -0500

    up

--- Modules/Lesson/Http/Controllers/LessonSurveyApiController.php
+++ Modules/Lesson/Http/Controllers/LessonSurveyApiController.php
@@ -3,17 +3,29 @@
 namespace Modules\Lesson\Http\Controllers;
 
 use App\Colorme\Transformers\TermTransformer;
-use App\Http\Controllers\ManageApiController;
+use App\Http\Controllers\ApiController;
+use App\Lesson;
+use Illuminate\Http\Request;
 
-class LessonSurveyApiController extends ManageApiController
+class LessonSurveyApiController extends ApiController
 {
-
-    protected $termTransformer;
-
     public function __construct(TermTransformer $termTransformer)
     {
         parent::__construct();
-        $this->termTransformer = $termTransformer;
     }
 
+    public function lessonSurveys($lessonId, Request $request)
+    {
+        $lesson = Lesson::find($lessonId);
+        if($lesson == null)
+            return $this->respondErrorWithStatus([
+                'message' => 'Không tồn tại tiết học'
+            ]);
+        $surveys = $lesson->surveys;
+        return $this->respondSuccessWithStatus([
+            'surveys' => $surveys->map(function ($survey){
+                return $survey->getData;
+            })
+        ]);
+    }
 }
--- Modules/Lesson/Http/routes.php
+++ Modules/Lesson/Http/routes.php
@@ -1,7 +1,8 @@
 <?php
-Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => '/lesson', 'namespace' => 'Modules\Lesson\Http\Controllers'], function () {
-
+Route::group(['domain' => 'api.' . config('app.domain'), 'prefix' => '/lesson', 'namespace' => 'Modules\Lesson\Http\Controllers'], function () {
+    Route::get('{lessonId}/survey', 'LessonSurveyApiController@lessonSurveys');
 });
+
 Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => '/v2/lesson', 'namespace' => 'Modules\Lesson\Http\Controllers'], function () {
     Route::get('/get-detail-lesson/{lesson_id}','LessonController@getdetailLesson');
     Route::post('/create-lesson/{courseId}','LessonController@createlesson');
