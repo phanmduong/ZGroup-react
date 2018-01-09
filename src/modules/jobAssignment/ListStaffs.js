540817391ff43b3bcb8cd6722ac4a60a837a6542
seenastic <goldenrezgun@gmail.com>
2018-01-09 09:47:06 +0700

    ADD INFO STAFF IN WORK

--- src/modules/jobAssignment/ListStaffs.js
+++ src/modules/jobAssignment/ListStaffs.js
@@ -3,11 +3,22 @@ import PropTypes from 'prop-types';
 import {ListGroup, ListGroupItem} from "react-bootstrap";
 import Avatar from "../../components/common/Avatar";
 import * as helper from "../../helpers/helper";
-
+import {Link}  from 'react-router';
+import {Modal} from 'react-bootstrap';
+import InfoStaffContainer from "../../modules/manageStaff/InfoStaffContainer";
 
 class ListStaffs extends React.Component {
     constructor(props, context) {
         super(props, context);
+        this.state = {
+            staffId: "",
+            show: false,
+        };
+        this.onHide = this.onHide.bind(this);
+    }
+
+    onHide(){
+        this.setState({show: false});
     }
 
     render() {
@@ -24,22 +35,39 @@ class ListStaffs extends React.Component {
                                 <div style={{display: "flex"}}>
                                     <Avatar size={30} url={helper.validateLinkImage(m.avatar_url)}/>{m.label || m.name}
                                 </div>
-                                {
-                                    this.props.remove ?
-                                        <div onClick={() => {
-                                            return this.props.remove(m)
-                                        }}>
-                                            <i className="material-icons">highlight_off</i>
+                                <div style={{display: "flex"}}>{
+                                    m.id ?
+                                        <div onClick={()=>{return this.setState({show: true, staffId:m.id});}}>
+                                            <i className="material-icons">info</i>
                                         </div>
                                         :
                                         <div></div>
                                 }
+                                {
+                                    this.props.remove ?
+                                        <div onClick={() => {return this.props.remove(m)}}>
+                                            <i className="material-icons">highlight_off</i>
+                                        </div>
+                                        :
+                                        <div></div>
+                                }</div>
 
                             </div>
                         </ListGroupItem>
                     )
                 )}
-            </ListGroup></div>
+            </ListGroup>
+                <Modal
+                    show={this.state.show}
+                    onHide={this.onHide}
+                    bsSize="large"
+                >
+                    <Modal.Header closeButton/>
+                    <Modal.Body>
+                        <InfoStaffContainer staffId={this.state.staffId} />
+                    </Modal.Body>
+                </Modal>
+            </div>
         );
     }
 }
