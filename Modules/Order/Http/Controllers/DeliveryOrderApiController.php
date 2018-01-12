9d7e28345c6f8dea8d41424db34c0f297e93e981
datvithanh98 <datvithanh98@gmail.com>
2018-01-12 11:49:16 +0700

    up

--- Modules/Order/Http/Controllers/DeliveryOrderApiController.php
+++ Modules/Order/Http/Controllers/DeliveryOrderApiController.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Order\Http\Controllers;
 
+use App\Colorme\Transformers\DeliveryOrderTransformer;
 use App\Register;
 use App\User;
 use Illuminate\Http\Request;
@@ -9,13 +10,33 @@ use App\Http\Controllers\ManageApiController;
 
 class DeliveryOrderApiController extends ManageApiController
 {
-    public function __construct()
+    public function __construct(DeliveryOrderTransformer $deliveryOrderTransformer)
     {
         parent::__construct();
+
+        $this->deliveryOrderTransformer = $deliveryOrderTransformer;
     }
 
-    public function getDeliveryOrders()
+    public function getDeliveryOrders(Request $request)
     {
+        $limit = $request->limit ? $request->limit : 20;
+        $deliveryOrders = Order::where('type', 'delivery');
+        //queries
 
+        if ($limit == -1) {
+            $deliveryOrders = $deliveryOrders->orderBy('created_at', 'desc')->get();
+            return $this->respondSuccessWithStatus([
+                'delivery_orders' => $this->deliveryOrderTransformer->transformCollection($deliveryOrders)
+            ]);
+        }
+        $deliveryOrders->orderBy('created_at', 'desc')->paginate($limit);
+        return $this->respondWithPagination(
+            $deliveryOrders
+            ,
+            [
+                'delivery_orders' => $this->deliveryOrderTransformer->transformCollection($deliveryOrders)
+            ]);
     }
+
+//    public function
 }
