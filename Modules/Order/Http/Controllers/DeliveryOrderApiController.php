ee890c37feaaad3b7e6735cc31e2c3a36d99d54e
datvithanh98 <datvithanh98@gmail.com>
2018-01-12 14:30:39 +0700

    up

--- Modules/Order/Http/Controllers/DeliveryOrderApiController.php
+++ Modules/Order/Http/Controllers/DeliveryOrderApiController.php
@@ -20,8 +20,27 @@ class DeliveryOrderApiController extends ManageApiController
     public function getDeliveryOrders(Request $request)
     {
         $limit = $request->limit ? $request->limit : 20;
+        $keyWord = $request->search;
+
         $deliveryOrders = Order::where('type', 'delivery');
         //queries
+        if ($keyWord) {
+            $userIds = User::where(function ($query) use ($keyWord) {
+                $query->where("name", "like", "%$keyWord%")->orWhere("phone", "like", "%$keyWord%");
+            })->pluck('id')->toArray();
+            $deliveryOrders = $deliveryOrders->where('type', 'order')->where(function ($query) use ($keyWord, $userIds) {
+                $query->whereIn('user_id', $userIds)->orWhere("code", "like", "%$keyWord%")->orWhere("email", "like", "%$keyWord%");
+            });
+        }
+
+        if ($request->staff_id)
+            $deliveryOrders = $deliveryOrders->where('staff_id', $request->staff_id);
+        if ($request->start_time)
+            $deliveryOrders = $deliveryOrders->whereBetween('created_at', array($request->start_time, $request->end_time));
+        if ($request->status)
+            $deliveryOrders = $deliveryOrders->where('status', $request->status);
+        if ($request->user_id)
+            $deliveryOrders = $deliveryOrders->where('user_id', $request->user_id);
 
         if ($limit == -1) {
             $deliveryOrders = $deliveryOrders->orderBy('created_at', 'desc')->get();
@@ -38,5 +57,38 @@ class DeliveryOrderApiController extends ManageApiController
             ]);
     }
 
-//    public function
+    public function infoDeliveryOrders(Request $request)
+    {
+        $keyWord = $request->search;
+
+        $deliveryOrders = Order::where('type', 'delivery');
+        //queries
+        if ($keyWord) {
+            $userIds = User::where(function ($query) use ($keyWord) {
+                $query->where("name", "like", "%$keyWord%")->orWhere("phone", "like", "%$keyWord%");
+            })->pluck('id')->toArray();
+            $deliveryOrders = $deliveryOrders->where('type', 'order')->where(function ($query) use ($keyWord, $userIds) {
+                $query->whereIn('user_id', $userIds)->orWhere("code", "like", "%$keyWord%")->orWhere("email", "like", "%$keyWord%");
+            });
+        }
+
+        if ($request->staff_id)
+            $deliveryOrders = $deliveryOrders->where('staff_id', $request->staff_id);
+        if ($request->start_time)
+            $deliveryOrders = $deliveryOrders->whereBetween('created_at', array($request->start_time, $request->end_time));
+        if ($request->status)
+            $deliveryOrders = $deliveryOrders->where('status', $request->status);
+        if ($request->user_id)
+            $deliveryOrders = $deliveryOrders->where('user_id', $request->user_id);
+
+        $deliveryOrders = $deliveryOrders->orderBy('created_at', 'desc')->get();
+
+        return $this->respondSuccessWithStatus([
+            'total_delivery_orders' => 10,
+            'locked' => 8,
+            'not_locked' => 2,
+            'total_money' => 15000000,
+            'total_paid_money' => 10000000
+        ]);
+    }
 }
