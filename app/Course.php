9b7526f360a83b41b775059254ca7561e08c4f8f
thanhphamtt <thanhpham1479@gmail.com>
2018-01-13 11:31:52 +0700

    up

--- app/Course.php
+++ app/Course.php
@@ -68,6 +68,7 @@ class Course extends Model
             'links' => $this->links,
             'terms' => $this->terms,
             'status' => $this->status,
+            'type_id' => $this->type_id,
             'type' => $this->courseType ? $this->courseType->getData : null,
             'categories' => $this->courseCategories->map(function ($courseCategory) {
                 return $courseCategory->getData();
