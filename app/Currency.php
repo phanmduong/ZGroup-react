3244edf76afbb1f688e601d01da0a14f1aa3998d
datvithanh98 <datvithanh98@gmail.com>
2018-01-14 17:19:18 +0700

    up

---
+++ app/Currency.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Currency extends Model
+{
+    //
+    protected $table = 'currencies';
+    public function transform(){
+        return[
+          "id" =>$this->id,
+          "name" =>$this->name,
+          "notation" =>$this->notation,
+          "ratio" =>$this->ration,
+        ];
+    }
+}
