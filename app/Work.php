98eef5b6bb8efe25918ce7269ffc5d9e31a9ed6f
LetHerGo <thanhpham1479@gmail.com>
2018-01-15 21:49:01 +0700

    up

--- app/Work.php
+++ app/Work.php
@@ -33,11 +33,11 @@ class Work extends Model
             "deadline" => $this->deadline,
             "bonus_value" => $this->bonus_value,
             "currency" => $this->currency ? $this->currency->transform() : [],
-            "payer" => [
+            "payer" =>$this->payer ? [
                 "id" => $this->payer->id,
                 "name" => $this->payer->name,
                 "avatar_url" => $this->payer->avatar_url,
-            ],
+            ] : [],
             "staffs" => $this->staffs->map(function ($staff) {
                 return [
                     "id" => $staff->id,
