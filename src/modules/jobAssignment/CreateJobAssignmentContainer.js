fedafe023ffdeb64500b350a0d43cea97775a9db
seenastic <goldenrezgun@gmail.com>
2018-01-19 10:21:24 +0700

    b

--- src/modules/jobAssignment/CreateJobAssignmentContainer.js
+++ src/modules/jobAssignment/CreateJobAssignmentContainer.js
@@ -75,7 +75,7 @@ class CreateJobAssignmentContainer extends React.Component {
     updateFormDataBonusType(e){
         if(!e) return;
         let newdata = {...this.props.data,currency : e};
-        this.props.jobAssgnmentAction.updateFormData(newdata);
+        this.props.jobAssignmentAction.updateFormData(newdata);
     }
 
 
