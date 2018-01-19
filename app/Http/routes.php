de344ebed77451891b398c55103fb7e6c6daaab3
LetHerGo <thanhpham1479@gmail.com>
2018-01-19 22:16:49 +0700

    up

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -167,9 +167,9 @@ Route::group(['domain' => 'manageapi.' . config('app.domain')], function () {
     Route::post('/save-post', 'ManageBlogController@save_post');
     Route::get('/posts', 'ManageBlogController@get_posts');
     Route::post('/post/{postId}/change-status','ManageBlogController@changeStatusPost');
+    Route::get('/post/categories','ManageBlogController@getAllCategory');
     Route::get('/post/{postId}', 'ManageBlogController@get_post');
     Route::delete('/post/{postId}/delete', 'ManageBlogController@delete_post');
-    Route::get('/post/categories','ManageBlogController@getAllCategory');
     //End blog api
 
     //Begin register students api
