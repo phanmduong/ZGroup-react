7414648e42638408522a5be7a81c5be45f730f07
Cao Anh Quan <aquancva@gmail.com>
2018-01-19 09:29:59 +0700

    up order, finance, good

---
+++ resources/views/client/order.blade.php
@@ -0,0 +1,9 @@
+@extends("client.layout.master")
+
+@section("css")
+    <link href="manage/order/main.c83ece5cde9e25340923fc03d2f130e1.css" rel="stylesheet">
+@endsection
+
+@section("js")
+    <script type="text/javascript" src="manage/order/main.3ea66f367b5724a8c60e.js"></script>
+@endsection
