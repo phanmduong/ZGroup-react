17bfe8020499d2b6a1bb82457da2ffbf55cc4211
Cao Anh Quan <aquancva@gmail.com>
2018-01-20 00:03:03 +0700

    add survey lesson

--- src/modules/survey/SurveyDisplayModalContainer.js
+++ src/modules/survey/SurveyDisplayModalContainer.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import PropTypes from 'prop-types';
-import {Modal} from "react-bootstrap";
+import {Button, ControlLabel, FormControl, FormGroup, Modal} from "react-bootstrap";
 import Select from 'react-select';
 import * as surveyActions from '../survey/surveyActions';
 import {loadAllCourses} from "../marketingCampaign/marketingCampaignApi";
+import {addSurveyLesson} from "./surveyApi";
 
 // Import actions here!!
 
@@ -15,24 +16,35 @@ class SurveyDisplayModalContainer extends React.Component {
         this.state = {
             course: {},
             courses: [],
+            lesson: {},
+            minutesDuration: 0,
+            minuteStart: 0,
             isLoadingCourses: true
         };
         this.handleClose = this.handleClose.bind(this);
         this.handleChangeCourse = this.handleChangeCourse.bind(this);
+        this.handleChangeLesson = this.handleChangeLesson.bind(this);
+        this.handleFormInput = this.handleFormInput.bind(this);
+        this.saveSurveyLesson = this.saveSurveyLesson.bind(this);
     }
 
     async componentWillMount() {
         const res = await loadAllCourses();
         const {courses} = res.data.data;
         this.setState({
-            courses: courses.map(({id, name}) => {
+            courses: courses.map(({id, name, lessons}) => {
                 return {
                     value: id,
-                    label: name
+                    label: name,
+                    lessons
                 };
             }),
             isLoadingCourses: false
         });
+    }
+
+    saveSurveyLesson() {
+        addSurveyLesson(this.props.survey.id, this.state.lesson.value, this.state.minutesDuration, this.state.minuteStart);
     }
 
     handleClose() {
@@ -40,9 +52,20 @@ class SurveyDisplayModalContainer extends React.Component {
     }
 
     handleChangeCourse(course) {
-        this.setState({
-            course
-        });
+        if (course)
+            this.setState({course});
+        else this.setState({course: {}});
+    }
+
+    handleChangeLesson(lesson) {
+        this.setState({lesson});
+    }
+
+    handleFormInput(event) {
+        let state = {...this.state};
+        const field = event.target.name;
+        state[field] = event.target.value;
+        this.setState(state);
     }
 
     render() {
@@ -52,13 +75,67 @@ class SurveyDisplayModalContainer extends React.Component {
                     <Modal.Title>Cài đặt hiển thị</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <Select
-                        name="start"
-                        isLoading={this.state.isLoadingCourses}
-                        value={this.state.course}
-                        options={this.state.courses}
-                        onChange={this.}
-                    />
+                    <FormGroup>
+                        <ControlLabel>Môn học hiển thị</ControlLabel>
+                        <Select
+                            name="course"
+                            isLoading={this.state.isLoadingCourses}
+                            value={this.state.course}
+                            options={this.state.courses}
+                            onChange={this.handleChangeCourse}/>
+                    </FormGroup>
+                    {
+                        this.state.course.lessons && (
+                            <div>
+                                <FormGroup>
+                                    <ControlLabel>Buổi học hiển thị</ControlLabel>
+                                    <Select
+                                        name="lesson"
+                                        isLoading={this.state.isLoadingCourses}
+                                        options={this.state.course.lessons.map((lesson) => {
+                                            return {
+                                                value: lesson.id,
+                                                label: lesson.name
+                                            };
+                                        })}
+                                        value={this.state.lesson}
+                                        onChange={this.handleChangeLesson}
+                                    />
+                                </FormGroup>
+                                <FormGroup controlId="minutesDuration">
+                                    <ControlLabel>Thời gian hiển thị (phút)</ControlLabel>
+                                    <FormControl
+                                        name="minutesDuration"
+                                        type="number"
+                                        value={this.state.minutesDuration}
+                                        placeholder="Thời gian hiển thị (phút)"
+                                        onChange={this.handleFormInput}
+                                    />
+                                </FormGroup>
+
+                                <FormGroup controlId="minuteStart">
+                                    <ControlLabel>Thời gian bắt đầu hiển thị (Phút thứ mấy của buổi học)</ControlLabel>
+                                    <FormControl
+                                        type="number"
+                                        name="minuteStart"
+                                        value={this.state.minuteStart}
+                                        placeholder="Thời gian bắt đầu hiển thị (Phút thứ mấy của buổi học)"
+                                        onChange={this.handleFormInput}
+                                    />
+                                </FormGroup>
+                            </div>
+                        )
+                    }
+
+
+                    <Modal.Footer>
+                        <Button
+                            disabled={
+                                this.state.course === {} || this.state.lesson === {} || !this.state.minutesDuration
+                            }
+                            className="btn btn-rose" onClick={this.handleClose}>Thêm</Button>
+                        <Button onClick={this.handleClose}>Close</Button>
+                    </Modal.Footer>
                 </Modal.Body>
             </Modal>
         );
@@ -67,12 +144,14 @@ class SurveyDisplayModalContainer extends React.Component {
 
 SurveyDisplayModalContainer.propTypes = {
     showDisplaySettingModal: PropTypes.bool.isRequired,
-    surveyActions: PropTypes.object.isRequired
+    surveyActions: PropTypes.object.isRequired,
+    survey: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state) {
     return {
-        showDisplaySettingModal: state.survey.showDisplaySettingModal
+        showDisplaySettingModal: state.survey.showDisplaySettingModal,
+        survey: state.survey.survey
     };
 }
 
--- src/modules/survey/surveyApi.js
+++ src/modules/survey/surveyApi.js
@@ -68,6 +68,16 @@ export const saveAnswer = (answer) => {
     });
 };
 
+export const addSurveyLesson = (surveyId, lessonId, minutesDuration, minuteStart) => {
+    const token = getToken();
+    const url = env.MANAGE_API_URL + `/v2/survey/${surveyId}/lesson/${lessonId}?token=${token}`;
+    return axios.post(url, {
+        time_display: minutesDuration,
+        start_time_display: minuteStart
+    });
+};
+
+
 export const updateQuestionOrders = (questions) => {
     const token = getToken();
     const url = env.MANAGE_API_URL + `/v2/survey/questions?token=${token}`;
@@ -85,3 +95,4 @@ export const deleteQuestion = (question) => {
     const url = env.MANAGE_API_URL + `/v2/survey/question/${question.id}?token=${token}`;
     return axios.delete(url);
 };
+
