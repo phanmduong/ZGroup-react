5c7e1f44062989be1f9c4756245b4c8166202083
datvithanh98 <datvithanh98@gmail.com>
2018-01-19 15:54:47 +0700

    up

--- Modules/Good/Http/Controllers/InventoryApiController.php
+++ Modules/Good/Http/Controllers/InventoryApiController.php
@@ -156,18 +156,15 @@ class InventoryApiController extends ManageApiController
             $goodIds = Good::where('manufacture_id', $manufacture_id)->pluck('id')->toArray();
             $inventories = $inventories->whereIn('good_id', $goodIds);
         }
-        if ($warehouse_id) {
-            $goodIds = Good::where('warehouse_id', $warehouse_id)->pluck('id')->toArray();
-            $inventories = $inventories->whereIn('good_id', $goodIds);
-        }
+        if ($warehouse_id)
+            $inventories = $inventories->where('warehouse_id', $warehouse_id);
+
         if ($keyword) {
             $goodIds = Good::where(function ($query) use ($keyword) {
                 $query->where("name", "like", "%$keyword%")->orWhere("code", "like", "%$keyword%");
             })->pluck('id')->toArray();
             $inventories = $inventories->whereIn('good_id', $goodIds)->get();
         }
-
-
         $count = $inventories->reduce(function ($total, $inventory) {
             return $total + $inventory->quantity;
         }, 0);
