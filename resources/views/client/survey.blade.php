486c04e93bc8675c1e3f83e6f93d432013969f88
Cao Anh Quan <aquancva@gmail.com>
2018-01-20 11:37:40 +0700

    up

--- resources/views/client/survey.blade.php
+++ resources/views/client/survey.blade.php
@@ -5,5 +5,5 @@
 @endsection
 
 @section("js")
-    <script type="text/javascript" src="manage/surveyv2/main.cf98370722cdb3be97b6.js"></script>
+    <script type="text/javascript" src="manage/surveyv2/main.3330930a52c929630bd9.js"></script>
 @endsection
