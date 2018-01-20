239d96127af45f65cb411158ebac5079292012a0
datvithanh98 <datvithanh98@gmail.com>
2018-01-20 14:41:14 +0700

    up

---
+++ Modules/Base/Http/Controllers/PublicApiController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Modules\Base\Http\Controllers;
+
+use App\Base;
+use App\Http\Controllers\NoAuthApiController;
+use Illuminate\Http\Request;
+
+class PublicApiController extends NoAuthApiController
+{
+    public function baseRooms($baseId, Request $request)
+    {
+        $base = Base::find($baseId);
+        $rooms = $base->rooms;
+        return $this->respondSuccessWithStatus([
+            'rooms' => $rooms->map(function ($room){
+                $data = $room->getData();
+                $data['available_seats'] = $room->seats;
+                return $data;
+            })
+        ]);
+    }
+
+    public function getAllBlogs($subfix, Request $request)
+    {
+        $blogs = Product::where('type', 2)->orderBy('created_at', 'desc')->paginate(10);
+        return $this->respondWithPagination($blogs, ["blogs" => $blogs->map(function ($blog) {
+            return $blog->blogTransform();
+        })]);
+    }
+
+    public function getDetailBlog($subfix, $id)
+    {
+        $product = Product::find($id);
+        if ($product == null) {
+            return $this->respondErrorWithStatus("Bài viết không tồn tại");
+        }
+        return $this->respondSuccessWithStatus([
+            "product" => $product->blogDetailTransform()
+        ]);
+    }
+}
