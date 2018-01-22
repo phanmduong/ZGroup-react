4a3aadb8ee80b7e26779b1779eb00a9d23a05414
Cao Anh Quan <aquancva@gmail.com>
2018-01-22 16:30:51 +0700

    up

--- Modules/Finance/Http/routes.php
+++ Modules/Finance/Http/routes.php
@@ -1,6 +1,8 @@
 <?php
 
 Route::group(['prefix' => 'finance', 'namespace' => 'Modules\Finance\Http\Controllers'], function () {
+    Route::get('/bank-accounts', 'FinanceManageApiController@bankTransfers');
+
     Route::group(['prefix' => 'bank-transfer'], function () {
         Route::get('/', 'FinanceManageApiController@bankTransfers');
     });
