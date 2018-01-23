5736eb886149c630fcfe19eb1390daa42752cae2
Cao Anh Quan <aquancva@gmail.com>
2018-01-23 09:38:01 +0700

    up

--- Modules/Survey/Http/routes.php
+++ Modules/Survey/Http/routes.php
@@ -1,6 +1,8 @@
 <?php
 
 Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => '/v2/survey', 'namespace' => 'Modules\Survey\Http\Controllers'], function () {
+
+
     Route::get('', 'SurveyController@getSurveys');
     Route::get('{surveyId}', 'SurveyController@getSurvey');
     Route::post('', 'SurveyController@createSurvey');
--- app/Survey.php
+++ app/Survey.php
@@ -30,10 +30,22 @@ class Survey extends Model
             ->withTimestamps();
     }
 
+    public function shortData()
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'staff' => $this->user ? $this->user->getData() : null,
+            "created_at" => format_time_to_mysql(strtotime($this->created_at))
+        ];
+    }
+
     public function getData()
     {
         return [
             'id' => $this->id,
+            "image_url" => $this->image_url ? $this->image_url : emptyImageUrl(),
+            "description" => $this->description,
             'name' => $this->name,
             'staff' => $this->user ? $this->user->getData() : null,
             "questions_count" => $this->questions()->count(),
