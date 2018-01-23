ef9b97f91a93b94331747a8c9eb0a94bf5f6f00c
Cao Anh Quan <aquancva@gmail.com>
2018-01-23 09:47:50 +0700

    up

--- Modules/Survey/Http/Controllers/SurveyController.php
+++ Modules/Survey/Http/Controllers/SurveyController.php
@@ -148,24 +148,26 @@ class SurveyController extends ManageApiController
         $survey->user_id = $this->user->id;
         $survey->is_final = $request->is_final;
         $survey->save();
-        $questions = json_decode($request->questions);
-        $order = 0;
-        foreach ($questions as $question) {
-            $newQuestion = new Question;
-            $newQuestion->survey_id = $survey->id;
-            $newQuestion->content = $question->content;
-            $newQuestion->type = $question->type;
-            $newQuestion->image_url = $question->image_url;
-            $newQuestion->order = ++$order;
-            $newQuestion->save();
-            $answers = $newQuestion->answers;
-            foreach ($answers as $answer) {
-                $newAnswer = new Answer;
-                $newAnswer->question_id = $newQuestion->id;
-                $newAnswer->content = $answer->content;
-                $newAnswer->image_url = $answer->image_url;
-                $newAnswer->correct = $answer->correct;
-                $newAnswer->save();
+        if ($request->questions) {
+            $questions = json_decode($request->questions);
+            $order = 0;
+            foreach ($questions as $question) {
+                $newQuestion = new Question;
+                $newQuestion->survey_id = $survey->id;
+                $newQuestion->content = $question->content;
+                $newQuestion->type = $question->type;
+                $newQuestion->image_url = $question->image_url;
+                $newQuestion->order = ++$order;
+                $newQuestion->save();
+                $answers = $newQuestion->answers;
+                foreach ($answers as $answer) {
+                    $newAnswer = new Answer;
+                    $newAnswer->question_id = $newQuestion->id;
+                    $newAnswer->content = $answer->content;
+                    $newAnswer->image_url = $answer->image_url;
+                    $newAnswer->correct = $answer->correct;
+                    $newAnswer->save();
+                }
             }
         }
     }
