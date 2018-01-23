87d5486bb03587b6929fdf4ecb0e26767d17e852
Cao Anh Quan <aquancva@gmail.com>
2018-01-23 13:30:19 +0700

    change route delete

--- Modules/Survey/Http/routes.php
+++ Modules/Survey/Http/routes.php
@@ -8,7 +8,7 @@ Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => '/v2/
     Route::post('', 'SurveyController@createSurvey');
 
     Route::post('/{surveyId}/user-lesson', 'SurveyController@createUserLessonSurvey');
-    Route::put('/{surveyId}/user-lesson', 'SurveyController@endUserLessonSurvey');
+    Route::put('/user-lesson-survey/{userLessonSurveyId}', 'SurveyController@endUserLessonSurvey');
     Route::post('/question/{questionId}/user-lesson/{userLessonSurveyId}/answer/{answerId}', 'SurveyController@saveUserLessonSurveyQuestion');
 
     Route::put('/questions', 'SurveyController@updateQuestionOrder');
