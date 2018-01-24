9174a58a1842f4a352c69f91b62dd0b11fd3e7cf
phanmduong <phanminhduong@hotmail.com>
2018-01-24 23:24:30 +0700

    update code

--- Modules/UpCoworkingSpace/Http/routes.php
+++ Modules/UpCoworkingSpace/Http/routes.php
@@ -12,11 +12,3 @@ $apiRoutes = function () {
 
 Route::group(['domain' => "api." . config('app.domain'), 'prefix' => 'coworking-space', 'namespace' => 'Modules\UpCoworkingSpace\Http\Controllers'], $apiRoutes);
 
-//Route::group(['middleware' => 'web', 'domain' => config('app.domain'), 'namespace' => 'Modules\UpCoworkingSpace\Http\Controllers'], function () {
-//    Route::get('/api/blogs', 'BlogApiController@getAllBlogs');
-//    Route::get('/api/blog/{id}', 'BlogApiController@getDetailBlog');
-//});
-//Route::group(['middleware' => 'web', 'prefix' => 'upcoworkingspace', 'namespace' => 'Modules\UpCoworkingSpace\Http\Controllers'], function()
-//{
-//    Route::get('/', 'UpCoworkingSpaceController@index');
-//});
