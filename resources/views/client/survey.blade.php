750a035436950dc7773ccb5bbd4f9f4cb4ede861
Cao Anh Quan <aquancva@gmail.com>
2018-01-25 16:12:54 +0700

    up

--- resources/views/client/survey.blade.php
+++ resources/views/client/survey.blade.php
@@ -1,9 +1,9 @@
 @extends("client.layout.master")
 
 @section("css")
-    <link href="manage/surveyv2/main.c83ece5cde9e25340923fc03d2f130e1.css" rel="stylesheet">
+    <link href="manage/surveyv2/main.9ad66f859ee6ea3f1baee70c1f7ae732.css" rel="stylesheet">
 @endsection
 
 @section("js")
-    <script type="text/javascript" src="manage/surveyv2/main.7cb16ce95fa22538056d.js"></script>
+    <script type="text/javascript" src="manage/surveyv2/main.f6d91a0744e647c65187.js"></script>
 @endsection
