28a56e713c6b47eaf42ed023f1bf61e083bcdec6
seenastic <goldenrezgun@gmail.com>
2018-01-25 20:25:13 +0700

    BUILD EMAIL

--- resources/views/client/email.blade.php
+++ resources/views/client/email.blade.php
@@ -1,9 +1,9 @@
 @extends("client.layout.master")
 
 @section("css")
-    <link href="manage/email/main.06bd531772552eea10d4a8f3ac543a83.css" rel="stylesheet">
+    <link href="manage/email/main.9ad66f859ee6ea3f1baee70c1f7ae732.css" rel="stylesheet">
 @endsection
 
 @section("js")
-    <script type="text/javascript" src="manage/email/main.1da67357df9c0454d9c5.js"></script>
+    <script type="text/javascript" src="manage/email/main.c6009415ac80c54492a7.js"></script>
 @endsection
