9ba000b7e91795bddc3fb0055343b84348817f5e
Cao Anh Quan <aquancva@gmail.com>
2018-01-26 21:20:45 +0700

    add point and drag points

--- src/modules/rooms/ListRoom.js
+++ src/modules/rooms/ListRoom.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ButtonGroupAction from "../../components/common/ButtonGroupAction";
 import Loading from "../../components/common/Loading";
 import PropTypes from "prop-types";
+import {Link} from "react-router";
 
 class ListRoom extends React.Component {
     constructor(props, context) {
@@ -22,22 +23,24 @@ class ListRoom extends React.Component {
                 <div className="row">
                     {this.props.rooms && this.props.rooms.map((room) => {
                         return (
-                            <div className="col-sm-4" id="card-email-template">
+                            <div key={room.id} className="col-sm-4" id="card-email-template">
                                 <div className="card card-chart">
-                                    <div className="card-header" data-background-color="white" style={{
-                                        borderRadius: '10px'
-                                    }}>
-                                        <div id="simpleBarChart" className="ct-chart"
-                                             style={{
-                                                 width: '100%',
-                                                 background: 'url(' + 'http://s3images.coroflot.com/user_files/individual_files/large_188723_s6nH2GYL11SSVLIkNiUbpdFqh.jpg' + ')',
-                                                 backgroundSize: 'cover',
-                                                 backgroundPosition: 'center',
-                                                 height: '200px',
-                                                 borderRadius: '10px'
-                                             }}
-                                        />
-                                    </div>
+                                    <Link to={`/base/room/${room.id}`}>
+                                        <div className="card-header" data-background-color="white" style={{
+                                            borderRadius: '10px'
+                                        }}>
+                                            <div id="simpleBarChart" className="ct-chart"
+                                                 style={{
+                                                     width: '100%',
+                                                     background: 'url(' + 'http://s3images.coroflot.com/user_files/individual_files/large_188723_s6nH2GYL11SSVLIkNiUbpdFqh.jpg' + ')',
+                                                     backgroundSize: 'cover',
+                                                     backgroundPosition: 'center',
+                                                     height: '200px',
+                                                     borderRadius: '10px'
+                                                 }}
+                                            />
+                                        </div>
+                                    </Link>
                                     <div className="card-content">
                                         <div className="card-action">
                                             <h4 className="card-title">{room.name}</h4>
