549325cab4ca06c7c615735aff5cc0394ecfd7c9
LetHerGo <thanhpham1479@gmail.com>
2018-01-27 10:06:32 +0700

    up

--- Modules/UpCoworkingSpace/Http/Controllers/UpCoworkingSpaceManageApiController.php
+++ Modules/UpCoworkingSpace/Http/Controllers/UpCoworkingSpaceManageApiController.php
@@ -77,9 +77,11 @@ class UpCoworkingSpaceManageApiController extends ManageApiController
         $subscriptions = RoomServiceSubscription::where('user_pack_id', $userPackId);
 
         $subscriptions = $subscriptions->orderBy('created_at', 'desc')->get();
+        $userPack = RoomServiceUserPack::find($userPackId);
         return $this->respondSuccessWithStatus([
+            'userPack' => $userPack->getData(),
             'subscriptions' => $subscriptions->map(function ($subscription) {
-                return $subscription->getData();
+                return $subscription->transform();
             })
         ]);
     }
