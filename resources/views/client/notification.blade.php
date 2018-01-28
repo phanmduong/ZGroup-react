40aabb5a5b75645d9e4889bb1e3b5c297e769339
phanmduong <phanminhduong@hotmail.com>
2018-01-28 11:25:52 +0700

    update code

---
+++ resources/views/client/notification.blade.php
@@ -0,0 +1,9 @@
+@extends("client.layout.master")
+
+@section("css")
+    <link href="manage/notification/main.17a4718843e3b863f9c04f1f700e97a8.css" rel="stylesheet">
+@endsection
+
+@section("js")
+    <script type="text/javascript" src="manage/notification/main.5dc1810bdcbef48cfdd7.js"></script>
+@endsection
