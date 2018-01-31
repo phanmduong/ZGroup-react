31c0d279785312640cfb35362339326c12b8d574
phanmduong <phanminhduong@hotmail.com>
2018-02-01 01:17:07 +0700

    update code

--- socket-http.js
+++ socket-http.js
@@ -45,7 +45,7 @@ var sendNotificationUser = function (notification) {
         case 'mobile_manage':
             notification.device_type = 'mobile_manage';
             notification.url = 'colormemanage://main';
-            addNotificationId(notification);
+            // addNotificationId(notification);
             return sendNotification(notification, env.NOTI_APP_MANAGE_ID, env.NOTI_APP_MANAGE_KEY);
         default:
             return;
