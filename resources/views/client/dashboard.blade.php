3719dcbe430543f32589d6c1737a74b9fdfbb4ee
phanmduong <phanminhduong@hotmail.com>
2018-02-02 17:06:07 +0700

    update code

--- resources/views/client/dashboard.blade.php
+++ resources/views/client/dashboard.blade.php
@@ -1,11 +1,11 @@
 @extends("client.layout.master")
 
 @section("css")
-    <link href="manage/dashboard/main.c1997d861045ccd4202937aa425b5799.css" rel="stylesheet">
+    <link href="manage/dashboard/main.6e20e2a2abb265f193e57cff0ca73085.css" rel="stylesheet">
 @endsection
 
 @section("js")
 
-    <script type="text/javascript" src="manage/dashboard/main.4c98e50236fb8c64b10b.js"></script>
+    <script type="text/javascript" src="manage/dashboard/main.dea5abc862505df7623e.js"></script>
 
 @endsection
