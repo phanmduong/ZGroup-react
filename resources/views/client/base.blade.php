06e2ad4ab150e316d96aff63c76f48f1b67f6edb
phanmduong <phanminhduong@hotmail.com>
2018-02-02 15:08:29 +0700

    update code

--- resources/views/client/base.blade.php
+++ resources/views/client/base.blade.php
@@ -1,9 +1,9 @@
 @extends("client.layout.master")
 
 @section("css")
-    <link href="manage/base/main.4ae93bdacfcc7c8e241e1f67247f7fc2.css" rel="stylesheet">
+    <link href="manage/base/main.8f0fec4cbeba45cdd2259fb683e47766.css" rel="stylesheet">
 @endsection
 
 @section("js")
-    <script type="text/javascript" src="manage/base/main.6c958921cda371fcaebe.js"></script>
+    <script type="text/javascript" src="manage/base/main.311dfa9085e3f88bf705.js"></script>
 @endsection
