243e19eb8873cce6d49ee067c6613553a8f1a3c5
datvithanh98 <datvithanh98@gmail.com>
2018-02-04 17:07:12 +0700

    up

--- Modules/TrongDongPalace/Http/routes.php
+++ Modules/TrongDongPalace/Http/routes.php
@@ -5,5 +5,5 @@ $publicRoutes = function () {
     Route::get('/blog', 'TrongDongPalaceController@blog');
 };
 
-Route::group(['middleware' => 'web', 'domain' => "keetool6.xyz", 'namespace' => 'Modules\TrongDongPalace\Http\Controllers'], $publicRoutes);
+Route::group(['middleware' => 'web', 'domain' => config('app.domain'), 'namespace' => 'Modules\TrongDongPalace\Http\Controllers'], $publicRoutes);
 Route::group(['middleware' => 'web', 'domain' => "trongdongpalace.test", 'namespace' => 'Modules\TrongDongPalace\Http\Controllers'], $publicRoutes);
