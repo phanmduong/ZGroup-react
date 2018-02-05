dc26ef5707291ec8e453895dc0a2ac2b8afcefbe
datvithanh98 <datvithanh98@gmail.com>
2018-02-05 23:35:35 +0700

    up

--- Modules/TrongDongPalace/Http/Controllers/TrongDongPalaceController.php
+++ Modules/TrongDongPalace/Http/Controllers/TrongDongPalaceController.php
@@ -87,7 +87,7 @@ class TrongDongPalaceController extends Controller
     {
         $data = ['email' => $request->email, 'phone' => $request->phone, 'name' => $request->name, 'message_str' => $request->message_str];
 
-        Mail::send('emails.contact_us', $data, function ($m) use ($request) {
+        Mail::send('emails.contact_us_trong_dong', $data, function ($m) use ($request) {
             $m->from('no-reply@colorme.vn', 'Graphics');
             $subject = "Xác nhận thông tin";
             $m->to($request->email, $request->name)->subject($subject);
