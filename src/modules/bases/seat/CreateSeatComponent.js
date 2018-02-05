10d84214a7b529cf0d786dbff388b28ee23af014
Cao Anh Quan <aquancva@gmail.com>
2018-02-05 21:45:04 +0700

    up

--- src/modules/bases/seat/CreateSeatComponent.js
+++ src/modules/bases/seat/CreateSeatComponent.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
 import {CirclePicker} from "react-color";
 import Slider from "../../../components/common/Slider";
-import {updateSeatFormData} from "./seatActions";
 import {
     FormGroup, FormControl, ControlLabel, Badge,
     Popover, OverlayTrigger
