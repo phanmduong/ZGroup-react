4944ab92357735cf8928ad66b09c80c4784cc893
Cao Anh Quan <aquancva@gmail.com>
2018-02-11 12:13:38 +0700

    up

--- resources/views/client/dashboard.blade.php
+++ resources/views/client/dashboard.blade.php
@@ -1,11 +1,11 @@
 @extends("client.layout.master")
 
 @section("css")
-    <link href="manage/dashboard/main.9871dc6b5831d425f60d9184a2fa5a2c.css" rel="stylesheet">
+    <link href="manage/dashboard/main.45ec5a2dd16785e4b80e462763663d95.css" rel="stylesheet">
 @endsection
 
 @section("js")
 
-    <script type="text/javascript" src="manage/dashboard/main.e55076a992ef314512fd.js"></script>
+    <script type="text/javascript" src="manage/dashboard/main.f3831a3cb22d2f43ba5d.js"></script>
 
 @endsection
