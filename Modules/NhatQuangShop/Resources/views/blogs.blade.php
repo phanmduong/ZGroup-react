f672e969b06b8797b4f517f39ec7752d19b81d63
datvithanh98 <datvithanh98@gmail.com>
2018-02-16 18:12:10 +0700

    up

--- Modules/NhatQuangShop/Resources/views/blogs.blade.php
+++ Modules/NhatQuangShop/Resources/views/blogs.blade.php
@@ -1,16 +1,17 @@
 @extends('nhatquangshop::layouts.master')
 
-
 @section('content')
-    <div class="page-header page-header-xs" style="background-image: url('http://d1j8r0kxyu9tj8.cloudfront.net/files/1508035903jSFNtNO4CXL5lfZ.png');">
+    <div class="page-header page-header-xs"
+         style="background-image: url('http://trongdongpalace.com/ckfinder/userfiles/images/hannah-and-matt-1401(1).jpg'); height: 350px">
         <div class="filter"></div>
         <div class="content-center">
             <div class="container">
                 <br><br>
+                <br><br>
                 <div class="row">
                     <div class="col-md-8 offset-md-2 text-center">
-                        <h1 class="title"><b>Blogs</b></h1>
-                        <h5 class=description">Các bài viết chia sẻ kiến thức và thông tin</h5>
+                        <h4 style="font-weight: 500">BÀI VIẾT MỚI NHẤT</h4>
+                        <h2>CHIA SẺ TỪ TRỐNG ĐỒNG PALACE</h2><br>
                         <br>
                     </div>
 
@@ -19,29 +20,17 @@
         </div>
     </div>
 
-    <div class="blog-4" style="margin-top:150px">
+    <div class="blog-4" style="margin-top:20px">
         <div class="container">
-            <div class="description">
-                <h1 class="medium-title">
-                    Bài viết mới nhất<br>
-                </h1>
-                <a href="#pablo" class="btn btn-link btn-success" style="padding:0!important; margin:0!important">Xem tất cả <i class="fa fa-angle-right"></i></a><br><br><br>
-            </div>
+            <br>
+            <br>
             <div class="row">
                 @foreach($blogs as $blog)
                     <div class="col-md-4">
                         <div class="card card-plain card-blog">
                             <div class="card-image">
                                 <a href="{{'/blog/post/'.$blog->id}}">
-                                    <div
-                                            style="width: 100%;
-                                                    border-radius: 15px;
-                                                    background: url({{generate_protocol_url($blog->url)}});
-                                                    background-size: cover;
-                                                    background-position: center;
-                                                    padding-bottom: 70%;"
-
-                                    ></div>
+                                    <img class="img img-raised" src="{{generate_protocol_url($blog->url)}}">
                                 </a>
                             </div>
                             <div class="card-block">
@@ -49,10 +38,10 @@
                                     <a href="{{'/blog/post/'.$blog->id}}">{{$blog->title}}</a>
                                 </h3>
                                 <p class="card-description">
-                                    {{$blog->description}}
-                                </p>
+                                    {{shortString($blog->description, 15)}}                                </p>
                                 <br>
-                                <a href="{{'/blog/post/'.$blog->id}}" style="color:#c50000!important"><b>Xem thêm</b></a>
+                                <a href="{{'/blog/post/'.$blog->id}}" style="color:#7bc043!important"><b>Xem
+                                        thêm</b></a>
                             </div>
                         </div>
                     </div>
@@ -60,12 +49,27 @@
             </div>
 
             <hr>
-            <div class="row">
-                <div class="col-md-2 offset-md-10">
-                    <div class="pull-right">
-                        {{--<button class="btn btn-link btn-default btn-move-right">Bài viết cũ hơn<i class="fa fa-angle-right"></i></button>--}}
-                        <a class="btn btn-link btn-default btn-move-right" href="{{'/blog?page='.$page_id}}" style="{{$display}}">  Bài viết cũ hơn  </a>
-                    </div>
+            <div id="pagination-blogs">
+                <div class="pagination-area">
+                    <ul class="pagination pagination-primary justify-content-center">
+                        <li class="page-item">
+                            <a href="/blog?page=1&search={{$search}}" class="page-link">
+                                <i class="fa fa-angle-double-left" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                        <li v-for="page in pages"
+                            v-bind:class="'page-item ' + (page=={{$current_page}} ? 'active' : '')">
+                            <a v-bind:href="'/blog?page='+page+'&search={{$search}}'" class="page-link">
+                                @{{page}}
+                            </a>
+                        </li>
+                        <li class="page-item">
+                            <a href="/blog?page={{$total_pages}}&search={{$search}}" class="page-link">
+                                <i class="fa fa-angle-double-right" aria-hidden="true">
+                                </i>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
             <br>
@@ -73,3 +77,30 @@
         </div>
     </div>
 @endsection
+
+@push('scripts')
+    <script>
+        var search = new Vue({
+            el: '#search-blog',
+            data: {
+                search: '{!! $search !!}'
+            },
+            methods: {
+                searchBlog: function () {
+                    window.open('/blog?page=1&search=' + this.search, '_self');
+                }
+            },
+
+        })
+
+        var pagination = new Vue({
+            el: '#pagination-blogs',
+            data: {
+                pages: []
+            },
+        });
+
+        pagination.pages = paginator({{$current_page}},{{$total_pages}})
+    </script>
+@endpush
+
