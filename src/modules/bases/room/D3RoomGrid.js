01f9e9db3541ecac580407bc782eeae7aed84793
Cao Anh Quan <aquancva@gmail.com>
2018-02-18 21:43:32 +0700

    change grid size

--- src/modules/bases/room/D3RoomGrid.js
+++ src/modules/bases/room/D3RoomGrid.js
@@ -2,22 +2,23 @@ import * as d3 from "d3";
 
 let ns = {};
 
-ns.onClick = (onClick) => {
+ns.onClick = onClick => {
     ns.onClick = onClick;
 };
 
-ns.onPointClick = (onPointClick) => {
+ns.onPointClick = onPointClick => {
     ns.onPointClick = onPointClick;
 };
 
-ns.onDrag = (onDrag) => {
+ns.onDrag = onDrag => {
     ns.onDrag = onDrag;
 };
 
-ns.create = function (el, props, state) {
+ns.create = function(el, props, state) {
     ns.el = el;
 
-    d3.select(el)
+    d3
+        .select(el)
         .append("div")
         .classed("svg-container", true) //container class to make it responsive
         .append("svg")
@@ -26,111 +27,102 @@ ns.create = function (el, props, state) {
         .attr("viewBox", `0 0 ${props.width} ${props.height}`)
         //class to make it responsive
         .classed("svg-content-responsive d3", true);
-    d3.select(".svg-content-responsive")
-        .on("click", function () {
+    d3.select(".svg-content-responsive").on("click", function() {
+        const mouse = d3.mouse(this);
+        const x = mouse[0];
+        const y = mouse[1];
 
-            const mouse = d3.mouse(this);
-            const x = mouse[0];
-            const y = mouse[1];
+        const scale = ns._scales(el, state.domain);
+        const rescaledX = scale.x.invert(x);
+        const rescaledY = scale.y.invert(y);
 
-            const scale = ns._scales(el, state.domain);
-            const rescaledX = scale.x.invert(x);
-            const rescaledY = scale.y.invert(y);
+        const point = {
+            x: rescaledX,
+            y: rescaledY,
+        };
 
-            const point = {
-                x: rescaledX,
-                y: rescaledY
-            };
-
-            if (ns.onClick) {
-                ns.onClick(point);
-            }
-        });
+        if (ns.onClick) {
+            ns.onClick(point);
+        }
+    });
 
     this.update(el, state);
 };
 
-ns.update = function (el, state) {
-    
+ns.update = function(el, state) {
     let scales = this._scales(state);
     ns.drawGrid(state);
     this._drawPoints(el, scales, state);
     // this._drawTooltips(el, scales, state.tooltips, prevScales);
 };
 
-ns.drawGrid = ({width, height, gridSize, gridOn}) => {
-    
-    
+ns.drawGrid = ({ width, height, gridSize, gridOn }) => {
+    d3
+        .select("svg")
+        .selectAll("path")
+        .remove();
+
     if (gridOn) {
         const widthArray = generateArray(width, gridSize);
         const heightArray = generateArray(height, gridSize);
 
         const topPoints = widthArray.map(w => {
             return {
-                x: w, y : 0    
-            };        
+                x: w,
+                y: 0,
+            };
         });
         const bottomPoints = widthArray.map(w => {
             return {
-                x: w, 
-                y : height    
-            };        
+                x: w,
+                y: height,
+            };
         });
 
         const verticalLines = topPoints.map((point, index) => {
-            return [
-                point,
-                bottomPoints[index]
-            ];
+            return [point, bottomPoints[index]];
         });
 
         const leftPoints = heightArray.map(h => {
             return {
-                x: 0, 
-                y : h    
-            };        
+                x: 0,
+                y: h,
+            };
         });
         const rightPoints = heightArray.map(h => {
             return {
-                x: width, 
-                y : h    
-            };        
+                x: width,
+                y: h,
+            };
         });
 
         const horizontalLines = leftPoints.map((point, index) => {
-            return [
-                point,
-                rightPoints[index]
-            ];
+            return [point, rightPoints[index]];
         });
-        
-        const lineFunction = d3.line()
-                            .x(function(d) { 
-                                return d.x; 
-                            })
-                            .y(function(d) { 
-                                return d.y; 
-                            });
 
-        d3.select("svg")
+        const lineFunction = d3
+            .line()
+            .x(function(d) {
+                return d.x;
+            })
+            .y(function(d) {
+                return d.y;
+            });
+
+        d3
+            .select("svg")
             .selectAll("path")
             .data([...horizontalLines, ...verticalLines])
             .enter()
             .append("path")
-                .attr("d", lineFunction)
-                .attr("stroke", "rgba(0, 0, 0, 0.25)")
-                .attr("stroke-width", 1)
-                .attr("fill", "none");       
-    } else {
-        d3.select("svg")
-            .selectAll("path")
-            .remove();
+            .attr("d", lineFunction)
+            .attr("stroke", "rgba(0, 0, 0, 0.25)")
+            .attr("stroke-width", 1)
+            .attr("fill", "none");
     }
-    
 };
 
-ns._scales = function ({width}) {
-
+ns._scales = function({ width }) {
     // const el = $(".d3");
     // let width = el.width();
     // let height = el.height();
@@ -145,13 +137,14 @@ ns._scales = function ({width}) {
     //     .range([0, height])
     //     .domain([0, state.height]);
     //
-    const r = d3.scaleLinear()
+    const r = d3
+        .scaleLinear()
         .range([10, width / 18])
         .domain([1, 10]);
 
-    const x = (x) => x;
+    const x = x => x;
     x.invert = x;
-    return {x, y: x, r};
+    return { x, y: x, r };
 };
 
 const generateArray = (size, gridSize) => {
@@ -162,129 +155,126 @@ const generateArray = (size, gridSize) => {
     return arr;
 };
 
-ns._drawPoints = function (el, scales, state) {
+ns._drawPoints = function(el, scales, state) {
+    const { seats, width, height, roomLayoutUrl } = state;
 
-    const {seats, width, height, roomLayoutUrl} = state;
-
-
-    const subject = function () {
-        return {x: d3.event.x, y: d3.event.y};
+    const subject = function() {
+        return { x: d3.event.x, y: d3.event.y };
     };
 
-    d3.select(".svg-container")
+    d3
+        .select(".svg-container")
         .style("padding-bottom", height * 100 / width + "%");
-
 
     if (ns.roomLayoutUrl !== roomLayoutUrl) {
         ns.roomLayoutUrl = roomLayoutUrl;
-        d3.select("svg")
+        d3
+            .select("svg")
             .style("background-image", `url("${roomLayoutUrl}")`)
             .style("background-size", "cover")
             .style("background-position", "center");
     }
-        
+
     if (ns.width !== width || ns.height !== height) {
         ns.width = width;
         ns.height = height;
-        d3.select("svg")
-            .attr("viewBox", `0 0 ${width} ${height}`);
-    }    
+        d3.select("svg").attr("viewBox", `0 0 ${width} ${height}`);
+    }
 
-    let drag = d3.drag()
+    let drag = d3
+        .drag()
         .subject(subject)
-        .on("start", function () {
+        .on("start", function() {
             d3.event.sourceEvent.stopPropagation(); // silence other listeners
         })
-        .on("drag", function (d) {
+        .on("drag", function(d) {
             // console.log(d3.event.x + "," + d3.event.y);
 
             const x = d3.event.x;
             const y = d3.event.y;
 
-            if (Math.sqrt(Math.pow(x - d.x, 2) +  Math.pow(y - d.y, 2)) > 1) {
-                 ns.onDrag({
+            if (Math.sqrt(Math.pow(x - d.x, 2) + Math.pow(y - d.y, 2)) > 1) {
+                ns.onDrag({
                     index: d.index,
                     x,
-                    y
+                    y,
                 });
             }
-           
         })
-        .on("end", function () {
-            
-        });
-    
+        .on("end", function() {});
+
     const svg = d3.select("svg");
-    let g = svg.selectAll("g")
+    let g = svg
+        .selectAll("g")
         .data(seats)
         .enter()
         .append("g")
         .attr("class", "seat-wrapper")
         .call(drag);
 
-    g.append("circle")
-        .attr('class', 'd3-point');
+    g.append("circle").attr("class", "d3-point");
 
-    g.append("text")
-        .attr('class', 'd3-text');
-    
-    svg.selectAll("g")
+    g.append("text").attr("class", "d3-text");
+
+    svg
+        .selectAll("g")
         .data(seats)
-        .exit().remove();
-    
-    g = d3.selectAll("g")
-            .data(seats)
-            .on('click', function (d) {
-                // d3.event.stopPropagation();
-                // console.log(d);
-                ns.onPointClick(d.index);
-            });
+        .exit()
+        .remove();
+
+    g = d3
+        .selectAll("g")
+        .data(seats)
+        .on("click", function(d) {
+            // d3.event.stopPropagation();
+            // console.log(d);
+            ns.onPointClick(d.index);
+        });
 
     g.attr("transform", d => "translate(" + d.x + "," + d.y + ")");
 
-    const transformTrasition = d3.transition()
+    const transformTrasition = d3
+        .transition()
         .duration(200)
         .ease(d3.easeLinear);
 
-    svg.selectAll(".d3-point")
+    svg
+        .selectAll(".d3-point")
         .data(seats)
         .transition(transformTrasition)
-        .attr('r', (d) => scales.r(d.r))
-        .attr('class', (d) => {
+        .attr("r", d => scales.r(d.r))
+        .attr("class", d => {
             if (d.archived) {
-                return 'd3-point archived';
+                return "d3-point archived";
             }
             if (d.active) {
-                return 'd3-point active';
+                return "d3-point active";
             }
-            return'd3-point';
+            return "d3-point";
         })
-        .style("fill", function (d) {
+        .style("fill", function(d) {
             if (d.booked) {
                 return "#bbb";
             }
             return d.color;
         });
 
-    svg.selectAll(".d3-text")
-        .data(seats)    
+    svg
+        .selectAll(".d3-text")
+        .data(seats)
         .transition(transformTrasition)
         .attr("dy", d => scales.r(d.r) / 3)
         .attr("text-anchor", "middle")
         .attr("fill", "white")
         .attr("font-size", d => scales.r(d.r))
-        .attr('class', (d) => {
-            return d.active ? 'd3-text active' : 'd3-text';
+        .attr("class", d => {
+            return d.active ? "d3-text active" : "d3-text";
         })
-        .text(function (d) {
+        .text(function(d) {
             return d.name || "";
         });
-
 };
 
-ns.destroy = function () {
-
-};
-
+ns.destroy = function() {};
 
 export default ns;
--- src/modules/bases/seat/CreateSeatComponent.js
+++ src/modules/bases/seat/CreateSeatComponent.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {CirclePicker} from "react-color";
+import React from "react";
+import PropTypes from "prop-types";
+import { CirclePicker } from "react-color";
 import Slider from "../../../components/common/Slider";
 import {
-    FormGroup, FormControl, ControlLabel, Badge,
-    Popover, OverlayTrigger
-}
-    from 'react-bootstrap';
+    FormGroup,
+    FormControl,
+    ControlLabel,
+    Badge,
+    Popover,
+    OverlayTrigger,
+} from "react-bootstrap";
 
 const propTypes = {
     seat: PropTypes.object.isRequired,
-    updateSeatFormData: PropTypes.func.isRequired
+    updateSeatFormData: PropTypes.func.isRequired,
 };
 
 class CreateSeatComponent extends React.Component {
@@ -23,7 +26,7 @@ class CreateSeatComponent extends React.Component {
     }
 
     handleChange(event) {
-        const seat = {...this.props.seat};
+        const seat = { ...this.props.seat };
         const field = event.target.name;
         seat[field] = event.target.value;
         this.props.updateSeatFormData(seat);
@@ -32,35 +35,34 @@ class CreateSeatComponent extends React.Component {
     changeColor(color) {
         this.props.updateSeatFormData({
             ...this.props.seat,
-            color: color.hex
+            color: color.hex,
         });
     }
 
     changeSlider(value) {
         this.props.updateSeatFormData({
             ...this.props.seat,
-            r: Number(value)
+            r: Number(value),
         });
     }
 
     render() {
-
-        const {seat} = this.props;
+        const { seat } = this.props;
 
         const popoverColor = (
             <Popover id="popover-positioned-bottom" title="Chọn màu ghế">
                 <CirclePicker
                     width="100%"
                     color={seat.color || "rgb(244, 67, 54)"}
-                    onChangeComplete={this.changeColor}/>
+                    onChangeComplete={this.changeColor}
+                />
             </Popover>
         );
 
         return (
             <div className="row">
-                <div className="col-sm-4">
-                    <FormGroup
-                        controlId="seat-name">
+                <div className="col-xs-4">
+                    <FormGroup controlId="seat-name">
                         <ControlLabel>Tên chỗ ngồi</ControlLabel>
                         <FormControl
                             type="text"
@@ -72,37 +74,45 @@ class CreateSeatComponent extends React.Component {
                     </FormGroup>
                 </div>
 
-                <div className="col-sm-4">
+                <div className="col-xs-4">
                     <FormGroup>
                         <ControlLabel>
-                            <span style={{marginRight: 5}}>Kích thước ghế</span>
+                            <span style={{ marginRight: 5 }}>
+                                Kích thước ghế
+                            </span>
                             <Badge>{parseInt(seat.r || 1)}</Badge>
                         </ControlLabel>
                         <Slider
+                            id="seatSize"
                             onChange={this.changeSlider}
                             step={1}
                             value={seat.r || 1}
                             min={1}
-                            max={10}/>
+                            max={10}
+                        />
                     </FormGroup>
                 </div>
 
-
-                <div className="col-sm-4">
+                <div className="col-xs-4">
                     <FormGroup>
                         <ControlLabel>Màu ghế</ControlLabel>
                         <div>
-                            <OverlayTrigger trigger="click"
-                                            rootClose
-                                            placement="bottom" overlay={popoverColor}>
-                                <div style={{
-                                    width: 30,
-                                    height: 30,
-                                    cursor: "pointer",
-                                    borderRadius: "50%",
-                                    border: "1px solid #d9d9d9",
-                                    background: `${seat.color}`
-                                }}/>
+                            <OverlayTrigger
+                                trigger="click"
+                                rootClose
+                                placement="bottom"
+                                overlay={popoverColor}
+                            >
+                                <div
+                                    style={{
+                                        width: 30,
+                                        height: 30,
+                                        cursor: "pointer",
+                                        borderRadius: "50%",
+                                        border: "1px solid #d9d9d9",
+                                        background: `${seat.color}`,
+                                    }}
+                                />
                             </OverlayTrigger>
                         </div>
                     </FormGroup>
