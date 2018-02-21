71d7c82bda6a6f64f197ecdab10cd036af781b7b
datvithanh98 <datvithanh98@gmail.com>
2018-02-21 17:43:33 +0700

    up

--- Modules/Order/Http/Controllers/TransferMoneyApiController.php
+++ Modules/Order/Http/Controllers/TransferMoneyApiController.php
@@ -64,12 +64,13 @@ class TransferMoneyApiController extends ManageApiController
         $transfer = TransferMoney::find($transferId);
         if ($transfer == null)
             return $this->respondErrorWithStatus('Không tồn tại chuyển khoản');
-        if($transfer->status == 'accept' || $transfer->status == 'cancel')
+        if ($transfer->status == 'accept' || $transfer->status == 'cancel')
             return $this->respondErrorWithStatus('Không cho phép chuyển trạng thái chấp nhận hoặc hủy');
-        if($request->status == 'accept')
-        {
+        if ($request->status == 'accept') {
             //cong tien vao vi user o day
         }
+        if ($request->status == 'cancel')
+            $transfer->note = $request->note;
         $transfer->status = $request->status;
         $transfer->save();
         return $this->respondSuccess('Đổi trạng thái thành công');
