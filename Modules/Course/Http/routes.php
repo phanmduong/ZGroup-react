c07bdc2c26016218a61a241d0e6d9d6451d39102
datvithanh98 <datvithanh98@gmail.com>
2018-02-23 17:22:11 +0700

    up

--- Modules/Course/Http/routes.php
+++ Modules/Course/Http/routes.php
@@ -16,10 +16,10 @@ Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => 'cour
 });
 
 Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => '/v2/course', 'namespace' => 'Modules\Course\Http\Controllers'], function () {
-    Route::get('/get-all','CourseController@getAllCourses');
+    Route::get('/get-all','CoursePublicApiController@getAllCourses');
     Route::get('/all','CourseController@getAll');
     Route::delete('/delete/{course_id}','CourseController@deleteCourse');
-    Route::get('/get-detailed/{course_id}', 'CourseController@getCourse');
+    Route::get('/get-detailed/{course_id}', 'CoursePublicApiController@getCourse');
     Route::post('/create-edit', 'CourseController@createOrEdit');
     Route::get('/get-detailed-link/{link_id}', 'CourseController@detailedLink');
     Route::post('/create-link', 'CourseController@createLink');
