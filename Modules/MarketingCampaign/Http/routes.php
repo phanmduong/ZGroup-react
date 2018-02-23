f00ee4b3345de7731fc6ef37abe9d3d18eec9ea6
datvithanh98 <datvithanh98@gmail.com>
2018-02-23 21:57:06 +0700

    up

--- Modules/MarketingCampaign/Http/routes.php
+++ Modules/MarketingCampaign/Http/routes.php
@@ -10,3 +10,10 @@ Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => 'mark
 Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => 'sales', 'namespace' => 'Modules\MarketingCampaign\Http\Controllers'], function () {
     Route::get('/summary', 'MarketingCampaignController@summarySales');
 });
+
+Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => 'room-service', 'namespace' => 'Modules\MarketingCampaign\Http\Controllers'], function () {
+    Route::get('/all', 'MarketingCampaignController@getAll');
+    Route::get('/summary', 'MarketingCampaignController@summaryMarketingCampaign');
+    Route::get('/sumary-register', 'MarketingCampaignController@summaryMarketingRegister');
+    Route::post('/store', 'MarketingCampaignController@storeMarketingCampaign');
+});
