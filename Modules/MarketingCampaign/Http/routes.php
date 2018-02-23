1f2185341eb41319a90fa8e0f95f5ea1ea6a9443
datvithanh98 <datvithanh98@gmail.com>
2018-02-24 00:22:00 +0700

    up

--- Modules/MarketingCampaign/Http/routes.php
+++ Modules/MarketingCampaign/Http/routes.php
@@ -12,8 +12,9 @@ Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => 'sale
 });
 
 Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => 'room-service', 'namespace' => 'Modules\MarketingCampaign\Http\Controllers'], function () {
-    Route::get('/all', 'MarketingCampaignController@getAll');
-    Route::get('/summary', 'MarketingCampaignController@summaryMarketingCampaign');
-    Route::get('/sumary-register', 'MarketingCampaignController@summaryMarketingRegister');
-    Route::post('/store', 'MarketingCampaignController@storeMarketingCampaign');
+    Route::get('/marketing-campaign/all', 'RoomServiceMarketingCampaignController@getAll');
+    Route::get('/marketing-campaign/summary', 'RoomServiceMarketingCampaignController@summaryMarketingCampaign');
+    Route::get('/marketing-campaign/sumary-register', 'RoomServiceMarketingCampaignController@summaryMarketingRegister');
+    Route::post('/marketing-campaign/store', 'RoomServiceMarketingCampaignController@storeMarketingCampaign');
+    Route::get('/sales/summary', 'MarketingCampaignController@summarySales');
 });
