3bf999b1b0a746201781449d986ff934af3a9281
datvithanh98 <datvithanh98@gmail.com>
2018-02-25 14:38:53 +0700

    up

--- Modules/Base/Http/routes.php
+++ Modules/Base/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 $baseRoutes = function () {
-    Route::group(['prefix' => 'v2/base'], function (){
+    Route::group(['prefix' => 'v2/base'], function () {
         Route::get('/', 'ManageBaseApiController@getBases');
         Route::post('/', 'ManageBaseApiController@createBase');
         Route::put('/{baseId}/display', 'ManageBaseApiController@createBase');
@@ -18,44 +18,56 @@ $baseRoutes = function () {
 };
 
 $provinceRoutes = function () {
-    Route::group(['prefix' => 'v2/province'], function () {
-
+    Route::group(['prefix' => 'province'], function () {
+        Route::get('/all', 'ManageBaseApiController@getAllProvinces');
     });
 };
 
-$provinceRoutes = function () {
-    Route::group(['prefix' => 'v2/province'], function () {
+$roomRoutes = function () {
+    Route::group(['prefix' => 'v2/room'], function () {
+        Route::post('/{roomId}/seat', 'ManageBaseApiController@createSeat');
+        Route::get('/{roomId}/seats', 'ManageBaseApiController@getSeats');
+        Route::post('/{roomId}/seats', 'ManageBaseApiController@createSeats');
+        Route::put('/seat/{seatId}', 'ManageBaseApiController@updateSeat');
+        Route::put('/{roomId}/seat/{seatId}', 'ManageBaseApiController@editSeat');
+        Route::post('/{roomId}/layout', 'ManageBaseApiController@roomLayout');
+    });
+};
 
+$seatRoutes = function () {
+    Route::group(['prefix' => 'v2/seat'], function () {
+        Route::get('available', 'ManageBaseApiController@availableSeats');
+    });
+};
+
+$routes = function () {
+    Route::group(['prefix' => 'v2'], function () {
+        Route::get('/base/provinces', 'PublicApiController@provinces');
+        Route::get('/base/province/{provinceId}', 'PublicApiController@basesInProvince');
+        Route::get('/base/{baseId}/room', 'PublicApiController@baseRooms');
+        Route::get('/blogs', 'PublicApiController@getAllBlogs');
+        Route::get('/blog/{id}', 'PublicApiController@getDetailBlog');
     });
 };
 
 Route::group(['domain' => 'manageapi.' . config('app.domain'), 'namespace' => 'Modules\Base\Http\Controllers'], $baseRoutes);
 
-Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => '/province', 'namespace' => 'Modules\Base\Http\Controllers'], function () {
-    Route::get('/all', 'ManageBaseApiController@getAllProvinces');
-});
+Route::group(['domain' => 'manageapi.' . config('app.domain'), 'namespace' => 'Modules\Base\Http\Controllers'], $provinceRoutes);
 
-Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => '/v2/room', 'namespace' => 'Modules\Base\Http\Controllers'], function () {
-    Route::post('/{roomId}/seat', 'ManageBaseApiController@createSeat');
-    Route::get('/{roomId}/seats', 'ManageBaseApiController@getSeats');
-    Route::post('/{roomId}/seats', 'ManageBaseApiController@createSeats');
-    Route::put('/seat/{seatId}', 'ManageBaseApiController@updateSeat');
-    Route::put('/{roomId}/seat/{seatId}', 'ManageBaseApiController@editSeat');
-    Route::post('/{roomId}/layout', 'ManageBaseApiController@roomLayout');
-});
+Route::group(['domain' => 'manageapi.' . config('app.domain'), 'namespace' => 'Modules\Base\Http\Controllers'], $roomRoutes);
 
-Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => '/v2/seat', 'namespace' => 'Modules\Base\Http\Controllers'], function () {
-    Route::get('available', 'ManageBaseApiController@availableSeats');
-});
+Route::group(['domain' => 'manageapi.' . config('app.domain'), 'namespace' => 'Modules\Base\Http\Controllers'], $seatRoutes);
 
-$routes = function () {
-    Route::get('/base/provinces', 'PublicApiController@provinces');
-    Route::get('/base/province/{provinceId}', 'PublicApiController@basesInProvince');
-    Route::get('/base/{baseId}/room', 'PublicApiController@baseRooms');
-    Route::get('/blogs', 'PublicApiController@getAllBlogs');
-    Route::get('/blog/{id}', 'PublicApiController@getDetailBlog');
-};
-Route::group(['domain' => 'api.' . config('app.domain'), 'prefix' => '/v2', 'namespace' => 'Modules\Base\Http\Controllers'], $routes);
+Route::group(['domain' => 'api.' . config('app.domain'), 'namespace' => 'Modules\Base\Http\Controllers'], $routes);
 
-// new routes
-Route::group(['domain' => config('app.domain'), 'prefix' => '/api/v3', 'namespace' => 'Modules\Base\Http\Controllers'], $routes);
+// new api routes
+
+Route::group(['domain' => config('app.domain'), 'prefix' => 'manageapi', 'namespace' => 'Modules\Base\Http\Controllers'],
+    function () use ($baseRoutes, $provinceRoutes, $roomRoutes, $seatRoutes) {
+        Route::group(['prefix' => 'v3'], $baseRoutes);
+        Route::group(['prefix' => 'v3'], $provinceRoutes);
+        Route::group(['prefix' => 'v3'], $roomRoutes);
+        Route::group(['prefix' => 'v3'], $seatRoutes);
+    });
+
+Route::group(['domain' => config('app.domain'), 'prefix' => 'api/v3', 'namespace' => 'Modules\Base\Http\Controllers'], $routes);
