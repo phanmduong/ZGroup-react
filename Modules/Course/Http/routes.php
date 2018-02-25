d19cb61da307b0c8bc6ca86fe91eb89d22e85602
Cao Anh Quan <aquancva@gmail.com>
2018-02-25 11:12:55 +0700

    fix .api

--- Modules/Course/Http/routes.php
+++ Modules/Course/Http/routes.php
@@ -5,9 +5,9 @@ Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => 'cour
     Route::delete('/pixel/{pixelId}', 'PixelApiController@deletePixel');
 
     Route::get('/type', 'CourseTypeApiController@getTypes');
-    Route::post('/type','CourseTypeApiController@addType');
-    Route::put('/type/{typeId}','CourseTypeApiController@editType');
-    Route::delete('/type/{typeId}','CourseTypeApiController@deleteType');
+    Route::post('/type', 'CourseTypeApiController@addType');
+    Route::put('/type/{typeId}', 'CourseTypeApiController@editType');
+    Route::delete('/type/{typeId}', 'CourseTypeApiController@deleteType');
 
     Route::get('/category', 'CourseCategoryApiController@getCategories');
     Route::post('/category', 'CourseCategoryApiController@createCategory');
@@ -16,9 +16,9 @@ Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => 'cour
 });
 
 Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => '/v2/course', 'namespace' => 'Modules\Course\Http\Controllers'], function () {
-    Route::get('/get-all','CourseController@getAllCourses');
-    Route::get('/all','CourseController@getAll');
-    Route::delete('/delete/{course_id}','CourseController@deleteCourse');
+    Route::get('/get-all', 'CourseController@getAllCourses');
+    Route::get('/all', 'CourseController@getAll');
+    Route::delete('/delete/{course_id}', 'CourseController@deleteCourse');
     Route::get('/get-detailed/{course_id}', 'CourseController@getCourse');
     Route::post('/create-edit', 'CourseController@createOrEdit');
     Route::get('/get-detailed-link/{link_id}', 'CourseController@detailedLink');
@@ -27,14 +27,16 @@ Route::group(['domain' => 'manageapi.' . config('app.domain'), 'prefix' => '/v2/
     Route::delete('/delete-link/{link_id}', 'CourseController@deleteLink');
     Route::post('/lesson/add/{courseId}', 'CourseController@addLesson');
     Route::put('/lesson/edit/{lessonId}', 'CourseController@editLesson');
-    Route::get('/get-attendance-lesson/{classId}/{lessonId}','CourseController@getAttendance');
-    Route::post('/change-attendances','CourseController@changeAttendance');
-    Route::put('/{course_id}/change-status','CourseController@changeStatusCourse');
-    Route::post('/{courseId}/duplicate','CourseController@duplicateCourse');
+    Route::get('/get-attendance-lesson/{classId}/{lessonId}', 'CourseController@getAttendance');
+    Route::post('/change-attendances', 'CourseController@changeAttendance');
+    Route::put('/{course_id}/change-status', 'CourseController@changeStatusCourse');
+    Route::post('/{courseId}/duplicate', 'CourseController@duplicateCourse');
 });
 
-Route::group(['domain' => 'api.' . config('app.domain'), 'prefix' => 'apiv2', 'namespace' => 'Modules\Course\Http\Controllers'], function () {
+$classRoutes = function () {
     Route::get('/gens/{genId}/classes', 'ClassApiController@genClasses');
     Route::get('/class/{classId}/attendance/lessons', 'ClassApiController@classLessons');
-    Route::get('/gens/teachers','ClassApiController@getAllTeacher');
-});
+    Route::get('/gens/teachers', 'ClassApiController@getAllTeacher');
+};
+Route::group(['domain' => 'api.' . config('app.domain'), 'prefix' => 'apiv2', 'namespace' => 'Modules\Course\Http\Controllers'], $classRoutes);
+Route::group(['domain' => config('app.domain'), 'prefix' => '/api/v3', 'namespace' => 'Modules\Course\Http\Controllers'], $classRoutes);
