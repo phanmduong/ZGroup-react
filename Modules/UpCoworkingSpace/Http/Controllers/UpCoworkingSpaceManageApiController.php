7e025573b8ce99151ff9dca3574dc8b066fed022
datvithanh98 <datvithanh98@gmail.com>
2018-02-26 14:30:01 +0700

    up

--- Modules/UpCoworkingSpace/Http/Controllers/UpCoworkingSpaceManageApiController.php
+++ Modules/UpCoworkingSpace/Http/Controllers/UpCoworkingSpaceManageApiController.php
@@ -40,6 +40,8 @@ class UpCoworkingSpaceManageApiController extends ManageApiController
                 $registers = $registers->where('base_id', $request->base_id);
             if ($request->staff_id)
                 $registers = $registers->where('staff_id', $request->staff_id);
+            if($request->saler_id)
+                $registers = $registers->where('saler_id', $request->saler_id);
             if ($request->campaign_id)
                 $registers = $registers->where('campaign_id', $request->campaign_id);
             if ($request->status)
