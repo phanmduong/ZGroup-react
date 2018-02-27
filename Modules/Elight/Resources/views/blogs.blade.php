13a96f0f89e06c3520cba5402c9016dbae6203ec
LetHerGo <thanhpham1479@gmail.com>
2018-02-27 12:11:00 +0700

    up

--- Modules/Elight/Resources/views/blogs.blade.php
+++ Modules/Elight/Resources/views/blogs.blade.php
@@ -1,7 +1,8 @@
 @extends('elight::layouts.master')
 
 @section('content')
-    <div class="page-header page-header-xs" style="background-image: url('http://d1j8r0kxyu9tj8.cloudfront.net/files/15132416649MjXr1VTKC53cHy.png');">
+    <div class="page-header page-header-xs"
+         style="background-image: url('http://d1j8r0kxyu9tj8.cloudfront.net/files/15132416649MjXr1VTKC53cHy.png');">
         <div class="filter"></div>
         <div class="content-center">
             <div class="container">
@@ -25,7 +26,8 @@
                 <h1 class="medium-title">
                     Bài viết mới nhất<br>
                 </h1>
-                <a href="#pablo" class="btn btn-link btn-google" style="padding:0!important; margin:0!important">Xem tất cả <i class="fa fa-angle-right"></i></a><br><br><br>
+                <a href="#pablo" class="btn btn-link btn-google" style="padding:0!important; margin:0!important">Xem tất
+                    cả <i class="fa fa-angle-right"></i></a><br><br><br>
             </div>
             <div class="row">
                 @foreach($blogs as $blog)
@@ -45,7 +47,12 @@
                                 </a>
                             </div>
                             <div class="card-block">
-                                <span class="label label-danger">{{$blog->category->name}}</span>
+
+                                @if($blog->category)
+                                    <span class="label label-danger">{{$blog->category->name}}</span>
+                                @else
+                                    <span class="label label-danger">NONE</span>
+                                @endif
                                 <h3 class="card-title">
                                     <a href="{{'/blog/post/'.$blog->id}}">{{$blog->title}}</a>
                                 </h3>
@@ -53,7 +60,8 @@
                                     {{$blog->description}}
                                 </p>
                                 <br>
-                                <a href="{{'/blog/post/'.$blog->id}}" style="color:#c50000!important"><b>Xem thêm</b></a>
+                                <a href="{{'/blog/post/'.$blog->id}}" style="color:#c50000!important"><b>Xem
+                                        thêm</b></a>
                             </div>
                         </div>
                     </div>
@@ -65,7 +73,8 @@
                 <div class="col-md-2 offset-md-10">
                     <div class="pull-right">
                         {{--<button class="btn btn-link btn-default btn-move-right">Bài viết cũ hơn<i class="fa fa-angle-right"></i></button>--}}
-                        <a class="btn btn-link btn-default btn-move-right" href="{{'/blog?page='.$page_id}}" style="{{$display}}">  Bài viết cũ hơn  </a>
+                        <a class="btn btn-link btn-default btn-move-right" href="{{'/blog?page='.$page_id}}"
+                           style="{{$display}}"> Bài viết cũ hơn </a>
                     </div>
                 </div>
             </div>
