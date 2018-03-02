6360c4c8651f65d3162b2c99b037a33f6458f2fa
datvithanh98 <datvithanh98@gmail.com>
2018-03-02 13:09:08 +0700

    up

--- Modules/Order/Http/Controllers/TransferMoneyApiController.php
+++ Modules/Order/Http/Controllers/TransferMoneyApiController.php
@@ -29,8 +29,8 @@ class TransferMoneyApiController extends ManageApiController
         if ($request->bank_account_id)
             $transfers = $transfers->where('bank_account_id', $request->bank_account_id);
         $transfer = $transfers->join('users', 'users.id', '=', 'transfer_money.user_id');
-        $transfer = $transfers->where('users.name', 'like', '%' . $request->search . '%')->groupBy('transfer_money.id');
-
+        $transfer = $transfers->where('users.name', 'like', '%' . $request->search . '%')->groupBy('transfer_money.id')
+            ->select('transfer_money.*');
         if ($limit == -1) {
             $transfers = $transfers->orderBy('created_at', 'desc')->get();
             return $this->respondSuccessWithStatus([
