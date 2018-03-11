268cfbb3a534c4efa9d2a44e7899f809bf680697
Batman Bruce Wayne <taideptrai02@gmail.com>
2018-03-11 22:06:46 +0700

    nhat quang

--- Modules/Finance/Http/routes.php
+++ Modules/Finance/Http/routes.php
@@ -2,6 +2,8 @@
 
 Route::group(['prefix' => 'finance', 'namespace' => 'Modules\Finance\Http\Controllers'], function () {
     Route::get('/bank-accounts', 'FinanceManageApiController@getBankAccounts');
+    Route::post('/bank-accounts', 'FinanceManageApiController@createBankAccount');
+    Route::put('/bank-accounts/{bankAccountId}','FinanceManageApiController@editBankAccount');
 
     Route::group(['prefix' => 'bank-transfer'], function () {
         Route::get('/', 'FinanceManageApiController@bankTransfers');
