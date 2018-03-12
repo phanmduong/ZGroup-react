3502e36ccac3f741cd8bdf6f92a849af22f83c08
phanmduong <phanminhduong@hotmail.com>
2018-03-12 23:30:23 +0700

    update code

--- app/Http/Controllers/ManageRegisterStudentApiController.php
+++ app/Http/Controllers/ManageRegisterStudentApiController.php
@@ -161,12 +161,9 @@ class ManageRegisterStudentApiController extends ManageApiController
         $register = Register::find($registerId);
         if ($register->code) {
             $prefix = substr($register->code, 0, strlen(config('app.prefix_code_wait')));
-            $code = Register::orderBy('code', 'desc')->first()->code;
 
-            if ($prefix == config('app.prefix_code_wait') && count(explode(config('app.prefix_code_wait'), $code)) > 1) {
-                $nextNumber = explode(config('app.prefix_code_wait'), $code)[1] + 1;
-                $nextCode = config('app.prefix_code') . $nextNumber;
-                $register->code = $nextCode;
+            if ($prefix == config('app.prefix_code_wait')) {
+                $register->code = next_code()['next_code'];
                 $register->save();
             }
         }
