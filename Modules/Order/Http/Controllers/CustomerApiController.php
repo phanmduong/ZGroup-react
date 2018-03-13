87036c6df3c575f25018a20c2465860f1b47a269
datvithanh98 <datvithanh98@gmail.com>
2018-03-13 22:06:51 +0700

    up

--- Modules/Order/Http/Controllers/CustomerApiController.php
+++ Modules/Order/Http/Controllers/CustomerApiController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Colorme\Transformers\DeliveryOrderTransformer;
 
-class CustomerController extends ManageApiController
+class CustomerApiController extends ManageApiController
 {
     public function __construct(DeliveryOrderTransformer $deliveryOrderTransformer)
     {
@@ -20,34 +20,37 @@ class CustomerController extends ManageApiController
     public function customers(Request $request)
     {
         $limit = $request->limit ? $request->limit : 20;
+        $keyword = $request->search;
         $customers = User::join('orders', 'users.id', '=', 'orders.user_id');
 
-        $customers = $customers->select('users.*', DB::raw('sum(orders.paid_status = 0) as count'));
-        $customers = $customers->where('orders.status', '<>', 'cancel');
+        $customers = $customers->select('users.*', DB::raw('sum(orders.status_paid = 0) as count'));
+        $customers = $customers->where('orders.status', '<>', 'cancel')->where('orders.type', '<>', 'import');
         $customers = $customers->groupBy('users.id');
         if ($request->status) {
+            // $customers = $customers->get();
+            // dd($customers);
             if ($request->status == 'paid')
-                $customers = $customers->having('count', '=', 0);
+                $customers = $customers->having(DB::raw('sum(orders.status_paid = 0)'), '=', 0);
             if ($request->status == 'debt')
-                $customers = $customers->having('count', '>', 0);
+                $customers = $customers->having(DB::raw('sum(orders.status_paid = 0)'), '>', 0);
         }
         $customers = $customers->where(function ($query) use ($keyword) {
-            $query->where('name', 'like', '%' . $keyword . '%')
-                ->orWhere('email', 'like', '%' . $keyword . '%')
-                ->orWhere('phone', 'like', '%' . $keyword . '%');
+            $query->where('users.name', 'like', '%' . $keyword . '%')
+                ->orWhere('users.email', 'like', '%' . $keyword . '%')
+                ->orWhere('users.phone', 'like', '%' . $keyword . '%');
         });
         $customers = $customers->paginate($limit);
         return $this->respondWithPagination($customers, [
             'customers' => $customers->map(function ($customer) {
                 $data = [
-                    'id' => $user->id,
-                    'name' => $user->name,
-                    'phone' => $user->phone,
-                    'email' => $user->email,
-                    'deposit' => $user->deposit,
-                    'money' => $user->money,
+                    'id' => $customer->id,
+                    'name' => $customer->name,
+                    'phone' => $customer->phone,
+                    'email' => $customer->email,
+                    'deposit' => $customer->deposit,
+                    'money' => $customer->money,
                 ];
-                $data['orders'] = $customer->orders->map(function($order){
+                $data['orders'] = $customer->orders->map(function ($order) {
                     return $order->transform();
                 });
                 $data['delivery_orders'] = $this->deliveryOrderTransformer->transformCollection($customer->deliveryOrders);
