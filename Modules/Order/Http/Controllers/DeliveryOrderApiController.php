98ac0504e8ea3b8a97726fcf9ead3f54c588e6b8
datvithanh98 <datvithanh98@gmail.com>
2018-03-22 23:38:30 +0700

    up

--- Modules/Order/Http/Controllers/DeliveryOrderApiController.php
+++ Modules/Order/Http/Controllers/DeliveryOrderApiController.php
@@ -129,7 +129,7 @@ class DeliveryOrderApiController extends ManageApiController
 
         return $this->respondSuccessWithStatus([
             'total_delivery_orders' => count($deliveryOrders),
-            'not_locked' => Order::where('type', 'delivery')->where('status', 'place_order')->count,
+            'not_locked' => Order::where('type', 'delivery')->where('status', 'place_order')->count(),
             'total_money' => $deliveryOrders->reduce(function($total, $order){
                 return $total + $order->price + $order->ship_money;
             }, 0),
