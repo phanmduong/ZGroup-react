e748a70d0d072922e58763b9faed18c8f9a4e251
kstn-dailoi <dailoi.kstn@gmail.com>
2018-04-02 15:36:23 +0700

    Loi sua giao dien ColorMe

--- src/modules/manageStaff/ListStaff.js
+++ src/modules/manageStaff/ListStaff.js
@@ -17,14 +17,8 @@ class ListStaff extends React.Component {
     render() {
         let {staffs, roles, bases, departments} = this.props;
         return (
-            <div className="col-md-12">
-                <div className="card">
-                    <div className="card-header card-header-icon" data-background-color="rose">
-                        <i className="material-icons">assignment</i>
-                    </div>
-                    <div className="card-content">
-                        <h4 className="card-title">{this.props.titleList}</h4>
-                        <div className="table-responsive">
+                <div className="col-md-12">
+                    <div className="table-responsive">
                             <table className="table">
                                 <thead className="text-rose">
                                 <tr>
@@ -130,10 +124,8 @@ class ListStaff extends React.Component {
                                 </tbody>
                             </table>
                         </div>
-                    </div>
-                </div>
-            </div>
-
+                </div>        
+                 
         );
     }
 }
