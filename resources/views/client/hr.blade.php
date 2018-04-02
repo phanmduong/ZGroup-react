f9cbaafaf8ddd99918662c71628e501eb9f7c4aa
Cao Anh Quan <aquancva@gmail.com>
2018-04-02 17:50:12 +0700

    up

--- resources/views/client/hr.blade.php
+++ resources/views/client/hr.blade.php
@@ -1,9 +1,9 @@
 @extends("client.layout.master")
 
 @section("css")
-    <link href="manage/hr/main.9c30782349967f77cde8ab323b964108.css" rel="stylesheet">
+    <link href="manage/hr/main.7a977b9eeb9c00b9854f88ce88ff1b79.css" rel="stylesheet">
 @endsection
 
 @section("js")
-    <script type="text/javascript" src="manage/hr/main.b0870f978adab750d77d.js"></script>
+    <script type="text/javascript" src="manage/hr/main.30523305a811c508da5e.js"></script>
 @endsection
