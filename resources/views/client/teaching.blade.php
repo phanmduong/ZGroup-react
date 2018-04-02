bcc443e147e1ff218b7f5531cd7077db1fe64ab6
kstn-dailoi <dailoi.kstn@gmail.com>
2018-04-02 22:05:24 +0700

    build teaching

--- resources/views/client/teaching.blade.php
+++ resources/views/client/teaching.blade.php
@@ -1,9 +1,9 @@
 @extends("client.layout.master")
 
 @section("css")
-    <link href="manage/teaching/main.9c30782349967f77cde8ab323b964108.css" rel="stylesheet">
+    <link href="manage/teaching/main.7a977b9eeb9c00b9854f88ce88ff1b79.css" rel="stylesheet">
 @endsection
 
 @section("js")
-    <script type="text/javascript" src="manage/teaching/main.4a18b6cbd719f003ac75.js"></script>
+    <script type="text/javascript" src="manage/teaching/main.70b19240d1a54db4eeda.js"></script>
 @endsection
