dcca5f125028baded13428af3d1d950c9f88ea8f
kstn-dailoi <dailoi.kstn@gmail.com>
2018-04-02 19:40:08 +0700

    Build module marketing

--- resources/views/client/marketing.blade.php
+++ resources/views/client/marketing.blade.php
@@ -1,9 +1,9 @@
 @extends("client.layout.master")
 
 @section("css")
-    <link href="manage/marketing/main.1c20a205e06fa09dfd05105d7a345529.css" rel="stylesheet">
+    <link href="manage/marketing/main.7ebe97ee48be7a4cb704c696fae7acd1.css" rel="stylesheet">
 @endsection
 
 @section("js")
-    <script type="text/javascript" src="manage/marketing/main.828aa78a00cdafa25915.js"></script>
+    <script type="text/javascript" src="manage/marketing/main.67eb82277d75d8f63507.js"></script>
 @endsection
