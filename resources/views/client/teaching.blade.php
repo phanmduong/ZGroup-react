acccf572bbda932e1f2170eab8a0eb0dcc84d75a
Cao Anh Quan <aquancva@gmail.com>
2018-04-03 17:47:20 +0700

    up

--- resources/views/client/teaching.blade.php
+++ resources/views/client/teaching.blade.php
@@ -1,9 +1,9 @@
 @extends("client.layout.master")
 
 @section("css")
-    <link href="manage/teaching/main.9c30782349967f77cde8ab323b964108.css" rel="stylesheet">
+    <link href="manage/teaching/main.7a977b9eeb9c00b9854f88ce88ff1b79.css" rel="stylesheet">
 @endsection
 
 @section("js")
-    <script type="text/javascript" src="manage/teaching/main.4a18b6cbd719f003ac75.js"></script>
+    <script type="text/javascript" src="manage/teaching/main.71fd2918d8d625c359fb.js"></script>
 @endsection
