5159b1b72cd47d167ac57d39b5435a54df64979a
Cao Anh Quan <aquancva@gmail.com>
2018-04-05 11:27:52 +0700

    up

--- docker/php/sync.sh
+++ docker/php/sync.sh
@@ -3,7 +3,7 @@
 export COMPOSER_HOME=/var/www/vendor/bin/composer;
 export HOME=/var/www;
 cd /var/www;
-git pull origin master;
+git pull "https://keetool:Hungkhi550623@github.com/caoquan95/keetool-client-server" master;
 php artisan migrate;
 php composer.phar dump-autoload
 php artisan clear-compiled;
