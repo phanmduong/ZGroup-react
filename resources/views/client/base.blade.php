c378e6bc24bd8c806fe92c91c1c462af836253b9
kstn-dailoi <dailoi.kstn@gmail.com>
2018-04-05 17:05:38 +0700

    Loi build : base

--- resources/views/client/base.blade.php
+++ resources/views/client/base.blade.php
@@ -1,1 +1,1 @@
-@extends("client.layout.master") @section("css") <link href="manage/base/main.9aa4b3d48c1b9dd4e562661b5efe257c.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/base/main.692e67726de98844ecce.js"></script> @endsection
+@extends("client.layout.master") @section("css") <link href="manage/base/main.9aa4b3d48c1b9dd4e562661b5efe257c.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/base/main.6cedba51e36389647e04.js"></script> @endsection
--- resources/views/client/survey.blade.php
+++
@@ -1,9 +0,0 @@
-@extends("client.layout.master")
-
-@section("css")
-    <link href="manage/survey/main.4fa83d84fd8e6badb61a807373704a28.css" rel="stylesheet">
-@endsection
-
-@section("js")
-    <script type="text/javascript" src="manage/survey/main.f3faeab8868dec5d0563.js"></script>
-@endsection
