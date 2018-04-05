023a590c571fc6572e8699cd0ecc063ed3c74e4d
kstn-dailoi <dailoi.kstn@gmail.com>
2018-04-05 15:28:07 +0700

    Build : noti, shift, workShift, work, telesales

--- resources/views/client/telesales.blade.php
+++ resources/views/client/telesales.blade.php
@@ -1,9 +1,9 @@
 @extends("client.layout.master")
 
 @section("css")
-    <link href="manage/telesales/main.cb7a0a29ee37d728d99c3705db173b44.css" rel="stylesheet">
+    <link href="manage/telesales/main.4fa83d84fd8e6badb61a807373704a28.css" rel="stylesheet">
 @endsection
 
 @section("js")
-    <script type="text/javascript" src="manage/telesales/main.b6ad761036b120a6d9c6.js"></script>
+    <script type="text/javascript" src="manage/telesales/main.8165c458bcfd69e16cb3.js"></script>
 @endsection
--- resources/views/client/work.blade.php
+++ resources/views/client/work.blade.php
@@ -1,9 +1,9 @@
 @extends("client.layout.master")
 
 @section("css")
-    <link href="manage/work/main.45ec5a2dd16785e4b80e462763663d95.css" rel="stylesheet">
+    <link href="manage/work/main.24cb2b04476a2c8583a70c6da0021f36.css" rel="stylesheet">
 @endsection
 
 @section("js")
-    <script type="text/javascript" src="manage/work/main.c6160b98897f255f6e64.js"></script>
+    <script type="text/javascript" src="manage/work/main.fa76ce57326f84a03c29.js"></script>
 @endsection
