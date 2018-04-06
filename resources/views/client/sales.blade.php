4dcc55fc0c646bf232338bb028b0dcd659688541
kstn-dailoi <dailoi.kstn@gmail.com>
2018-04-06 22:45:07 +0700

    Loi build:sales

--- resources/views/client/sales.blade.php
+++ resources/views/client/sales.blade.php
@@ -1,1 +1,1 @@
-@extends("client.layout.master") @section("css") <link href="manage/sales/main.597d9a0e0aa889986a5603ae643026c9.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/sales/main.2f05ab2aa82caf89b639.js"></script> @endsection
+@extends("client.layout.master") @section("css") <link href="manage/sales/main.597d9a0e0aa889986a5603ae643026c9.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/sales/main.f734e2bb73881921b1bc.js"></script> @endsection
