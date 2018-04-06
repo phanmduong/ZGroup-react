876c07e42ee83eefeb75848249eabf05f2656e4e
Cao Anh Quan <aquancva@gmail.com>
2018-04-06 12:05:18 +0700

    Build module sales

--- resources/views/client/sales.blade.php
+++ resources/views/client/sales.blade.php
@@ -1,1 +1,1 @@
-@extends("client.layout.master") @section("css") <link href="manage/sales/main.597d9a0e0aa889986a5603ae643026c9.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/sales/main.54bec5e98d76a4bb6805.js"></script> @endsection
+@extends("client.layout.master") @section("css") <link href="manage/sales/main.597d9a0e0aa889986a5603ae643026c9.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/sales/main.2f05ab2aa82caf89b639.js"></script> @endsection
