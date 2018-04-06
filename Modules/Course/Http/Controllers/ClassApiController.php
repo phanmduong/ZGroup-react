7cf0c6c8882ac94ff64610fdb238284a03c5418d
datvithanh98 <datvithanh98@gmail.com>
2018-04-06 16:44:44 +0700

    up

--- Modules/Course/Http/Controllers/ClassApiController.php
+++ Modules/Course/Http/Controllers/ClassApiController.php
@@ -131,7 +131,7 @@ class ClassApiController extends ApiController
 
     public function getClasses($courseId, Request $request)
     {
-        $currentGen = $request->gen();
+        $currentGen = $request->gen_id;
         $classes = StudyClass::where('gen_id', $currentGen->id);
         if($request->base_id)
             $classes = $classes->where('base_id', $request->base_id);
