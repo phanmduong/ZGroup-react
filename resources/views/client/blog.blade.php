54f42f2c9d237105eb468c77f66778229906efc6
Kiyoshitaro <hunglhp1998@gmail.com>
2018-04-07 11:47:04 +0700

    blog

--- resources/views/client/blog.blade.php
+++ resources/views/client/blog.blade.php
@@ -1,9 +1,9 @@
 @extends("client.layout.master")
 
 @section("css")
-    <link href="manage/blog/main.7a977b9eeb9c00b9854f88ce88ff1b79.css" rel="stylesheet">
+    <link href="manage/blog/main.494d38e2ea553e52ace68f05859f90ad.css" rel="stylesheet">
 @endsection
 
 @section("js")
-    <script type="text/javascript" src="manage/blog/main.997170fe89d2ec2824b9.js"></script>
+    <script type="text/javascript" src="manage/blog/main.36511df7798c20882d45.js"></script>
 @endsection
