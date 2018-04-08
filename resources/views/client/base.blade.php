ad807ee471be2d7912df738e994c0245ac3434c8
Cao Anh Quan <aquancva@gmail.com>
2018-04-08 15:27:11 +0700

    Build module base

--- resources/views/client/base.blade.php
+++ resources/views/client/base.blade.php
@@ -1,9 +1,1 @@
-@extends("client.layout.master")
-
-@section("css")
-    <link href="manage/base/main.42b321f51314e04071f58b893197738e.css" rel="stylesheet">
-@endsection
-
-@section("js")
-    <script type="text/javascript" src="manage/base/main.e66c25b7e238692e7ab2.js"></script>
-@endsection
+@extends("client.layout.master") @section("css") <link href="manage/[CSS]" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/[JS]"></script> @endsection
