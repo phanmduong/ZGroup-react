82ae097787c3a9f2c4eeb9ee7145adfc6cdea13e
kstn-dailoi <dailoi.kstn@gmail.com>
2018-04-08 11:22:26 +0700

    loi build:teaching

--- resources/views/client/teaching.blade.php
+++ resources/views/client/teaching.blade.php
@@ -1,4 +1,4 @@
 @extends("client.layout.master") @section("css") 
 <link href="manage/teaching/main.63c12c39033a065d8820078fcf741b7d.css" rel="stylesheet"> 
 @endsection @section("js") 
-<script type="text/javascript" src="manage/teaching/main.d24a065b451b940cc076.js"></script> @endsection
+<script type="text/javascript" src="manage/teaching/main.49c06eed8599163730a1.js"></script> @endsection
