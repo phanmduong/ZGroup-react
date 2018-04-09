0ef254926da52c61e60d0d9964e7bbdcc191b538
datvithanh98 <datvithanh98@gmail.com>
2018-04-09 18:10:10 +0700

    up

--- Modules/User/Http/Controllers/UserManageApiController.php
+++ Modules/User/Http/Controllers/UserManageApiController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\ManageApiController;
 use App\Gen;
 use App\MarketingCampaign;
 use Illuminate\Support\Facades\DB;
+use App\Shift;
 
 class UserManageApiController extends ManageApiController
 {
@@ -43,26 +44,77 @@ class UserManageApiController extends ManageApiController
                 'role_title' => $user->current_role->role_title
             ]
         ];
+        //saler
 
         $registers = $user->sale_registers()->where('gen_id', $gen_id);
 
         $cloneRegisters = clone $registers;
-        
+
         $data['total_registers_count'] = $cloneRegisters->count();
 
-        $data['paid_registers_count'] = $cloneRegisters->select(DB::raw('sum(status) as paid_registers_count'))->first()->paid_registers_count;
+        $data['paid_registers_count'] = $cloneRegisters->where('status', 1)->count();//select(DB::raw('sum(status) as paid_registers_count'))->first()->paid_registers_count;
 
         $data['total_money'] = $cloneRegisters->select(DB::raw('sum(money) as total_money'))->first()->total_money;
 
         $data['registers'] = $registers->orderBy('created_at', 'desc')->get();
-        
+
         $data['campaigns'] = MarketingCampaign::join('registers', 'marketing_campaign.id', '=', 'registers.campaign_id')
             ->where('deleted_at', null)
             ->where('registers.gen_id', $gen_id)
             ->where('registers.saler_id', $user->id)
             ->select('marketing_campaign.*', DB::raw('count(*) as register_count'), DB::raw('sum(registers.status) as paid_register_count'), DB::raw('sum(money) as total_money'))
             ->groupBy('marketing_campaign.id')->get();
-            
+        
+        //shifts
+        $start = date('Y-m-d');
+        $end = date("Y-m-d", strtotime("+1 week"));
+        // dd([$start, $end]);
+        $shifts = Shift::whereRaw('date between "' . $start . '" and "' . $end . '"')
+            ->where('user_id', $user->id)
+            ->join('shift_sessions', 'shifts.shift_session_id', '=', 'shift_sessions.id')
+            ->orderBy('shifts.shift_session_id')
+            ->select('shifts.*', 'shift_sessions.start_time', 'shift_sessions.end_time', 'shift_sessions.name')->get();
+        $shifts = $shifts->map(function ($shift) {
+            $attendanceShift = [
+                'id' => $shift->id,
+                'name' => $shift->name,
+                'start_shift_time' => format_time_shift(strtotime($shift->start_time)),
+                'end_shift_time' => format_time_shift(strtotime($shift->end_time)),
+            ];
+
+            if ($shift->user) {
+                $attendanceShift['staff'] = [
+                    'id' => $shift->user->id,
+                    'name' => $shift->user->name,
+                    'color' => $shift->user->color,
+                ];
+            }
+
+            if ($shift->base) {
+                $attendanceShift['base'] = [
+                    'id' => $shift->base->id,
+                    'name' => $shift->base->name,
+                ];
+            }
+
+            if ($shift->check_in) {
+                $attendanceShift['check_in_time'] = format_time_shift(strtotime($shift->check_in->created_at));
+            }
+
+            if ($shift->check_out) {
+                $attendanceShift['check_out_time'] = format_time_shift(strtotime($shift->check_out->created_at));
+            }
+
+            return $attendanceShift;
+
+        });
+
+        if ($shifts->count() != 0)
+            $data['shifts'] = $shifts;
+
+        //lecturer
+
+
         return $this->respondSuccessWithStatus(['user' => $data]);
     }
 }
