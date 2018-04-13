19b56273027aa279377f6aa85007f8437ee884dd
seenastic <goldenrezgun@gmail.com>
2018-04-13 15:02:37 +0700

    add_col_advance_payment

--- database/migrations/2018_04_13_150116_add_col_advance_payment.php
+++ database/migrations/2018_04_13_150116_add_col_advance_payment.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductCategoryProductTable extends Migration
+class AddColAdvancePayment extends Migration
 {
     /**
      * Run the migrations.
@@ -12,11 +12,7 @@ class CreateProductCategoryProductTable extends Migration
      */
     public function up()
     {
-        Schema::create('product_category_product', function(Blueprint $table){
-            $table->increments('id');
-            $table->integer('product_id')->index()->unsigned();
-            $table->integer('category_product_id')->index()->unsigned();
-        });
+        //
     }
 
     /**
