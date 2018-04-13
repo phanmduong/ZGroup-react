2bacd696a64b5e568ace400753c07c6e78228187
komatsu98 <chemgiosa5@gmail.com>
2018-04-13 15:01:42 +0700

    ss

--- Modules/FilmZgroup/Resources/views/index.blade.php
+++ Modules/FilmZgroup/Resources/views/index.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <h1>Komatsu</h1>
     @foreach($sessions as $session)
-        {{$session->name}}
+        <div>
+            {{$session->film->running_time}}
+        </div>
     @endforeach
 
 @endsection
