234f8bdf3509a77d8f7397c5def4346230a702f3
Cao Anh Quan <aquancva@gmail.com>
2018-04-13 14:59:30 +0700

    up

--- resources/views/client/manufacture.blade.php
+++ resources/views/client/manufacture.blade.php
@@ -1,1 +1,1 @@
-@extends("client.layout.master") @section("css") <link href="manage/manufacture/main.2c0a81fb4d7720d0c1a7f8dfde285d9b.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/manufacture/main.0505b2ad215457a77640.js"></script> @endsection
+@extends("client.layout.master") @section("css") <link href="manage/manufacture/main.2c0a81fb4d7720d0c1a7f8dfde285d9b.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/manufacture/main.5b29cf6a6cc585e4a41d.js"></script> @endsection
