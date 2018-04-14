d5055c053ebe4018e45d2231d94f7d3e5fe918cc
Kiyoshitaro <hunglhp1998@gmail.com>
2018-04-14 11:42:35 +0700

    build blog

--- resources/views/client/blog.blade.php
+++ resources/views/client/blog.blade.php
@@ -8,6 +8,4 @@
 @section("js")
     <script type="text/javascript" src="manage/blog/main.248e781d74c44a576345.js"></script>
 @endsection
-=======
-@extends("client.layout.master") @section("css") <link href="manage/blog/main.4fa83d84fd8e6badb61a807373704a28.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/blog/main.493445ee2326709a33b7.js"></script> @endsection
->>>>>>> 15ce1621d3e1a3bf8487c39e6b4d5bcb2ad20617
+
