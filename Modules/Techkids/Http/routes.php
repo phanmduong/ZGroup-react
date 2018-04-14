378501466e4496b74ea57868ec2dd6f2bdea598a
datvithanh98 <datvithanh98@gmail.com>
2018-04-14 17:05:56 +0700

    up

--- Modules/Techkids/Http/routes.php
+++ Modules/Techkids/Http/routes.php
@@ -1,6 +1,8 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'techkids', 'namespace' => 'Modules\Techkids\Http\Controllers'], function()
-{
+$routes = function () {
     Route::get('/', 'TechkidsController@index');
-});
+};
+
+Route::group(['middleware' => 'web', 'domain' => 'keetool3.{subfix}', 'namespace' => 'Modules\Techkids\Http\Controllers'], $routes);
+
