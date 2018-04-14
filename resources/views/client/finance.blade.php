b23304ffb25e18b0e34f37da082abce2ceb48435
phanmduong <phanminhduong@hotmail.com>
2018-04-14 11:53:26 +0700

    update code

--- resources/views/client/finance.blade.php
+++ resources/views/client/finance.blade.php
@@ -1,1 +1,4 @@
-@extends("client.layout.master") @section("css") <link href="manage/finance/main.13af2cc7f78b9effb6c4e850544de7b4.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/finance/main.01ffcb29d662d64d4db2.js"></script> @endsection
+@extends("client.layout.master") @section("css") 
+<link href="manage/finance/main.13af2cc7f78b9effb6c4e850544de7b4.css" rel="stylesheet"> @endsection @section("js")
+ <script type="text/javascript" src="manage/finance/main.01ffcb29d662d64d4db2.js">
+ </script> @endsection
