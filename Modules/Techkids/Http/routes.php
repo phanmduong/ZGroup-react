533b94c68c1c07dfdfc1665ab9a9a11fcd46805a
Cao Anh Quan <aquancva@gmail.com>
2018-04-15 15:50:00 +0700

    up

--- Modules/Techkids/Http/routes.php
+++ Modules/Techkids/Http/routes.php
@@ -2,7 +2,8 @@
 
 $routes = function () {
     Route::get('/', 'TechkidsController@index');
+    Route::get('/khoa-hoc-lap-trinh/{id}', 'TechkidsController@course');
 };
 
 Route::group(['middleware' => 'web', 'domain' => 'techkids.test', 'namespace' => 'Modules\Techkids\Http\Controllers'], $routes);
-
+Route::group(['middleware' => 'web', 'domain' => 'keetool1.xyz', 'namespace' => 'Modules\Techkids\Http\Controllers'], $routes);
