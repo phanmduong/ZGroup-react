9ae81ec784e4cd2f1226f140b5d3ea67af572921
Minh <minhnora98@gmail.com>
2018-04-15 17:14:05 +0700

    up

--- Modules/BeeSchool/Http/Controllers/BeeSchoolController.php
+++ Modules/BeeSchool/Http/Controllers/BeeSchoolController.php
@@ -5,68 +5,56 @@ namespace Modules\BeeSchool\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
+use App\Product;
 
 class BeeSchoolController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     * @return Response
-     */
     public function index()
     {
         return view('beeschool::index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Response
-     */
-    public function create()
+    public function blogs(Request $request)
     {
-        return view('beeschool::create');
+        $blogs = Product::where('type', 2)->where('status', 1);
+
+        $search = $request->search;
+
+        if ($search) {
+            $blogs = $blogs->where('title', 'like', '%' . $search . '%');
+        }
+
+        $blogs = $blogs->orderBy('created_at', 'desc')->paginate(6);
+
+        $display = '';
+        if ($request->page == null) {
+            $page_id = 2;
+        } else {
+            $page_id = $request->page + 1;
+        }
+        if ($blogs->lastPage() == $page_id - 1) {
+            $display = 'display:none';
+        }
+
+        $this->data['blogs'] = $blogs;
+        $this->data['page_id'] = $page_id;
+        $this->data['display'] = $blogs;
+        $this->data['search'] = $search;
+
+        $this->data['total_pages'] = ceil($blogs->total() / $blogs->perPage());
+        $this->data['current_page'] = $blogs->currentPage();
+        // dd($this->data);
+        return view('beeschool::blogs',$this->data);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     * @param  Request $request
-     * @return Response
-     */
-    public function store(Request $request)
+    public function post($post_id)
     {
-    }
-
-    /**
-     * Show the specified resource.
-     * @return Response
-     */
-    public function show()
-    {
-        return view('beeschool::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @return Response
-     */
-    public function edit()
-    {
-        return view('beeschool::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param  Request $request
-     * @return Response
-     */
-    public function update(Request $request)
-    {
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @return Response
-     */
-    public function destroy()
-    {
+        $post = Product::find($post_id);
+        if ($post == null) {
+            return 'Bài viết không tồn tại';
+        }
+        $this->data['post'] = $post;
+        // $this->data['blogs'];
+        return view('beeschool::post',$this->data);
     }
 }
