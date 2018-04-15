69d3148b9fc442bf1497634f3f97e0e3846ad665
komatsu98 <chemgiosa5@gmail.com>
2018-04-15 12:45:09 +0700

    clone index techkids

--- Modules/Techkids/Resources/views/layouts/master.blade.php
+++ Modules/Techkids/Resources/views/layouts/master.blade.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Module Techkids</title>
-    </head>
-    <body>
-        @yield('content')
-    </body>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Module Techkids</title>
+</head>
+<body>
+@include('techkids::layouts.nav-bar')
+@yield('content')
+@include('techkids::layouts.footer')
+
+</body>
 </html>
+
+<script src="https://d255zuevr6tr8p.cloudfront.net/landingpage/assets/js/bootstrap.min.js"
+
+<script src="https://d255zuevr6tr8p.cloudfront.net/landingpage/assets/js/paper-kit.js?v=2.0.0"></script>
+<script src="https://d255zuevr6tr8p.cloudfront.net/landingpage/assets/js/jquery-ui-1.12.1.custom.min.js"></script>
+<link rel="stylesheet" href="/assets/css/slick.css">
+<link rel="stylesheet" href="/assets/css/slick-theme.css">
+<script src="/assets/js/slick.min.js"></script>
+@stack('scripts')
