bbcf839144e939cfdf113119c792750376e5e8a8
seenastic <goldenrezgun@gmail.com>
2018-04-15 17:09:35 +0700

    build_administration

--- resources/views/client/administration.blade.php
+++ resources/views/client/administration.blade.php
@@ -1,1 +1,1 @@
-@extends("client.layout.master") @section("css") <link href="manage/administration/main.6a87c626d862b4a213510fe909e4a0ec.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/administration/main.25665da4466f80740e2f.js"></script> @endsection
+@extends("client.layout.master") @section("css") <link href="manage/administration/main.247b587742f37057a12e6c36e550a971.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/administration/main.25cca896c2273d891fb2.js"></script> @endsection
