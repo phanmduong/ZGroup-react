d04e3afc21772b63c4ca8dbc0eea6e9fd3746172
Minh <minhnora98@gmail.com>
2018-04-15 17:24:06 +0700

    up

--- Modules/BeeSchool/Http/Controllers/BeeSchoolController.php
+++ Modules/BeeSchool/Http/Controllers/BeeSchoolController.php
@@ -16,34 +16,8 @@ class BeeSchoolController extends Controller
 
     public function blogs(Request $request)
     {
-        $blogs = Product::where('type', 2)->where('status', 1);
-
-        $search = $request->search;
-
-        if ($search) {
-            $blogs = $blogs->where('title', 'like', '%' . $search . '%');
-        }
-
-        $blogs = $blogs->orderBy('created_at', 'desc')->paginate(6);
-
-        $display = '';
-        if ($request->page == null) {
-            $page_id = 2;
-        } else {
-            $page_id = $request->page + 1;
-        }
-        if ($blogs->lastPage() == $page_id - 1) {
-            $display = 'display:none';
-        }
-
+        $blogs = Product::all();
         $this->data['blogs'] = $blogs;
-        $this->data['page_id'] = $page_id;
-        $this->data['display'] = $blogs;
-        $this->data['search'] = $search;
-
-        $this->data['total_pages'] = ceil($blogs->total() / $blogs->perPage());
-        $this->data['current_page'] = $blogs->currentPage();
-        // dd($this->data);
         return view('beeschool::blogs',$this->data);
     }
 
