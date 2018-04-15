5a087d8a71a1594011aa74f549db8d39f291dcca
komatsu98 <chemgiosa5@gmail.com>
2018-04-15 16:41:36 +0700

    clone techkids

--- Modules/Techkids/Http/routes.php
+++ Modules/Techkids/Http/routes.php
@@ -2,6 +2,7 @@
 
 $routes = function () {
     Route::get('/', 'TechkidsController@index');
+    Route::get('/blogs', 'TechkidsController@blogs');
 };
 
 Route::group(['middleware' => 'web', 'domain' => 'techkids.test', 'namespace' => 'Modules\Techkids\Http\Controllers'], $routes);
