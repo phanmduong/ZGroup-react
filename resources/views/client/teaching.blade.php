9e46385e9eaeb6d4190749f811f4dd3603ea6af8
seenastic <goldenrezgun@gmail.com>
2018-04-16 17:15:54 +0700

    build_teaching

--- resources/views/client/teaching.blade.php
+++ resources/views/client/teaching.blade.php
@@ -1,1 +1,1 @@
-@extends("client.layout.master") @section("css") <link href="manage/teaching/main.247b587742f37057a12e6c36e550a971.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/teaching/main.5bd0fcd619a54686cde0.js"></script> @endsection
+@extends("client.layout.master") @section("css") <link href="manage/teaching/main.247b587742f37057a12e6c36e550a971.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/teaching/main.5e14492c3df57c962110.js"></script> @endsection
