8296f92d238680e813e0be87557affb1aaf8f9b0
Cao Anh Quan <aquancva@gmail.com>
2018-04-16 14:30:12 +0700

    build order

--- resources/views/client/order.blade.php
+++ resources/views/client/order.blade.php
@@ -1,1 +1,1 @@
-@extends("client.layout.master") @section("css") <link href="manage/order/main.4fa83d84fd8e6badb61a807373704a28.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/order/main.4478dd323bf7b335e382.js"></script> @endsection
+@extends("client.layout.master") @section("css") <link href="manage/order/main.247b587742f37057a12e6c36e550a971.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/order/main.fff8c11b444a71e7456b.js"></script> @endsection
