0bb9fb39ef1d68fd070f9f3c8bf02e57fb107c0c
seenastic <goldenrezgun@gmail.com>
2018-04-21 14:20:42 +0700

    save

--- src/modules/Zgroup/warehouse/HistoryGoodComponent.js
+++ src/modules/Zgroup/warehouse/HistoryGoodComponent.js
@@ -1,63 +1,66 @@
 import React from "react";
-import {Modal} from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 import PropTypes from "prop-types";
 import Pagination from "../../../components/common/Pagination";
+import BigCloseButtonForModal from "../../../components/common/BigCloseButtonForModal";
 
-class HistoryGoodComponent extends React.Component{
+class HistoryGoodComponent extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.loadHistoryGood = this.loadHistoryGood.bind(this);
     }
-    loadHistoryGood(page){
-        this.props.loadHistory(page,this.props.id);
+    loadHistoryGood(page) {
+        this.props.loadHistory(page, this.props.id);
     }
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <Modal
                     show={this.props.show}
                     onHide={this.props.onHide}
                     bsSize="large"
                 >
-                    <Modal.Header closeButton/>
+                    <Modal.Header>
+                        <BigCloseButtonForModal onClick={this.props.onHide} />
+                    </Modal.Header>
                     <Modal.Body>
-                        <div className="table-responsive" style={{minHeight: 400}}>
+                        <div className="table-responsive" style={{ minHeight: 400 }}>
 
                             <table id="datatables"
-                                   className="table table-hover"
-                                   cellSpacing="0" width="100%" style={{width: "100%"}}>
+                                className="table table-hover"
+                                cellSpacing="0" width="100%" style={{ width: "100%" }}>
                                 <thead className="text-rose">
-                                <tr>
-                                    <th/>
-                                    <th>STT</th>
-                                    <th>Ngày</th>
-                                    <th>Trạng thái</th>
-                                    <th>Số lượng</th>
-                                    <th>Kho hàng</th>
-                                    <th>Mã giao dịch</th>
-                                    <th> Người thực hiện </th>
+                                    <tr>
+                                        <th />
+                                        <th>STT</th>
+                                        <th>Ngày</th>
+                                        <th>Trạng thái</th>
+                                        <th>Số lượng</th>
+                                        <th>Kho hàng</th>
+                                        <th>Mã giao dịch</th>
+                                        <th> Người thực hiện </th>
 
 
-                                </tr>
+                                    </tr>
                                 </thead>
 
                                 <tbody>
-                                {
-                                    this.props.data && this.props.data.map((pp,index) =>{
-                                        return(
-                                            <tr key={index}>
-                                                <td/>
-                                                <td> {index+1} </td>
-                                                <td> {pp.order.created_at} </td>
-                                                <td> {pp.order.type === "be-ordered" ? "Xuất hàng" : "Nhập hàng" }</td>
-                                                <td>  {pp.quantity}</td>
-                                                <td>  {pp.warehouse.name}</td>
-                                                <td> {pp.order.command_code} </td>
-                                                <td> {pp.order.staff.name} </td>
-                                            </tr>
-                                        );
-                                    })
-                                }
+                                    {
+                                        this.props.data && this.props.data.map((pp, index) => {
+                                            return (
+                                                <tr key={index}>
+                                                    <td />
+                                                    <td> {index + 1} </td>
+                                                    <td> {pp.order.created_at} </td>
+                                                    <td> {pp.order.type === "be-ordered" ? "Xuất hàng" : "Nhập hàng"}</td>
+                                                    <td>  {pp.quantity}</td>
+                                                    <td>  {pp.warehouse.name}</td>
+                                                    <td> {pp.order.command_code} </td>
+                                                    <td> {pp.order.staff.name} </td>
+                                                </tr>
+                                            );
+                                        })
+                                    }
                                 </tbody>
                             </table>
                             <Pagination
