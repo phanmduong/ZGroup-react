5fe73f8c3c8b7572f9697504228e5cfbe5b4f6a5
Cao Anh Quan <aquancva@gmail.com>
2018-04-22 16:48:08 +0700

    up

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -9,10 +9,13 @@ use Illuminate\Support\Facades\Auth;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use  GuzzleHttp\Client;
 use Illuminate\Support\Facades\Hash;
+use App\Services\EmailService;
 
 class ClientController extends Controller
 {
-    public function __construct()
+    protected $emailService;
+
+    public function __construct(EmailService $emailService)
     {
         if (url('/') != url()->current()) {
             $path = explode(url('/') . '/', url()->current())[1];
@@ -22,6 +25,7 @@ class ClientController extends Controller
         if ($path != 'login-free-trial') {
             $this->middleware('permission_tab_react:' . $path);
         }
+        $this->emailService = $emailService;
     }
 
     public function loginFreeTrial(Request $request)
