0621a629787ce408abbb4d841ddc14a58edd939f
Cao Anh Quan <aquancva@gmail.com>
2018-04-22 16:44:01 +0700

    up

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Role;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use  GuzzleHttp\Client;
 
 class ClientController extends Controller
 {
