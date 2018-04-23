5748b9ac6815d4a103fb78584c5b46704408ce92
Cao Anh Quan <aquancva@gmail.com>
2018-04-23 17:08:51 +0700

    rebuild

--- resources/views/client/shift.blade.php
+++ resources/views/client/shift.blade.php
@@ -1,1 +1,1 @@
-@extends("client.layout.master") @section("css") <link href="manage/shift/main.247b587742f37057a12e6c36e550a971.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/shift/main.c3b896aa30f6e7985fb3.js"></script> @endsection
+@extends("client.layout.master") @section("css") <link href="manage/shift/main.a048feb634eea7e5692c03d1f852e8e0.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/shift/main.e756924ae110661c3170.js"></script> @endsection
