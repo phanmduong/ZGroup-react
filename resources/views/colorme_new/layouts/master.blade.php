c25a04078f493731ed9ca751ae9938823161af42
Cao Anh Quan <aquancva@gmail.com>
2018-04-27 16:22:04 +0700

    up

--- resources/views/colorme_new/layouts/master.blade.php
+++ resources/views/colorme_new/layouts/master.blade.php
@@ -431,35 +431,36 @@
 @stack("scripts")
 <script>
 
-    $(function(){
+    $(document).ready(function () {
 
         var iFrames = $('iframe');
 
         function iResize() {
 
             for (var i = 0, j = iFrames.length; i < j; i++) {
-              iFrames[i].style.height = iFrames[i].contentWindow.document.body.offsetHeight + 'px';}
+                iFrames[i].style.height = iFrames[i].contentWindow.document.body.offsetHeight + 'px';
             }
+        }
 
-            if ($.browser.safari || $.browser.opera) {
+        if ($.browser.safari || $.browser.opera) {
 
-               iFrames.load(function(){
-                   setTimeout(iResize, 0);
-               });
+            iFrames.load(function(){
+                setTimeout(iResize, 0);
+            });
 
-               for (var i = 0, j = iFrames.length; i < j; i++) {
+            for (var i = 0, j = iFrames.length; i < j; i++) {
                     var iSource = iFrames[i].src;
                     iFrames[i].src = '';
                     iFrames[i].src = iSource;
-               }
-
-            } else {
-               iFrames.load(function() {
-                   this.style.height = this.contentWindow.document.body.offsetHeight + 'px';
-               });
             }
 
-        });
+        } else {
+            iFrames.load(function() {
+                this.style.height = this.contentWindow.document.body.offsetHeight + 'px';
+            });
+        }
+    });
+    
 
 </script>
 </body>
