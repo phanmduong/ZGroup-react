ef7b3627edc1dab86cb0c101cce1c93a0c1005d0
Kiyoshitaro <hunglhp1998@gmail.com>
2018-04-27 16:39:10 +0700

    build studentRegister

--- resources/views/client/sales.blade.php
+++ resources/views/client/sales.blade.php
@@ -1,3 +1,3 @@
 @extends("client.layout.master") @section("css")
     <link href="manage/sales/main.287f5e9b9c4282cb4ee6df767ed64ce3.css" rel="stylesheet"> @endsection @section("js")
-    <script type="text/javascript" src="manage/sales/main.4159daf6be0751529685.js"></script> @endsection
+    <script type="text/javascript" src="manage/sales/main.3bae447efb59731f79a4.js"></script> @endsection
