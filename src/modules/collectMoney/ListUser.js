b6fa590bbe4e6e9028510d00ba5f9e0d0f6cf6b8
phanmduong <phanminhduong@hotmail.com>
2018-04-27 17:52:26 +0700

    update code

--- src/modules/collectMoney/ListUser.js
+++ src/modules/collectMoney/ListUser.js
@@ -1,51 +1,74 @@
-import React from 'react';
-import ListRegister from './ListRegister';
-import PropTypes from 'prop-types';
+import React from "react";
+import ListRegister from "./ListRegister";
+import PropTypes from "prop-types";
 
 class ListUser extends React.Component {
     constructor(props, context) {
         super(props, context);
     }
 
+    checkCollectingMoney() {
+        let isCollectingMoney = false;
+
+        this.props.users.map(user => {
+            if (isCollectingMoney) return;
+            user.registers.map(register => {
+                if (register.isUpdating) {
+                    isCollectingMoney = true;
+                    return;
+                }
+            });
+        });
+        return isCollectingMoney;
+    }
+
     render() {
         return (
             <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                {
-                    this.props.users.map((user) => {
-                        return (
-                            <div className="panel panel-default" key={user.id}>
-                                <div className="panel-heading" role="tab" id={'headingCollectMoneyUser' + user.id}>
-                                    <a role="button" data-toggle="collapse" data-parent="#accordion"
-                                       href={'#collectMoneyUser' + user.id}
-                                       aria-expanded="false" aria-controls={'collectMoneyUser' + user.id}
-                                       className="collapsed"
-                                    >
-                                        <h4 className="panel-title">
-                                            {user.name}
-                                            {(user.email) ? ` (${user.email})` : ''}
-                                            {(user.phone) ? ` (${user.phone})` : ''}
-                                            <i className="material-icons">keyboard_arrow_down</i>
-                                        </h4>
-                                    </a>
-                                </div>
-                                <div id={'collectMoneyUser' + user.id} className="panel-collapse collapse"
-                                     role="tabpanel"
-                                     aria-labelledby={'headingCollectMoneyUser' + user.id} aria-expanded="false"
-                                     style={{height: '0px'}}>
-                                    <div className="panel-body">
-                                        <ListRegister
-                                            user={user}
-                                            nextCode={this.props.nextCode}
-                                            nextWaitingCode={this.props.nextWaitingCode}
-                                            registers={user.registers}
-                                            updateMoney={this.props.updateMoney}
-                                        />
-                                    </div>
+                {this.props.users.map(user => {
+                    return (
+                        <div className="panel panel-default" key={user.id}>
+                            <div
+                                className="panel-heading"
+                                role="tab"
+                                id={"headingCollectMoneyUser" + user.id}>
+                                <a
+                                    role="button"
+                                    data-toggle="collapse"
+                                    data-parent="#accordion"
+                                    href={"#collectMoneyUser" + user.id}
+                                    aria-expanded="false"
+                                    aria-controls={"collectMoneyUser" + user.id}
+                                    className="collapsed">
+                                    <h4 className="panel-title">
+                                        {user.name}
+                                        {user.email ? ` (${user.email})` : ""}
+                                        {user.phone ? ` (${user.phone})` : ""}
+                                        <i className="material-icons">keyboard_arrow_down</i>
+                                    </h4>
+                                </a>
+                            </div>
+                            <div
+                                id={"collectMoneyUser" + user.id}
+                                className="panel-collapse collapse"
+                                role="tabpanel"
+                                aria-labelledby={"headingCollectMoneyUser" + user.id}
+                                aria-expanded="false"
+                                style={{ height: "0px" }}>
+                                <div className="panel-body">
+                                    <ListRegister
+                                        user={user}
+                                        nextCode={this.props.nextCode}
+                                        nextWaitingCode={this.props.nextWaitingCode}
+                                        registers={user.registers}
+                                        updateMoney={this.props.updateMoney}
+                                        isCollectingMoney={this.checkCollectingMoney()}
+                                    />
                                 </div>
                             </div>
-                        );
-                    })
-                }
+                        </div>
+                    );
+                })}
             </div>
         );
     }
@@ -55,8 +78,7 @@ ListUser.propTypes = {
     nextCode: PropTypes.string.isRequired,
     nextWaitingCode: PropTypes.string.isRequired,
     users: PropTypes.array.isRequired,
-    updateMoney: PropTypes.func.isRequired,
+    updateMoney: PropTypes.func.isRequired
 };
-
 
 export default ListUser;
--- src/modules/spendMoney/HistoryTransaction.js
+++ src/modules/spendMoney/HistoryTransaction.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import PropTypes from 'prop-types';
-import {bindActionCreators} from 'redux';
-import * as spendMoneyActions from './spendMoneyActions';
-import {dotNumber} from "../../helpers/helper";
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+import * as spendMoneyActions from "./spendMoneyActions";
+import { dotNumber } from "../../helpers/helper";
 import Loading from "../../components/common/Loading";
 import Pagination from "../../components/common/Pagination";
 
@@ -22,13 +22,13 @@ class HistoryTransaction extends React.Component {
     }
 
     loadData(page = 1) {
-        this.setState({page});
+        this.setState({ page });
         this.props.spendMoneyActions.loadHistoryTransactions(page, this.state.type);
     }
 
     changeType(value) {
         if (value != this.state.type) {
-            this.setState({type: value, page: 1});
+            this.setState({ type: value, page: 1 });
             this.props.spendMoneyActions.loadHistoryTransactions(1, value);
         }
     }
@@ -39,57 +39,89 @@ class HistoryTransaction extends React.Component {
                 <div className="card-content">
                     <div className="tab-content">
                         <h4 className="card-title">
-                            <strong>Lịch sử thu/chi</strong>
+                            <strong>Lịch sử giao dịch</strong>
                         </h4>
-                        <br/>
+                        <br />
                         <div>
-                            <button className={"btn btn-round margin-right-10" + (this.state.type == "" ? " btn-rose" : "")}
-                                    onClick={() => this.changeType("")}
-                            >
+                            <button
+                                className={
+                                    "btn btn-round margin-right-10" +
+                                    (this.state.type == "" ? " btn-rose" : "")
+                                }
+                                onClick={() => this.changeType("")}>
                                 Tất cả
                             </button>
                             <button
-                                className={"btn btn-round margin-right-10 margin-left-20" + (this.state.type == "1" ? " btn-rose" : "")}
-                                onClick={() => this.changeType("1")}
-                            >
+                                className={
+                                    "btn btn-round margin-right-10 margin-left-20" +
+                                    (this.state.type == "1" ? " btn-rose" : "")
+                                }
+                                onClick={() => this.changeType("1")}>
                                 Thu
                             </button>
                             <button
-                                className={"btn btn-round margin-left-20" + (this.state.type == "2" ? " btn-rose" : "")}
-                                onClick={() => this.changeType("2")}
-                            >
+                                className={
+                                    "btn btn-round margin-left-20" +
+                                    (this.state.type == "2" ? " btn-rose" : "")
+                                }
+                                onClick={() => this.changeType("2")}>
                                 Chi
                             </button>
+                            <button
+                                className={
+                                    "btn btn-round margin-left-20" +
+                                    (this.state.type == "0" ? " btn-rose" : "")
+                                }
+                                onClick={() => this.changeType("0")}>
+                                Chuyển tiền
+                            </button>
                         </div>
-                        {this.props.isLoading ?
-                            <Loading/>
-                            :
+                        {this.props.isLoading ? (
+                            <Loading />
+                        ) : (
                             <div>
                                 <div className="table-responsive">
                                     <table className="table">
                                         <thead className="text-rose">
-                                        <tr>
-                                            <th>Loại giao dịch</th>
-                                            <th>Nhóm</th>
-                                            <th>Lý do</th>
-                                            <th>Ngày giờ</th>
-                                            <th>Số tiền trước giao dịch</th>
-                                            <th className="text-center">Số tiền</th>
-                                            <th/>
-                                        </tr>
+                                            <tr>
+                                                <th>Loại giao dịch</th>
+                                                <th>Nhóm</th>
+                                                <th>Lý do</th>
+                                                <th>Ngày giờ</th>
+                                                <th>Số tiền trước giao dịch</th>
+                                                <th className="text-center">Số tiền</th>
+                                                <th />
+                                            </tr>
                                         </thead>
                                         <tbody>
-                                        {
-                                            this.props.transactions.map((transaction) => {
-                                                const classType = transaction.type == 1 ? " btn-success " : " btn-danger ";
-                                                const textType = transaction.type == 1 ? "Thu" : "Chi";
+                                            {this.props.transactions.map(transaction => {
+                                                const classType =
+                                                    transaction.type === 0
+                                                        ? " btn-info "
+                                                        : transaction.type == 1
+                                                            ? " btn-success "
+                                                            : " btn-danger ";
+                                                const textType =
+                                                    transaction.type === 0
+                                                        ? "Chuyển tiền"
+                                                        : transaction.type == 1
+                                                            ? "Thu"
+                                                            : "Chi";
                                                 let classStatus;
                                                 let textStatus;
 
-                                                if (transaction.type == 1) {
+                                                if (
+                                                    transaction.type == 1 ||
+                                                    (transaction.type == 0 &&
+                                                        transaction.receiver_id == this.props.user.id)
+                                                ) {
                                                     classStatus = " btn-success ";
                                                     textStatus = "+";
-                                                } else if (transaction.type == 2) {
+                                                } else if (
+                                                    transaction.type == 2 ||
+                                                    (transaction.type == 0 &&
+                                                        transaction.sender_id == this.props.user.id)
+                                                ) {
                                                     classStatus = " btn-danger ";
                                                     textStatus = "-";
                                                 }
@@ -98,50 +130,52 @@ class HistoryTransaction extends React.Component {
                                                     <tr key={transaction.id}>
                                                         <td>
                                                             <button
-                                                                className={classType + "btn btn-sm width-100 bold"}>
+                                                                className={
+                                                                    classType + "btn btn-sm width-100 bold"
+                                                                }>
                                                                 {textType}
                                                             </button>
                                                         </td>
                                                         <td>
-                                                            {
-                                                                transaction.category &&
+                                                            {transaction.category && (
                                                                 <button
                                                                     className={"btn btn-sm width-100 bold"}
-                                                                    style={{backgroundColor: transaction.category.color}}
-                                                                >
+                                                                    style={{
+                                                                        backgroundColor:
+                                                                            transaction.category.color
+                                                                    }}>
                                                                     {transaction.category.name}
                                                                 </button>
-                                                            }
-
+                                                            )}
                                                         </td>
-                                                        <td>
-                                                            {transaction.note}
-                                                        </td>
+                                                        <td>{transaction.note}</td>
                                                         <td>{transaction.updated_at}</td>
                                                         <td className="text-align-right">
                                                             {dotNumber(transaction.before_money)}đ
                                                         </td>
                                                         <td>
                                                             <button
-                                                                className={classStatus + "btn btn-sm width-100 bold lowercase"}>
+                                                                className={
+                                                                    classStatus +
+                                                                    "btn btn-sm width-100 bold lowercase"
+                                                                }>
                                                                 {textStatus + dotNumber(transaction.money)}đ
                                                             </button>
                                                         </td>
                                                     </tr>
                                                 );
-                                            })
-                                        }
+                                            })}
                                         </tbody>
                                     </table>
                                 </div>
                             </div>
-                        }
+                        )}
                         <Pagination
                             totalPages={this.props.totalPages}
                             currentPage={this.state.page}
-                            loadDataPage={(page) => this.loadData(page)}
+                            loadDataPage={page => this.loadData(page)}
                         />
-                    </div>    
+                    </div>
                 </div>
             </div>
         );
@@ -150,6 +184,7 @@ class HistoryTransaction extends React.Component {
 
 HistoryTransaction.propTypes = {
     spendMoneyActions: PropTypes.object.isRequired,
+    user: PropTypes.object.isRequired,
     isLoading: PropTypes.bool.isRequired,
     transactions: PropTypes.array.isRequired,
     totalPages: PropTypes.number.isRequired,
@@ -160,6 +195,7 @@ function mapStateToProps(state) {
         isLoading: state.spendMoney.historyTransaction.isLoading,
         transactions: state.spendMoney.historyTransaction.transactions,
         totalPages: state.spendMoney.historyTransaction.totalPages,
+        user: state.spendMoney.user,
     };
 }
 
