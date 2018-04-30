5a3728df545d566e2aa2730dc8985f2dd945ddbe
Batman Bruce Wayne <taideptrai02@gmail.com>
2018-04-30 17:55:47 +0700

    build nhatquang

--- resources/views/client/order.blade.php
+++ resources/views/client/order.blade.php
@@ -1,3 +1,3 @@
 
-@extends("client.layout.master") @section("css") <link href="manage/order/main.bd98ff1ff5fb0b13a0725d4f990c8a78.css" rel="stylesheet"> @endsection @section("js")
-    <script type="text/javascript" src="manage/order/main.6c107eed6af984fb3c9f.js"></script> @endsection
+@extends("client.layout.master") @section("css") <link href="manage/order/main.b1481ddc9f30aea4565e30ed443aca7e.css" rel="stylesheet"> @endsection @section("js")
+    <script type="text/javascript" src="manage/order/main.8686b6b12a054a852156.js"></script> @endsection
