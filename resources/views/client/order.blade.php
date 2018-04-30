96abd75ae46d4f2a9a1bfcc2e078dd84b13e94b1
phanmduong <phanminhduong@hotmail.com>
2018-04-30 18:51:33 +0700

    update code

--- resources/views/client/order.blade.php
+++ resources/views/client/order.blade.php
@@ -1,3 +1,4 @@
-
-@extends("client.layout.master") @section("css") <link href="manage/order/main.b1481ddc9f30aea4565e30ed443aca7e.css" rel="stylesheet"> @endsection @section("js")
+@extends("client.layout.master") @section("css")
+    <link href="manage/order/main.b1481ddc9f30aea4565e30ed443aca7e.css" rel="stylesheet">
+@endsection @section("js")
     <script type="text/javascript" src="manage/order/main.8686b6b12a054a852156.js"></script> @endsection
