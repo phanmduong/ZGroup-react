a0b14819d1f304928b6f03562bbd24ad63050673
Cao Anh Quan <aquancva@gmail.com>
2018-05-03 11:55:00 +0700

    make coupon nullable

--- Modules/Survey/Http/Controllers/RenderSurveyController.php
+++ Modules/Survey/Http/Controllers/RenderSurveyController.php
@@ -3,13 +3,12 @@
 namespace Modules\Survey\Http\Controllers;
 
 use Modules\Survey\Services\SurveyService;
-use \App\Http\Controllers\Controller;
 use App\Survey;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
-
 use App\User;
 use Illuminate\Support\Facades\Auth;
+use App\SurveyUser;
 
 class RenderSurveyController extends ApiController
 {
@@ -28,37 +27,68 @@ class RenderSurveyController extends ApiController
         if ($survey == null) {
             return 'Khảo sát không tồn tại';
         } else {
+            $user = Auth::user();
             $data = [
-                // 'user' => Auth::user(),
+                'user' => $user,
                 'survey' => $survey
             ];
             return view('survey::survey', $data);
         }
     }
 
-    public function submitForm(Request $request) {
+    public function surveySubmitted()
+    {
+        return view('survey::survey_submitted');
+    }
+
+    public function submitForm($surveyId, Request $request)
+    {
         $data = $request->data;
         $email = $request->email;
         $name = $request->name;
         $phone = $request->phone;
 
+        // Get logged user
+        $user = Auth::user();
+
+        $survey = Survey::find($surveyId);
+        if ($survey == null) {
+            return $this->respondErrorWithStatus('Survey không tồn tại');
+        }
+
         if ($data == null) {
-            return $this->respondErrorWithStatus("Bạn truyền lên thiếu dữ liệu form");
+            // Form data is missing
+            return $this->respondErrorWithStatus('Bạn truyền lên thiếu dữ liệu form');
         }
 
-        if ($email == null || $name == null || $phone == null ) {
-            return $this->respondErrorWithStatus("Bạn truyền lên thiếu dữ liệu user");
-        }
-
-        $user = User::where("email", $email)->first();
         if ($user == null) {
-            $user = User::create([
-                "name" => $name,
-                "email" => $email,
-                "phone" => $phone
-            ]);
-            $user->save();
+            // if user not logged in
+            if ($email == null || $name == null || $phone == null) {
+                return $this->respondErrorWithStatus('Bạn truyền lên thiếu dữ liệu user');
+            }
+            $user = User::where('email', $email)->first();
+
+            if ($user == null) {
+                // There is no user with this $email
+                $user = User::create([
+                    'name' => $name,
+                    'email' => $email,
+                    'phone' => $phone
+                ]);
+                $user->save();
+            }
         }
-        // TODO: save data to survey_user 
+        // $user and $survey are surely not null
+        $surveyUser = new SurveyUser();
+        $surveyUser->user_id = $user->id;
+        $surveyUser->status = 1;
+        $surveyUser->content = $data;
+        $surveyUser->gen_id = 0;
+        $surveyUser->survey_id = $surveyId;
+        $surveyUser->save();
+
+        return $this->respondSuccessV2([
+            'message' => 'Bạn đã gửi khảo sát thành công'
+        ]);
     }
 }
--- Modules/Survey/Resources/views/survey.blade.php
+++ Modules/Survey/Resources/views/survey.blade.php
@@ -1,19 +1,10 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends("survey::layouts.master")
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-
-    <title>{{$survey->name}}</title>
-  </head>
-  <body>
-        <div class="container">
-            <h5 style="text-align:center">{{$survey->name}}</h5>
-            <form id="form">
+@section("content")
+    <div class="container">
+        <h5 style="text-align:center">{{$survey->name}}</h5>
+        <form id="form">
+            @if($user == null)
                 <div class="form-group">
                     <label for="name">Họ tên</label>
                     <input type="text" 
@@ -35,29 +26,32 @@
                         id="phone"
                         class="form-control">
                 </div>
+            @endif
 
-                @foreach($survey->questions()->orderby("order")->get() as $question)
-                    @include(question_view($question->type), ['question' => $question])
-                @endforeach
-                <div style="text-align: center">
-                    <input type="submit" class="btn btn-success" value="Gửi"/>
-                </div>
-            </form>
-        </div>
+            @foreach($survey->questions()->orderby("order")->get() as $question)
+                @include(question_view($question->type), ['question' => $question])
+            @endforeach
+            <div style="text-align: center" id="submit-button">
+                <input style="padding:6px 40px" type="submit" class="btn btn-success" value="Gửi"/>
+            </div>
+        </form>
+    </div>
+@endsection
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-    <script>
+@section("script")
+<script>
         $(document).ready(() => {
             let user = {};
             const form = $("#form");
             form.submit((event) => {             
                 event.preventDefault();
                 const answers = form.serializeArray();
-                const reducedArray = [];
+                
+                console.log(answers);
+                const reducedArray = {};
                 for (let i = 0; i < answers.length; i++) {
-                    const {name, value} = answers[i];
+                    let {name, value} = answers[i];
+                    name = name + "";
                     if (name === "name") {
                         user.name = value;
                     } else if (name === "email") {
@@ -73,11 +67,23 @@
                         }
                     }
                 }
-                console.log(reducedArray);
+                const submitData = JSON.stringify(reducedArray);
+                
+                console.log(submitData);
                 console.log(user);
-                // TODO: post data to server  
+                $("#submit-button").html(
+                    "<i class=\"fa fa-circle-o-notch fa-spin\"></i> Đang tải lên"
+                );
+
+                $.post("/survey/{{$survey->id}}/store", {
+                    data : submitData,
+                    email: user.email,
+                    name: user.name,
+                    phone: user.phone
+                }, (data) => {
+                    window.location.replace("/survey/submitted");
+                })
             });
         });        
     </script>
-  </body>
-</html>
+@endsection
