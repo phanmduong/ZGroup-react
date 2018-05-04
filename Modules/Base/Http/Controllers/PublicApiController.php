e42716b8fd92b7d14393be799af1e63789f123ec
komatsu98 <chemgiosa5@gmail.com>
2018-05-04 20:26:03 +0700

    api blog

--- Modules/Base/Http/Controllers/PublicApiController.php
+++ Modules/Base/Http/Controllers/PublicApiController.php
@@ -79,8 +79,8 @@ class PublicApiController extends NoAuthApiController
         $limit = $request->limit ? $request->limit : 6;
         $category_id = $request->category_id;
         $kind = $request->kind;
-        $tag = $request->tag;
-        $blogs = Product::where([['type', '=', 2], ['kind', '=', $kind], ['category_id', '=', $category_id,], ['tag', '=', $tag]])->orderBy('created_at', 'desc')->get();
+//        $tag = $request->tag;
+        $blogs = Product::where([['type', '=', 2], ['kind', '=', $kind], ['category_id', '=', $category_id,]])->orderBy('created_at', 'desc')->get();
         $blogs = $blogs->where('title', 'like', '%' . trim($request->search) . '%');
         $blogs = $blogs->paginate($limit);
         return $this->respondWithPagination($blogs, ['blogs' => $blogs->map(function ($blog) {
