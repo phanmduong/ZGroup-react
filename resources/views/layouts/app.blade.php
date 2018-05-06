be84fabf83989caff06f723355b6005ce9c6c36e
datvithanh98 <datvithanh98@gmail.com>
2018-05-06 09:27:59 +0700

    up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,95 +1,95 @@
-{{--<!DOCTYPE html>--}}
-{{--<html lang="en">--}}
-{{--<head>--}}
-    {{--<meta charset="utf-8">--}}
-    {{--<meta http-equiv="X-UA-Compatible" content="IE=edge">--}}
-    {{--<meta name="viewport" content="width=device-width, initial-scale=1">--}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    {{--<title>Laravel</title>--}}
+    <title>Laravel</title>
 
-    {{--<!-- Fonts -->--}}
-    {{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">--}}
-    {{--<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">--}}
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
 
-    {{--<!-- Styles -->--}}
-    {{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">--}}
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+     <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
 
-    {{--<style>--}}
-        {{--body {--}}
-            {{--font-family: 'Lato';--}}
-        {{--}--}}
+    <style>
+        body {
+            font-family: 'Lato';
+        }
 
-        {{--.fa-btn {--}}
-            {{--margin-right: 6px;--}}
-        {{--}--}}
-    {{--</style>--}}
-{{--</head>--}}
-{{--<body id="app-layout">--}}
-    {{--<nav class="navbar navbar-default navbar-static-top">--}}
-        {{--<div class="container">--}}
-            {{--<div class="navbar-header">--}}
+        .fa-btn {
+            margin-right: 6px;
+        }
+    </style>
+</head>
+<body id="app-layout">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
 
-                {{--<!-- Collapsed Hamburger -->--}}
-                {{--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">--}}
-                    {{--<span class="sr-only">Toggle Navigation</span>--}}
-                    {{--<span class="icon-bar"></span>--}}
-                    {{--<span class="icon-bar"></span>--}}
-                    {{--<span class="icon-bar"></span>--}}
-                {{--</button>--}}
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
 
-                {{--<!-- Branding Image -->--}}
-                {{--<a class="navbar-brand" href="{{ url('/') }}">--}}
-                    {{--Laravel--}}
-                {{--</a>--}}
-            {{--</div>--}}
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    Laravel
+                </a>
+            </div>
 
-            {{--<div class="collapse navbar-collapse" id="app-navbar-collapse">--}}
-                {{--<!-- Left Side Of Navbar -->--}}
-                {{--<ul class="nav navbar-nav">--}}
-                    {{--<li><a href="{{ url('/home') }}">Home</a></li>--}}
-                {{--</ul>--}}
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ url('/home') }}">Home</a></li>
+                </ul>
 
-                {{--<!-- Right Side Of Navbar -->--}}
-                {{--<ul class="nav navbar-nav navbar-right">--}}
-                    {{--<!-- Authentication Links -->--}}
-                    {{--@if (Auth::guest())--}}
-                        {{--<li><a href="{{ url('/login') }}">Login</a></li>--}}
-                        {{--<li><a href="{{ url('/register') }}">Register</a></li>--}}
-                    {{--@else--}}
-                        {{--<li class="dropdown">--}}
-                            {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">--}}
-                                {{--{{ Auth::user()->name }} <span class="caret"></span>--}}
-                            {{--</a>--}}
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
 
-                            {{--<ul class="dropdown-menu" role="menu">--}}
-                                {{--<li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>--}}
-                            {{--</ul>--}}
-                        {{--</li>--}}
-                    {{--@endif--}}
-                {{--</ul>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</nav>--}}
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-    {{--@yield('content')--}}
+    @yield('content')
 
-    {{--<!-- JavaScripts -->--}}
-    {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>--}}
-    {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>--}}
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
-{{--</body>--}}
-{{--</html>--}}
-        <!DOCTYPE html>
+    <!-- JavaScripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+     <script src="{{ elixir('js/app.js') }}"></script>
+</body>
+</html>
+        <!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <title>@yield('title')</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" name="viewport">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-{{--<link rel="stylesheet" href="{{ URL::asset('css/materialize.min.css') }}"/>--}}
+<link rel="stylesheet" href="{{ URL::asset('css/materialize.min.css') }}"/> -->
 
 <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/css/materialize.min.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/css/materialize.min.css">
 
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
@@ -108,7 +108,7 @@
           href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/flick/jquery-ui.css">
     <link href="{{url('css/jquery.tagit.css')}}" rel="stylesheet" type="text/css">
 
-    {{--<script src="//fast.eager.io/II2OmwIcNV.js"></script>--}}
+    <script src="//fast.eager.io/II2OmwIcNV.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
     <script src="{{URL::asset('js/pickadate/lib/picker.js')}}"></script>
     <script src="{{URL::asset('js/pickadate/lib/picker.date.js')}}"></script>
@@ -155,10 +155,10 @@
         </ul>
         <nav>
             <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">toc</i></a>
-            <ul class="right">
+            <ul class="right"> -->
 
                 <!-- Authentication Links -->
-                @if (Auth::guest())
+                <!-- @if (Auth::guest())
                     <li><a href="{{ url('/login') }}">Login</a></li>
                     <li><a href="{{ url('/register') }}">Register</a></li>
                 @else
@@ -180,11 +180,11 @@
 </main>
 
 
-{{--<script src="{{URL::asset('js/materialize.min.js')}}">--}}
-{{--</script>--}}
+<script src="{{URL::asset('js/materialize.min.js')}}">
+</script> -->
 
 <!-- Compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/js/materialize.min.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/js/materialize.min.js"></script>
 
 <script src="{{URL::asset('js/jquery.zmd.hierarchical-display.min.js')}}">
 
@@ -199,4 +199,4 @@
 </script>
 <script src="{{URL::asset('js/tag-it.js')}}"></script>
 </body>
-</html>
+</html> -->
