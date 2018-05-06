b352968f0cfe73963545212c932b2f9995749d3d
Batman Bruce Wayne <taideptrai02@gmail.com>
2018-05-06 11:59:02 +0700

    sms

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -894,6 +894,23 @@ export function changeToSlug(title) {
     return slug;
 }
 
+export function changeToSlugSpace(title) {
+    let slug = title;
+
+    //Đổi ký tự có dấu thành không dấu
+    slug = slug.replace(/á|à|ả|ạ|ã|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ/gi, 'a');
+    slug = slug.replace(/é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ/gi, 'e');
+    slug = slug.replace(/i|í|ì|ỉ|ĩ|ị/gi, 'i');
+    slug = slug.replace(/ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ/gi, 'o');
+    slug = slug.replace(/ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự/gi, 'u');
+    slug = slug.replace(/ý|ỳ|ỷ|ỹ|ỵ/gi, 'y');
+    slug = slug.replace(/đ/gi, 'd');
+    // eslint-disable-next-line
+
+    return slug;
+}
+
+
 export function readExcel(file, isSkipReadFile) {
 
     let promise = new Promise((resolve) => {
