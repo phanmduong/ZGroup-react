6961e577c32c75de6e08a358d1d8015256283c2e
datvithanh98 <datvithanh98@gmail.com>
2018-05-06 17:03:50 +0700

    up

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
         SendCheckInCheckOutNotification::class,
         CheckInCheckOutSMNotification::class,
         WorkShiftsCheckInCheckOutNoti::class,
-        SendEmailsResource::class
+        SendEmailsResource::class,
         SendSmsCampaign::class
     ];
 
@@ -59,6 +59,6 @@ class Kernel extends ConsoleKernel
         $schedule->command('create:workShifts')->weekly()->fridays()->at('9:00');
         $schedule->command('calendarEvent:remind')->everyMinute();
 
-        $schedule->command('emailsSending:resource')->weekly()->sundays()->at('17:05');
+        $schedule->command('emailsSending:resource')->weekly()->sundays()->at('17:07');
     }
 }
