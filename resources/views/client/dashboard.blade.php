a3315570d92752b2cafa4b1f2f7121dfba766138
Kiyoshitaro <hunglhp1998@gmail.com>
2018-05-11 15:38:26 +0700

    build dashboard

--- resources/views/client/dashboard.blade.php
+++ resources/views/client/dashboard.blade.php
@@ -1,1 +1,4 @@
-@extends("client.layout.master") @section("css") <link href="manage/dashboard/main.ed9b849a7856592b2c3c190005c81922.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/dashboard/main.b273eaac4636a71fe708.js"></script> @endsection
+@extends("client.layout.master") @section("css")
+    <link href="manage/dashboard/main.073dcb5253225c8c881fb8d12f3cfac1.css" rel="stylesheet"> @endsection @section("js")
+    <script type="text/javascript" src="manage/dashboard/main.991b5d99c2b89e308ab9.js"></script> @endsection
+
