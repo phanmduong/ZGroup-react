c99fee39765e8b8906b2af98e2830950c076d783
Batman Bruce Wayne <taideptrai02@gmail.com>
2018-05-11 16:56:39 +0700

    sms

--- resources/views/client/sms.blade.php
+++ resources/views/client/sms.blade.php
@@ -1,2 +1,2 @@
-@extends("client.layout.master") @section("css") <link href="manage/sms/main.472a649d5aae157aa387d0b9590f64ad.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/sms/main.b96cd647fdaf6157e7b6.js"></script> @endsection
+@extends("client.layout.master") @section("css") <link href="manage/sms/main.472a649d5aae157aa387d0b9590f64ad.css" rel="stylesheet"> @endsection @section("js") <script type="text/javascript" src="manage/sms/main.62347b32fb2caa61edd1.js"></script> @endsection
 
