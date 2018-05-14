4e434b13f8bf1e7c060998e78c1a5e10622f0584
phanmduong <phanminhduong@hotmail.com>
2018-05-14 11:25:39 +0700

    update code

--- Modules/TrongDongPalace/Http/routes.php
+++ Modules/TrongDongPalace/Http/routes.php
@@ -2,7 +2,7 @@
 
 $publicRoutes = function () {
     Route::get('/', 'TrongDongPalaceController@index');
-    Route::get('/blog1', 'TrongDongPalaceController@blog');
+    Route::get('/blog', 'TrongDongPalaceController@blog');
     Route::get('/album', 'TrongDongPalaceController@album');
     Route::get('/blog/post/{postId}', 'TrongDongPalaceController@post');
     Route::get('/test', 'TrongDongPalaceController@test');
@@ -26,7 +26,6 @@ $manageApiRoutes = function () {
 
 Route::group(['middleware' => 'web', 'domain' => 'keetool6.xyz', 'namespace' => 'Modules\TrongDongPalace\Http\Controllers'], $publicRoutes);
 Route::group(['middleware' => 'web', 'domain' => 'trongdongpalace.test', 'namespace' => 'Modules\TrongDongPalace\Http\Controllers'], $publicRoutes);
-Route::group(['middleware' => 'web', 'domain' => 'zgroup.dev', 'namespace' => 'Modules\TrongDongPalace\Http\Controllers'], $publicRoutes);
 
 Route::group(
     ['domain' => config('app.domain'), 'prefix' => 'manageapi', 'namespace' => 'Modules\TrongDongPalace\Http\Controllers'],
