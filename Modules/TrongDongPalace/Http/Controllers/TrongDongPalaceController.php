4606a1ee90978ed3ec146fbd5f06aa17b0832a63
phanmduong <phanminhduong@hotmail.com>
2018-05-14 12:01:56 +0700

    update code

--- Modules/TrongDongPalace/Http/Controllers/TrongDongPalaceController.php
+++ Modules/TrongDongPalace/Http/Controllers/TrongDongPalaceController.php
@@ -71,7 +71,7 @@ class TrongDongPalaceController extends Controller
                                   count(SUBSTRING_INDEX(SUBSTRING_INDEX(products.tags, ',', tag_numbers.id), ',', -1)) sum_tag
                                 FROM
                                   tag_numbers INNER JOIN products
-                                  ON products.kind='$kind' AND CHAR_LENGTH(products.tags)
+                                  ON products.kind='$kind' AND CHAR_LENGTH(products.tags) AND products.deleted_at = NULL
                                      -CHAR_LENGTH(REPLACE(products.tags, ',', ''))>=tag_numbers.id-1 
                                 WHERE (SUBSTRING_INDEX(SUBSTRING_INDEX(products.tags, ',', tag_numbers.id), ',', -1) <> '' || SUBSTRING_INDEX(SUBSTRING_INDEX(products.tags, ',', tag_numbers.id), ',', -1) <> NULL)
                                 GROUP BY tag 
