db316b651226567bf3799abdba275f5471fd8f8b
caoquan95 <aquancva@gmail.com>
2018-05-20 14:03:19 +0700

    add converter html

--- src/components/common/editor/HtmlConverter.js
+++ src/components/common/editor/HtmlConverter.js
@@ -6,9 +6,11 @@ const BLOCK_TAGS = {
     blockquote: "quote",
     pre: "code",
     img: "image",
+
     li: "list-item",
     ul: "bulleted-list",
     ol: "numbered-list",
+
     h1: "heading-one",
     h2: "heading-two",
     h3: "heading-three",
@@ -57,6 +59,27 @@ const rules = [
                         const style = { display: "block", width: "100%" };
                         return <img src={src} style={style} />;
                     }
+                    case "heading-one":
+                        return <h1>{children}</h1>;
+                    case "heading-two":
+                        return <h2>{children}</h2>;
+                    case "heading-three":
+                        return <h3>{children}</h3>;
+                    case "heading-four":
+                        return <h4>{children}</h4>;
+                    case "heading-five":
+                        return <h5>{children}</h5>;
+                    case "heading-six":
+                        return <h6>{children}</h6>;
+
+                    case "list-item":
+                        return <li>{children}</li>;
+
+                    case "bulleted-list":
+                        return <ul>{children}</ul>;
+                    case "numbered-list":
+                        return <ol>{children}</ol>;
+
                     case "paragraph":
                         return <p>{children}</p>;
                     case "quote":
