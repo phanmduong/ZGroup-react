bb4097989abd4a0f24c15cf2b14ff99b212752a6
hahieu <goldenrezgun@gmail.com>
2019-12-07 17:05:29 +0700

    Build

---
+++ bash_scripts/buildMultiThreads.sh
@@ -0,0 +1,25 @@
+branch=$1
+if [ -z "$branch" ]; then
+    branch=zgroup
+fi
+
+branch_name=$(git symbolic-ref -q HEAD)
+branch_name=${branch_name##refs/heads/}
+branch_name=${branch_name:-HEAD}
+
+
+i=0
+for file in $(find "../src/entries" -type f)
+do
+file_name=$(basename $file)  
+module_name="${file_name%.*}"
+echo "${i} : ${file_name%.*}"
+i=$((i+1))
+if ! (( i % 12 ));
+then
+  sleep 100
+fi
+
+gnome-terminal --tab --command="./build.sh $module_name $branch_name & exit"
+
+done
