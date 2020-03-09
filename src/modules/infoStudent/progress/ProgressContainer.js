337188d9fb409c0d4851d9312709bd101ae3f002
hahieu <goldenrezgun@gmail.com>
2020-03-09 16:10:31 +0700

    Build

--- src/modules/infoStudent/progress/ProgressContainer.js
+++ src/modules/infoStudent/progress/ProgressContainer.js
@@ -19,7 +19,16 @@ class ProgressContainer extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.studentId = this.props.params ? this.props.params.studentId : this.props.studentId;
+        this.state = {
+            selectedTabs:{
 
+            }
+        };
+        this.tabs = [
+            {label:'Điểm danh', },
+            {label:'Bài kiểm tra', },
+            {label:'Nhận xét', },
+        ];
     }
 
 
@@ -107,6 +116,7 @@ class ProgressContainer extends React.Component {
                         {
                             this.props.progress && this.props.progress.length > 0 ? this.props.progress.map((progressClass, index) => {
                                 let noneGroup = progressClass.exams.filter(e => isEmptyInput(e.group_exam_id));
+                                let currentTab = this.state.selectedTabs[index] || 0;
                                 return (
                                     <li className="timeline-inverted" key={index}>
                                         <div className="timeline-badge">
@@ -184,6 +194,82 @@ class ProgressContainer extends React.Component {
                                                     </div>
                                                 </div>
                                                 <div className="row">
+                                                    <div className="col-md-12">
+                                                        <div className="flex flex-wrap">
+                                                            {this.tabs.map((tab,key)=>{
+                                                                let classNameTab = currentTab == key ? "btn btn-actions" : "btn btn-actions background-transparent";
+                                                                return (
+                                                                    <div className={classNameTab}
+                                                                        onClick={()=>{
+                                                                            let {selectedTabs} = this.state;
+                                                                            selectedTabs[index] = key;
+                                                                            this.setState({selectedTabs});
+                                                                        }}
+                                                                    >{tab.label}</div>
+                                                                );
+                                                            })}
+                                                            {currentTab == 0 && <div>
+
+                                                            </div>}
+                                                            {currentTab == 1 &&
+                                                            <div className="col-md-12">
+                                                                <div className="margin-top-10">
+                                                                    {progressClass.exams.length == 0 && noneGroup.length == 0 && <div>Không có dữ liệu</div>}
+                                                                    {progressClass.group_exams.map(group => {
+                                                                        return (
+                                                                            <div className="margin-bottom-10">
+                                                                                <div><b>{group.name}</b></div>
+                                                                                <div className="padding-left-15">
+                                                                                    <table id="datatables"
+                                                                                           className="table table-hover table-split"
+                                                                                           cellSpacing="0" width="100%"
+                                                                                           style={{width: "100%"}}>
+                                                                                        <tbody>
+                                                                                        {progressClass.exams.filter(e => e.group_exam_id == group.id).map(exam => {
+                                                                                            return (<tr>
+                                                                                                <td>{exam.title}</td>
+                                                                                                <td>{exam.score}</td>
+                                                                                                <td>{exam.comment}</td>
+                                                                                                {exam.inputTeacher ? <td>Nhập
+                                                                                                    bởi <b>{exam.inputTeacher.name}</b>
+                                                                                                </td> : <td/>}
+                                                                                                <td>{exam.created_at}</td>
+                                                                                            </tr>);
+                                                                                        })}
+                                                                                        </tbody>
+                                                                                    </table>
+                                                                                </div>
+                                                                            </div>
+                                                                        );
+                                                                    })}
+                                                                    {noneGroup && noneGroup.length > 0 &&
+                                                                    <div className="margin-bottom-10">
+                                                                        <div><b>Không có nhóm</b></div>
+                                                                        <div className="padding-left-15">
+                                                                            <table id="datatables"
+                                                                                   className="table table-hover table-split"
+                                                                                   cellSpacing="0" width="100%"
+                                                                                   style={{width: "100%"}}>
+                                                                                <tbody>
+                                                                                {noneGroup.map(exam => {
+                                                                                    return (<tr>
+                                                                                        <td>{exam.title}</td>
+                                                                                        <td>{exam.score}</td>
+                                                                                        <td>{exam.comment}</td>
+                                                                                        {exam.inputTeacher ? <td>Nhập
+                                                                                            bởi <b>{exam.inputTeacher.name}</b>
+                                                                                        </td> : <td/>}
+                                                                                        <td>{exam.created_at}</td>
+                                                                                    </tr>);
+                                                                                })}
+                                                                                </tbody>
+                                                                            </table>
+                                                                        </div>
+                                                                    </div>}
+                                                                </div>
+                                                            </div>}
+                                                        </div>
+                                                    </div>
                                                     <div className="col-md-12">
                                                         <div className="margin-top-10">
                                                             {progressClass.group_exams.map(group => {
