500b974882c2405e1f522db0d99eca56f6dcf8ee
hahieu <goldenrezgun@gmail.com>
2020-05-12 11:55:04 +0700

    Build

--- src/modules/registerStudentsV2/RegisterList.js
+++ src/modules/registerStudentsV2/RegisterList.js
@@ -65,7 +65,8 @@ class RegisterList extends React.Component {
                                     <div className="container-dot-bottom-right">
                                         {register.studyClass &&
                                         <button className="btn btn-round btn-fab btn-fab-mini text-white">
-                                            {register.course &&register.course.icon_url && <TooltipButton text={register.studyClass.name} placement="top">
+                                            {register.course && register.course.icon_url &&
+                                            <TooltipButton text={register.studyClass.name} placement="top">
                                                 <img src={register.course.icon_url} alt=""/>
                                             </TooltipButton>}
                                         </button>}
@@ -75,16 +76,17 @@ class RegisterList extends React.Component {
                                 <td>
                                     {register.tele_call &&
                                     <TooltipButton text={register.tele_call.call_status_text} placement="top">
-                                    <div className="container-call-status">
+                                        <div className="container-call-status">
 
-                                        <button className={"btn btn-round " + btn + " full-width padding-left-right-10"}
-                                            // onClick={() => this.props.viewCall(register)}
+                                            <button
+                                                className={"btn btn-round " + btn + " full-width padding-left-right-10"}
+                                                // onClick={() => this.props.viewCall(register)}
                                             >
 
                                                 <i className="material-icons">phone</i>
                                                 {register.tele_call.call_status !== 'calling' && (register.time_to_reach + 'h')}
 
-                                        </button>
+                                            </button>
                                         </div>
                                     </TooltipButton>}
                                 </td>
@@ -98,7 +100,8 @@ class RegisterList extends React.Component {
                                         {/*     type="button" rel="tooltip"*/}
                                         {/*     data-original-title={register.email}>*/}
                                         {/*    {register.email}</div>*/}
-                                        <a href={"tel:" + register.student.phone} className="text-phone-student-register">
+                                        <a href={"tel:" + register.student.phone}
+                                           className="text-phone-student-register">
                                             {helper.formatPhone(register.student.phone)}
                                         </a>
                                     </div>}
@@ -136,7 +139,7 @@ class RegisterList extends React.Component {
                                                 (
                                                     <button className="btn btn-xs btn-main"
                                                             style={{backgroundColor: '#' + register.marketing_campaign.color}}
-                                                            // onClick={() => this.props.loadRegisterStudentByCampaign(register.campaign.id)}
+                                                        // onClick={() => this.props.loadRegisterStudentByCampaign(register.campaign.id)}
                                                     >
                                                         {register.campaign.name}
                                                         <div className="ripple-container"/>
@@ -145,7 +148,7 @@ class RegisterList extends React.Component {
                                                 :
                                                 (
                                                     <button className="btn btn-xs btn-main"
-                                                            // onClick={() => this.props.loadRegisterStudentByCampaign('-1')}
+                                                        // onClick={() => this.props.loadRegisterStudentByCampaign('-1')}
                                                     >
                                                         Không có
                                                         <div className="ripple-container"/>
@@ -165,16 +168,16 @@ class RegisterList extends React.Component {
                                 <td>
                                     {
                                         register.paid_status ?
-                                            <div className="btn btn-xs btn-main main-background-color"
-                                                 data-toggle="tooltip" title=""
-                                                 type="button" rel="tooltip"
-                                                 data-original-title={register.note}>
-                                                {helper.dotNumber(register.money)} vnd
-                                            </div>
+                                            <TooltipButton text={register.note} placement="top">
+                                                <div className="btn btn-xs btn-main main-background-color">
+                                                    {helper.dotNumber(register.money)} vnd
+                                                </div>
+                                            </TooltipButton>
                                             :
                                             <div>{register.tele_call &&
-                                            <TooltipButton text={`Hẹn nộp ${register.tele_call.appointment_payment_date}`}
-                                                           placement="top">
+                                            <TooltipButton
+                                                text={`Hẹn nộp ${register.tele_call.appointment_payment_date}`}
+                                                placement="top">
                                                 <div className="margin-bottom-10">
                                                     {
                                                         register.tele_call.appointment_payment ?
