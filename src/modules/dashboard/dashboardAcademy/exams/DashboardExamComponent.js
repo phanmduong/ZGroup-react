f847216d5d9b88e51fd75167174e9c419bea5474
hahieu <goldenrezgun@gmail.com>
2020-05-31 10:38:49 +0700

    Save

--- src/modules/dashboard/dashboardAcademy/exams/DashboardExamComponent.js
+++ src/modules/dashboard/dashboardAcademy/exams/DashboardExamComponent.js
@@ -5,12 +5,12 @@ import filterExamStore from "./filterExamStore";
 import CardAcademy from "../CardAcademy";
 import Loading from "../../../../components/common/Loading";
 import DashboardExamStore from "./DashboardExamStore";
-import Barchart from "./BarChart";
-import MNABarChart from "./MNABarChart";
-import * as helper from "../../../../helpers/helper";
 import {NO_AVATAR} from "../../../../constants/env";
 import EmptyData from "../../../../components/common/EmptyData";
 import {openModalRegisterDetail} from "../../../globalModal/globalModalActions";
+import Barchart from "./BarChart";
+import MNABarChart from "./MNABarChart";
+import * as helper from "../../../../helpers/helper";
 
 const labels = ['0-1', '1-2', '2-3', '3-4', '4-5', '5-6', '6-7', '7-8', '8-9', '9-10'];
 
@@ -950,11 +950,8 @@ class DashboardExamComponent extends React.Component {
     }
     getGroupStudent = (analytic, group) => {
         const analyticTemplate = analytic.filter((item) => item.group_exam_id == group.id);
-
         let students = [];
         let student_ids = [];
-
-
         analyticTemplate.forEach((item) => {
             item.user_exams.forEach((user_exam) => {
                 if (student_ids.indexOf(user_exam.user_id) < 0) {
@@ -963,7 +960,6 @@ class DashboardExamComponent extends React.Component {
                 }
             });
         });
-
         return students;
 
     }
@@ -976,16 +972,11 @@ class DashboardExamComponent extends React.Component {
                 class_ids.push(o.class.id);
             }
         });
-
         let labels = classes.map(c => c.name);
-
-
         let data = [
             {name: 'Min', fill: false, data: []},
             {name: 'Avg', fill: false, data: []},
             {name: 'Max', fill: false, data: []},
-
-
         ];
         classes.forEach(c => {
             let min = 10, max = 0, sum = 0, count = 0;
@@ -1039,7 +1030,6 @@ class DashboardExamComponent extends React.Component {
                 label: c.name,
                 name: c.name,
                 color: '#c50000',
-                // yAxisID: 'A',
                 data
             };
         });
@@ -1048,8 +1038,7 @@ class DashboardExamComponent extends React.Component {
     };
 
     getGroupExams = () => {
-        let groupExamIds = [];
-        let groupExams = [];
+        let groupExamIds = [],groupExams = [];
         let {analytic_exam} = this.store;
         analytic_exam.forEach(itm => {
             if (groupExamIds.indexOf(itm.group_exam_id) == -1) {
@@ -1061,23 +1050,20 @@ class DashboardExamComponent extends React.Component {
     }
 
     render() {
-        let {isLoading, analytic_exam} = this.store;
         let {course_id, class_id} = filterExamStore.filter;
-        const emptyTitle = (course_id || class_id) ? "Không có dữ liệu" : "Chọn môn học hoặc lớp để xem thống kê bài kiểm tra";
+        let {isLoading, analytic_exam} = this.store;
         let groupExams = this.getGroupExams();
-        // console.log(analytic_exam)
+        const emptyTitle = (course_id || class_id) ? "Không có dữ liệu" : "Chọn môn học hoặc lớp để xem thống kê bài kiểm tra";
+        console.log(analytic_exam);
         return (
             <div>
                 <FilterExam loadData={this.loadData}/>
                 <CardAcademy/>
-
                 <div className="row gutter-20 margin-top-20">
                     <div className="col-md-12">
                         {(isLoading || filterExamStore.isLoading) && <Loading/>}
                         {!(isLoading || filterExamStore.isLoading) && analytic_exam.length == 0 &&
                         <EmptyData title={emptyTitle}/>}
-
-
                         {!(isLoading || filterExamStore.isLoading) && groupExams.map((group, key_group) => {
                             let templates = analytic_exam.filter((template) => template.group_exam_id == group.id);
                             let groupScores = this.getGroupScore(analytic_exam, group);
@@ -1086,18 +1072,14 @@ class DashboardExamComponent extends React.Component {
                             if (templates.length) return (
                                 <div className="card" key={key_group}>
                                     <div className="card-content">
-
                                         <div className="flex flex-row flex-align-items-center" style={{
-                                            fontWeight: 'bold',
                                             fontSize: 30,
-                                            marginTop: 30,
                                             marginBottom: 40,
-                                        }}>
-                                            {group.name}
-                                        </div>
+                                            marginTop: 30,
+                                            fontWeight: 'bold',
+                                        }}>{group.name}</div>
                                         <div>
                                             {templates.map((template, key_temp) => {
-
                                                 const scores = this.getScore(analytic_exam, template);
                                                 const data = this.getAnalytics(scores);
                                                 let barchartId = `barchart-analytics-exam-${key_group}-${key_temp}-${template.group_exam_id}-${template.exam_template_id}`;
@@ -1114,7 +1096,6 @@ class DashboardExamComponent extends React.Component {
                                                                     id={barchartId}
                                                                 />
                                                             </div>
-
                                                         </div>
                                                     </div>
                                                 );
@@ -1178,7 +1159,6 @@ class DashboardExamComponent extends React.Component {
                                         </div>
                                         <div className="margin-top-30">
                                             {templates.map((template, key_temp) => {
-
                                                 const scores = this.getScore(analytic_exam, template);
                                                 const analyticsMna = this.getAnalyticsMna(scores);
                                                 // console.log('temp :' + template.id, scores);
@@ -1249,16 +1229,11 @@ class DashboardExamComponent extends React.Component {
                                 </div>
                             );
                         })}
-
-
                     </div>
-
                 </div>
             </div>
-
         );
     }
 }
-
 
 export default DashboardExamComponent;
