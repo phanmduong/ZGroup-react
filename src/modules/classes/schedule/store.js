7a5fc7acb88156370e7e34d86fa73937555ab094
hahieu <goldenrezgun@gmail.com>
2020-06-10 15:23:23 +0700

    Save

--- src/modules/classes/schedule/store.js
+++ src/modules/classes/schedule/store.js
@@ -13,7 +13,8 @@ class Store {
     @action
     createSchedule(callback) {
         this.isLoading = true;
-        let studySessions = this.days.filter((day) => !isEmptyInput(day.start_time) && !isEmptyInput(day.end_time))
+        let studySessions = this.days.sort((a,b)=>a.order > b.order)
+            .filter((day) => !isEmptyInput(day.start_time) && !isEmptyInput(day.end_time))
             .map((day) => {
                 return {
                     weekday: day.value,
@@ -34,42 +35,49 @@ export default Store;
 
 const DAY_OF_WEEK = [
     {
+        order: 1,
         key: 'T2',
         value: "Thứ hai",
         start_time: "",
         end_time: "",
         selected: false
     }, {
+        order: 2,
         key: 'T3',
         value: "Thứ ba",
         start_time: "",
         end_time: "",
         selected: false
     }, {
+        order: 3,
         key: 'T4',
         value: "Thứ tư",
         start_time: "",
         end_time: "",
         selected: false
     }, {
+        order: 4,
         key: 'T5',
         value: "Thứ năm",
         start_time: "",
         end_time: "",
         selected: false
     }, {
+        order: 5,
         key: 'T6',
         value: "Thứ sáu",
         start_time: "",
         end_time: "",
         selected: false
     }, {
+        order: 6,
         key: 'T7',
         value: "Thứ bảy",
         start_time: "",
         end_time: "",
         selected: false
     }, {
+        order: 7,
         key: 'CN',
         value: "Chủ nhật",
         start_time: "",
